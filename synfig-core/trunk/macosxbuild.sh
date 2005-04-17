9689624f60b85da0a22bd6e77f1f8ff0b2b61a9d
darco <darco@1f10aa63-cdf2-0310-b900-c93c546f37ac>
2005-04-17 18:59:24 +0000

    more updates
    git-svn-id: http://svn.voria.com/code@17 1f10aa63-cdf2-0310-b900-c93c546f37ac

--- synfig-core/trunk/macosxbuild.sh
+++ synfig-core/trunk/macosxbuild.sh
@@ -5,9 +5,19 @@
 OPTIONS=""
 #OPTIONS="$OPTIONS --enable-timelimit=120"
 OPTIONS="$OPTIONS --enable-optimization=2"
-OPTIONS="$OPTIONS --enable-optimization=3 --enable-g5opt"
+#OPTIONS="$OPTIONS --enable-optimization=3 --enable-g5opt"
 #OPTIONS="$OPTIONS --disable-optimization"
 OPTIONS="$OPTIONS --disable-debug"
+OPTIONS="$OPTIONS --with-vimage"
+
+CC=gcc
+CXX=g++
+
+#distcc --version && {
+#	CC=distcc
+#	CXX="distcc g++"
+#}
+
 
 BUILDDIR=macosxbuild
 
@@ -19,9 +29,11 @@ mkdir $BUILDDIR
 
 cd $BUILDDIR
 
-../configure $OPTIONS || exit 1
+echo ../configure $OPTIONS CC="$CC" CXX="$CXX"
 
-make package
+../configure $OPTIONS CC="$CC" CXX="$CXX" || exit 1
+
+make package $MAKEFLAGS
 
 #make installer
 
