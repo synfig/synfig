c1a94c021695a1d85fe7effbba7f8fea67c2958e
darco <darco@1f10aa63-cdf2-0310-b900-c93c546f37ac>
2005-11-29 04:30:35 +0000

    Bump version number. Enable optimisation level 2 and disable debug on MacOS X. Put GPL2 instead of LGPL in the COPYING file.
    git-svn-id: http://svn.voria.com/code@66 1f10aa63-cdf2-0310-b900-c93c546f37ac

--- synfig-studio/trunk/macosxbuild.sh
+++ synfig-studio/trunk/macosxbuild.sh
@@ -5,11 +5,11 @@ export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/usr/lib/pkgconfig:/usr/X11R6/li
 export MACOSX_DEPLOYMENT_TARGET=10.4
 OPTIONS=""
 #OPTIONS="$OPTIONS --enable-timelimit=120"
-#OPTIONS="$OPTIONS --enable-optimization=2"
+OPTIONS="$OPTIONS --enable-optimization=2"
 #OPTIONS="$OPTIONS --enable-optimization=3 --enable-g5opt"
-OPTIONS="$OPTIONS --disable-optimization"
-#OPTIONS="$OPTIONS --disable-debug"
-OPTIONS="$OPTIONS --enable-debug"
+#OPTIONS="$OPTIONS --disable-optimization"
+OPTIONS="$OPTIONS --disable-debug"
+#OPTIONS="$OPTIONS --enable-debug"
 #export LDFLAGS='-undefined dynamic-lookup'
 LDFLAGS="-prebind -prebind_all_twolevel_modules -twolevel_namespace -bind_at_load -undefined dynamic_lookup"
 BUILDDIR=macosxbuild
