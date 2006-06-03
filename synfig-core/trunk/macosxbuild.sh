6a271229bf821e96db38c918f02c6c14786043ec
darco <darco@1f10aa63-cdf2-0310-b900-c93c546f37ac>
2006-06-03 20:12:46 +0000

    Minor PNG fix in configure script. Updates to macosxbuild for universal builds.
    git-svn-id: http://svn.voria.com/code@179 1f10aa63-cdf2-0310-b900-c93c546f37ac

--- synfig-core/trunk/macosxbuild.sh
+++ synfig-core/trunk/macosxbuild.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 #OPTIONS="--disable-optimization --enable-debug"
-export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/usr/lib/pkgconfig:/usr/X11R6/lib/pkgconfig
-OPTIONS=""
+
+PREFIX=/Applications/SynfigStudio.app/Contents/synfig
+
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:/usr/lib/pkgconfig:/usr/X11R6/lib/pkgconfig
+OPTIONS="--prefix=$PREFIX"
 #OPTIONS="$OPTIONS --enable-timelimit=120"
 #OPTIONS="$OPTIONS --enable-optimization=2"
 OPTIONS="$OPTIONS --enable-optimization=3"
@@ -11,9 +14,21 @@ OPTIONS="$OPTIONS --enable-optimization=3"
 OPTIONS="$OPTIONS --disable-debug"
 #OPTIONS="$OPTIONS --with-vimage"
 OPTIONS="$OPTIONS --without-openexr"
+OPTIONS="$OPTIONS --disable-dependency-tracking"
+#OPTIONS="$OPTIONS --enable-universal"
 
-CC=gcc
-CXX=g++
+ARCH_FLAGS=""
+ARCH_FLAGS="$ARCH_FLAGS -arch ppc"
+ARCH_FLAGS="$ARCH_FLAGS -arch i386"
+
+LDFLAGS="-L$PREFIX/lib"
+CFLAGS="-I$PREFIX/include"
+CXXFLAGS="-I$PREFIX/include"
+
+CC="gcc $LDFLAGS $CFLAGS $ARCH_FLAGS"
+CXX="g++ $LDFLAGS $CFLAGS $ARCH_FLAGS"
+CPP="gcc -E"
+CXXCPP="g++ -E"
 
 #distcc --version && {
 #	CC=distcc
@@ -31,11 +46,14 @@ mkdir $BUILDDIR
 
 cd $BUILDDIR
 
-echo ../configure $OPTIONS CC="$CC" CXX="$CXX"
+echo ../configure $OPTIONS CC="$CC" CXX="$CXX" CPP="$CPP" CXXCPP="$CXXCPP" LDFLAGS="$LDFLAGS" CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS"
 
-../configure $OPTIONS CC="$CC" CXX="$CXX" MACOS_DEPLOYMENT_TARGET=10.4 || exit 1
+../configure $OPTIONS CC="$CC" CXX="$CXX" CPP="$CPP" CXXCPP="$CXXCPP" LDFLAGS="$LDFLAGS" CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" MACOS_DEPLOYMENT_TARGET=10.4 || exit 1
 
-make package $MAKEFLAGS
+make
+make install-strip
+
+#make package $MAKEFLAGS
 
 #make installer
 
