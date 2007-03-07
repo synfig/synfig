6ee1233ccd00e071d9a1b1f305d980ed91bd1939
dooglus <dooglus@1f10aa63-cdf2-0310-b900-c93c546f37ac>
2007-03-07 00:29:08 +0000

    The 'polygon', 'draw', 'sketch', and 'width' tools are disabled by default.  This change allows the user to re-enable them by setting the appropriate environment variable(s):
      export SYNFIG_ENABLE_POLYGON=1
      export SYNFIG_ENABLE_DRAW=1
      export SYNFIG_ENABLE_SKETCH=1
      export SYNFIG_ENABLE_WIDTH=1
    
    
    git-svn-id: http://svn.voria.com/code@267 1f10aa63-cdf2-0310-b900-c93c546f37ac

--- synfig-studio/trunk/src/gtkmm/app.cpp
+++ synfig-studio/trunk/src/gtkmm/app.cpp
@@ -1224,17 +1224,15 @@ App::App(int *argc, char ***argv):
 		
 		state_manager->add_state(&state_zoom);
 
-		if(false) {
-			// Disabled this tool because it should be
-			// considered deprecated. Use the bline tool instead.
-			state_manager->add_state(&state_polygon);
+		// Disabled this tool because it should be
+		// considered deprecated. Use the bline tool instead.
+		if(getenv("SYNFIG_ENABLE_POLYGON")) state_manager->add_state(&state_polygon);
 	
-			// These tools are disabled for now,
-			// because they tend to confuse users.
-			state_manager->add_state(&state_draw);
-			state_manager->add_state(&state_sketch);
-			state_manager->add_state(&state_width);
-		}
+		// These tools are disabled by default for now,
+		// because they tend to confuse users.
+		if(getenv("SYNFIG_ENABLE_DRAW"  )) state_manager->add_state(&state_draw);
+		if(getenv("SYNFIG_ENABLE_SKETCH")) state_manager->add_state(&state_sketch);
+		if(getenv("SYNFIG_ENABLE_WIDTH" )) state_manager->add_state(&state_width);
 
 		studio_init_cb.task("Init ModPalette...");
 		module_list_.push_back(new ModPalette()); module_list_.back()->start();
