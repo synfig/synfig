484e17b2ae8d29dc5142cc0a1dbfbdb514c297f4
dooglus <dooglus@1f10aa63-cdf2-0310-b900-c93c546f37ac>
2007-04-18 09:45:40 +0000

    Partially fix 1702760: stop compiler warning about trgt_png.cpp.
    git-svn-id: http://svn.voria.com/code@474 1f10aa63-cdf2-0310-b900-c93c546f37ac

--- synfig-core/trunk/src/modules/mod_png/trgt_png.cpp
+++ synfig-core/trunk/src/modules/mod_png/trgt_png.cpp
@@ -203,10 +203,14 @@ png_trgt::start_frame(synfig::ProgressCallback *callback)
 	// Output any text info along with the file
 	png_text comments[]=
 	{
-		{ PNG_TEXT_COMPRESSION_NONE, "Title", const_cast<char *>(get_canvas()->get_name().c_str()) },
-		{ PNG_TEXT_COMPRESSION_NONE, "Description", const_cast<char *>(get_canvas()->get_description().c_str()) },
-//		{ PNG_TEXT_COMPRESSION_NONE, "Copyright", "(c) 2004 Voria Studios, LLC" },
-		{ PNG_TEXT_COMPRESSION_NONE, "Software", "SYNFIG" },
+		{ PNG_TEXT_COMPRESSION_NONE, "Title", const_cast<char *>(get_canvas()->get_name().c_str()),
+		  strlen(get_canvas()->get_name().c_str()) },
+		{ PNG_TEXT_COMPRESSION_NONE, "Description", const_cast<char *>(get_canvas()->get_description().c_str()),
+		  strlen(get_canvas()->get_description().c_str()) },
+//		{ PNG_TEXT_COMPRESSION_NONE, "Copyright", "(c) 2004 Voria Studios, LLC",
+//		  strlen("(c) 2004 Voria Studios, LLC") },
+		{ PNG_TEXT_COMPRESSION_NONE, "Software", "SYNFIG",
+		  strlen("SYNFIG") },
 	};
 	png_set_text(png_ptr,info_ptr,comments,sizeof(comments)/sizeof(png_text));
 
