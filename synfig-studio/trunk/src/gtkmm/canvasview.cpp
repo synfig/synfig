0cf1eb982fba77837eebba7c42cf8d92907a5f8b
dooglus <dooglus@1f10aa63-cdf2-0310-b900-c93c546f37ac>
2007-10-04 22:20:47 +0000

    Fix r841.  Previously it was setting the menu to quality 10, but using parameteric rendering.
    git-svn-id: http://svn.voria.com/code@842 1f10aa63-cdf2-0310-b900-c93c546f37ac

--- synfig-studio/trunk/src/gtkmm/canvasview.cpp
+++ synfig-studio/trunk/src/gtkmm/canvasview.cpp
@@ -1265,7 +1265,10 @@ CanvasView::init_menus()
 		{
 			Glib::RefPtr<Gtk::RadioAction> action(Gtk::RadioAction::create(quality_group,strprintf("quality-%02d",i), strprintf("Set Quality to %d",i)));
 			if(i==10)
+			{
 				action->set_active();
+				work_area->set_quality(i);
+			}
 			action_group->add( action,
 				sigc::bind(
 					sigc::mem_fun(*work_area, &studio::WorkArea::set_quality),
