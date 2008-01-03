b46ffd205b49c1a05a71c4efb0d337e685a2fe26
dooglus <dooglus@1f10aa63-cdf2-0310-b900-c93c546f37ac>
2008-01-03 13:48:56 +0000

    Use translated versions of the layers' local names and categories, like we do for built-in layers.
    git-svn-id: http://svn.voria.com/code@1263 1f10aa63-cdf2-0310-b900-c93c546f37ac

--- synfig-core/trunk/src/synfig/module.h
+++ synfig-core/trunk/src/synfig/module.h
@@ -89,7 +89,7 @@
 #define BEGIN_LAYERS {
 
 //! DEPRECATED - use #INCLUDE_LAYER(class)
-#define LAYER(class)  	synfig::Layer::register_in_book(synfig::Layer::BookEntry(class::create,class::name__,class::local_name__,class::category__,class::cvs_id__,class::version__));
+#define LAYER(class)  	synfig::Layer::register_in_book(synfig::Layer::BookEntry(class::create,class::name__,dgettext("synfig",class::local_name__),dgettext("synfig",class::category__),class::cvs_id__,class::version__));
 //#define LAYER(x) synfig::Layer::book()[synfig::String(x::name__)]=x::create;
 #define LAYER_ALIAS(class,alias)  	synfig::Layer::register_in_book(synfig::Layer::BookEntry(class::create,alias,alias,_("Do Not Use"),class::cvs_id__,class::version__));
 
