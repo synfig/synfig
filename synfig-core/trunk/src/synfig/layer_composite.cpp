a39876ecfa56805d2b780ebccaae2e8a4577efbc
dooglus <dooglus@1f10aa63-cdf2-0310-b900-c93c546f37ac>
2008-01-25 14:14:24 +0000

    Avoid warning.
    git-svn-id: http://svn.voria.com/code@1473 1f10aa63-cdf2-0310-b900-c93c546f37ac

--- synfig-core/trunk/src/synfig/layer_composite.cpp
+++ synfig-core/trunk/src/synfig/layer_composite.cpp
@@ -172,6 +172,7 @@ Layer_Composite::set_param(const String & param, const ValueBase &value)
 				String version(canvas->get_version());
 
 				if (version == "0.1" || version == "0.2")
+				{
 					if (get_name() == "PasteCanvas")
 						warning("loaded a version %s canvas with a 'Straight' blended PasteCanvas (%s) - check it renders OK",
 								version.c_str(), get_non_empty_description().c_str());
@@ -186,6 +187,7 @@ Layer_Composite::set_param(const String & param, const ValueBase &value)
 						if (transparent_color_)
 							set_param("color", get_param("color"));
 					}
+				}
 			}
 		}
 	}
