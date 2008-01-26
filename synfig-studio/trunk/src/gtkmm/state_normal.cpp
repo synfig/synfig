aec0d83591b1f69e0f227b36ef45069908e98b72
dooglus <dooglus@1f10aa63-cdf2-0310-b900-c93c546f37ac>
2008-01-26 11:44:39 +0000

    Disable the "Entered Normal State" message.
    git-svn-id: http://svn.voria.com/code@1499 1f10aa63-cdf2-0310-b900-c93c546f37ac

--- synfig-studio/trunk/src/gtkmm/state_normal.cpp
+++ synfig-studio/trunk/src/gtkmm/state_normal.cpp
@@ -121,7 +121,7 @@ StateNormal::~StateNormal()
 StateNormal_Context::StateNormal_Context(CanvasView *canvas_view):
 	canvas_view(canvas_view)
 {
-	synfig::info("Entered Normal State");
+	// synfig::info("Entered Normal State");
 }
 
 StateNormal_Context::~StateNormal_Context()
