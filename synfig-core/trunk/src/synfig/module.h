1db43ade922934813333bf62b3bda90a793a4170
dooglus <dooglus@1f10aa63-cdf2-0310-b900-c93c546f37ac>
2008-02-10 09:13:44 +0000

    Reformat a couple of macros.
    git-svn-id: http://svn.voria.com/code@1647 1f10aa63-cdf2-0310-b900-c93c546f37ac

--- synfig-core/trunk/src/synfig/module.h
+++ synfig-core/trunk/src/synfig/module.h
@@ -91,9 +91,23 @@
 #define BEGIN_LAYERS {
 
 //! DEPRECATED - use #INCLUDE_LAYER(class)
-#define LAYER(class)  	synfig::Layer::register_in_book(synfig::Layer::BookEntry(class::create,class::name__,dgettext("synfig",class::local_name__),dgettext("synfig",class::category__),class::cvs_id__,class::version__));
-//#define LAYER(x) synfig::Layer::book()[synfig::String(x::name__)]=x::create;
-#define LAYER_ALIAS(class,alias)  	synfig::Layer::register_in_book(synfig::Layer::BookEntry(class::create,alias,alias,CATEGORY_DO_NOT_USE,class::cvs_id__,class::version__));
+#define LAYER(class)																			\
+	synfig::Layer::register_in_book(															\
+		synfig::Layer::BookEntry(class::create,													\
+								 class::name__,													\
+								 dgettext("synfig", class::local_name__),						\
+								 dgettext("synfig", class::category__),							\
+								 class::cvs_id__,												\
+								 class::version__));
+
+#define LAYER_ALIAS(class,alias)																\
+	synfig::Layer::register_in_book(															\
+		synfig::Layer::BookEntry(class::create,													\
+								 alias,															\
+								 alias,															\
+								 CATEGORY_DO_NOT_USE,											\
+								 class::cvs_id__,												\
+								 class::version__));
 
 //! Marks the end of the layers in the module's inventory
 #define END_LAYERS }
