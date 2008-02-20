711a59730f72759a5b2cdb004d602a3f2eace56e
dooglus <dooglus@1f10aa63-cdf2-0310-b900-c93c546f37ac>
2008-02-20 01:32:10 +0000

    Uncomment the code that gets add_button() to set the tooltip.  The function only seems to still be used for the 'Meta' panel anyway.
    git-svn-id: http://svn.voria.com/code@1777 1f10aa63-cdf2-0310-b900-c93c546f37ac

--- synfig-studio/trunk/src/gtkmm/dockable.cpp
+++ synfig-studio/trunk/src/gtkmm/dockable.cpp
@@ -328,7 +328,7 @@ Dockable::add_button(const Gtk::StockID& stock_id, const synfig::String& tooltip
 	//ret->set_relief(Gtk::RELIEF_HALF);
 	//ret->set_relief(Gtk::RELIEF_NONE);
 	ret->set_label(tooltip);
-	//toolbar_->get_tooltips_object()->set_tip(*ret,tooltip);
+	toolbar_->get_tooltips_object()->set_tip(*ret,tooltip);
 
 	ret->show();
 	//icon->show();
