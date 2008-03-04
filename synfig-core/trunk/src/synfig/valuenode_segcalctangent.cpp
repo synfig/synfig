bb1dc53ed239937ec79467d2a30d5d933287f4bb
dooglus <dooglus@1f10aa63-cdf2-0310-b900-c93c546f37ac>
2008-03-04 14:18:23 +0000

    Same as previous commit, but for 'segment tangent' instead of 'bline tangent'.  Select the 'segment' layer in http://dooglus.rincevent.net/random/bline-and-segment-tangents.sifz and see how the two tangents are different lengths without this revision.
    git-svn-id: http://svn.voria.com/code@1855 1f10aa63-cdf2-0310-b900-c93c546f37ac

--- synfig-core/trunk/src/synfig/valuenode_segcalctangent.cpp
+++ synfig-core/trunk/src/synfig/valuenode_segcalctangent.cpp
@@ -82,7 +82,7 @@ ValueNode_SegCalcTangent::operator()(Time t)const
 	Segment segment((*segment_)(t).get(Segment()));
 
 	etl::hermite<Vector> curve(segment.p1,segment.p2,segment.t1,segment.t2);
-	etl::derivative< etl::hermite<Vector> > deriv(curve);
+	etl::derivative< etl::hermite<Vector> > deriv(curve, 0.0001);
 
 	return deriv((*amount_)(t).get(Real()))*(0.5);
 }
