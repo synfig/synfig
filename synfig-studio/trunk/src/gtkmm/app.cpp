4480346f1b1db4bf75a3ecbc9cfa320decb4b4b3
dooglus <dooglus@1f10aa63-cdf2-0310-b900-c93c546f37ac>
2008-04-10 14:30:29 +0000

    Alt-D = draw, Alt-W = width.  Previously there were 2 different definitions for Alt-T.
    git-svn-id: http://svn.voria.com/code@2009 1f10aa63-cdf2-0310-b900-c93c546f37ac

--- synfig-studio/trunk/src/gtkmm/app.cpp
+++ synfig-studio/trunk/src/gtkmm/app.cpp
@@ -1018,9 +1018,9 @@ init_ui_manager()
 	ACCEL("<Actions>//state-eyedrop",     "<Mod1>e");
 	ACCEL("<Actions>//state-zoom",        "<Mod1>z");
 
-	ACCEL("<Actions>//state-draw",        "<Mod1>w");
+	ACCEL("<Actions>//state-draw",        "<Mod1>d");
 	ACCEL("<Actions>//state-sketch",      "<Mod1>k");
-	ACCEL("<Actions>//state-width",       "<Mod1>t");
+	ACCEL("<Actions>//state-width",       "<Mod1>w");
 
 	ACCEL("<Actions>//canvas-zoom-fit","<Control><Shift>z");
 
