2d1ee9b0b0951b7bfb77b92f4fdd7e69d84c8e9e
genete <genete@1f10aa63-cdf2-0310-b900-c93c546f37ac>
2008-10-13 23:13:07 +0000

    Update/add the copyright for poeple who modified synfig-core code.
    git-svn-id: https://synfig.svn.sourceforge.net/svnroot/synfig@2095 1f10aa63-cdf2-0310-b900-c93c546f37ac

--- synfig-core/trunk/src/modules/mod_libavcodec/trgt_av.cpp
+++ synfig-core/trunk/src/modules/mod_libavcodec/trgt_av.cpp
@@ -6,6 +6,8 @@
 **
 **	\legal
 **	Copyright (c) 2002-2005 Robert B. Quattlebaum Jr., Adrian Bentley
+**  Copyright (c) 2008 Paul Wise
+**  Copyright (c) 2008 Gerco Ballintijn
 **
 **	This package is free software; you can redistribute it and/or
 **	modify it under the terms of the GNU General Public License as
--- synfig-core/trunk/src/synfig/time.cpp
+++ synfig-core/trunk/src/synfig/time.cpp
@@ -7,6 +7,8 @@
 **	\legal
 **	Copyright (c) 2002-2005 Robert B. Quattlebaum Jr., Adrian Bentley
 **	Copyright (c) 2007, 2008 Chris Moore
+**  Copyright (c) 2008 Gerco Ballintijn
+**  Copyright (c) 2008 Carlos López
 **
 **	This package is free software; you can redistribute it and/or
 **	modify it under the terms of the GNU General Public License as
