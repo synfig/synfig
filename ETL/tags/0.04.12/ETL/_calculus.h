d953ebc0f88bcc2533ed89a2800283f529a57620
genete <genete@1f10aa63-cdf2-0310-b900-c93c546f37ac>
2008-10-21 21:24:37 +0000

    Release 0.04.12
    git-svn-id: https://synfig.svn.sourceforge.net/svnroot/synfig@2127 1f10aa63-cdf2-0310-b900-c93c546f37ac

---
+++ ETL/tags/0.04.12/ETL/_calculus.h
@@ -0,0 +1,102 @@
+/*! ========================================================================
+** Extended Template and Library
+** Calculus Functional Classes Implementation
+** $Id$
+**
+** Copyright (c) 2002 Robert B. Quattlebaum Jr.
+** Copyright (c) 2008 Chris Moore
+**
+** This package is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public License as
+** published by the Free Software Foundation; either version 2 of
+** the License, or (at your option) any later version.
+**
+** This package is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+** General Public License for more details.
+**
+** === N O T E S ===========================================================
+**
+** ========================================================================= */
+
+/* === S T A R T =========================================================== */
+
+#ifndef __ETL__CALCULUS_H
+#define __ETL__CALCULUS_H
+
+/* === H E A D E R S ======================================================= */
+
+#include <functional>
+
+#include "hermite"
+
+/* === M A C R O S ========================================================= */
+
+//#ifndef _EPSILON
+//#define _EPSILON		0.0000001
+//#endif
+
+/* === T Y P E D E F S ===================================================== */
+
+/* === C L A S S E S & S T R U C T S ======================================= */
+
+_ETL_BEGIN_NAMESPACE
+
+template <typename T>
+class derivative : public std::unary_function<typename T::argument_type,typename T::result_type>
+{
+	T func;
+	typename T::argument_type epsilon;
+public:
+	explicit derivative(const T &x, const typename T::argument_type &epsilon=0.000001):func(x),epsilon(epsilon) { }
+
+	typename T::result_type
+	operator()(const typename T::argument_type &x)const
+	{
+		return (func(x+epsilon)-func(x))/epsilon;
+	}
+};
+
+template <typename T>
+class derivative<hermite<T> > : public std::unary_function<typename hermite<T>::argument_type,typename hermite<T>::result_type>
+{
+	hermite<T> func;
+public:
+	explicit derivative(const hermite<T> &x):func(x) { }
+
+	typename hermite<T>::result_type
+	operator()(const typename hermite<T>::argument_type &x)const
+	{
+		T a = func[0], b = func[1], c = func[2], d = func[3];
+		typename hermite<T>::argument_type y(1-x);
+		return ((b-a)*y*y + (c-b)*x*y*2 + (d-c)*x*x) * 3;
+	}
+};
+
+template <typename T>
+class integral : public std::binary_function<typename T::argument_type,typename T::argument_type,typename T::result_type>
+{
+	T func;
+	int samples;
+public:
+	explicit integral(const T &x, const int &samples=500):func(x),samples(samples) { }
+
+	typename T::result_type
+	operator()(typename T::argument_type x,typename T::argument_type y)const
+	{
+		typename T::result_type ret=0;
+		int i=samples;
+		const typename T::argument_type increment=(y-x)/i;
+
+		for(;i;i--,x+=increment)
+			ret+=(func(x)+func(x+increment))*increment/2;
+		return ret;
+	}
+};
+
+_ETL_END_NAMESPACE
+
+/* === E N D =============================================================== */
+
+#endif
---
+++ ETL/tags/0.04.12/ETL/_misc.h
@@ -0,0 +1,103 @@
+/*! ========================================================================
+** Extended Template and Library
+** Misc
+** $Id$
+**
+** Copyright (c) 2002 Robert B. Quattlebaum Jr.
+** Copyright (c) 2007 Chris Moore
+**
+** This package is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public License as
+** published by the Free Software Foundation; either version 2 of
+** the License, or (at your option) any later version.
+**
+** This package is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+** General Public License for more details.
+**
+** === N O T E S ===========================================================
+**
+** ========================================================================= */
+
+/* === S T A R T =========================================================== */
+
+#ifndef __ETL__MISC_H_
+#define __ETL__MISC_H_
+
+/* === H E A D E R S ======================================================= */
+#include <cmath>
+
+#include <math.h>
+
+/* === M A C R O S ========================================================= */
+
+/* === T Y P E D E F S ===================================================== */
+
+/* === C L A S S E S & S T R U C T S ======================================= */
+
+_ETL_BEGIN_NAMESPACE
+
+template<typename I, typename T> inline I
+binary_find(I begin, I end, const T& value)
+{
+#if 1
+	I iter(begin+(end-begin)/2);
+
+	while(end-begin>1 && !(*iter==value))
+	{
+		((*iter<value)?begin:end) = iter;
+
+		iter = begin+(end-begin)/2;
+	}
+	return iter;
+#else
+	size_t len_(end-begin);
+	size_t half_(len_/2);
+
+	I iter(begin);
+	iter+=half_;
+
+	while(len_>1 && !(*iter==value))
+	{
+		((*iter<value)?begin:end) = iter;
+
+		len_=half_;
+		half_/=2;
+
+		iter=begin;
+		iter+=half_;
+	}
+	return iter;
+#endif
+}
+
+inline int round_to_int(const float x) {
+	/*!	\todo Isn't there some x86 FPU instruction for quickly
+	**	converting a float to a rounded integer? It's worth
+	**	looking into at some point... */
+	// return static_cast<int>(x+0.5f);			// <-- (a) fast, but rounds -1.333 to 0!
+	// return static_cast<int>(rintf(x));		// <-- (b) slow, but correct
+    if (x>=0) return static_cast<int>(x + 0.5);	// <-- slower than (a), but correct, and faster than (b)
+    else      return static_cast<int>(x - 0.5);
+}
+inline int round_to_int(const double x) {
+	// return static_cast<int>(x+0.5);
+	// return static_cast<int>(rint(x));
+	if (x>=0) return static_cast<int>(x + 0.5);
+    else      return static_cast<int>(x - 0.5);
+}
+
+inline int ceil_to_int(const float x) { return static_cast<int>(ceil(x)); }
+inline int ceil_to_int(const double x) { return static_cast<int>(ceil(x)); }
+
+inline int floor_to_int(const float x) { return static_cast<int>(x); }
+inline int floor_to_int(const double x) { return static_cast<int>(x); }
+
+_ETL_END_NAMESPACE
+
+/* === E X T E R N S ======================================================= */
+
+/* === E N D =============================================================== */
+
+#endif
