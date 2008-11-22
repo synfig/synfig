d5492843531fb8aad372df09f39c2c0ec3f6b27d
dooglus <dooglus@1f10aa63-cdf2-0310-b900-c93c546f37ac>
2008-11-22 15:35:33 +0000

    Put the three example layers in the 'Example' group in the 'new layer' menu.
    git-svn-id: https://synfig.svn.sourceforge.net/svnroot/synfig@2240 1f10aa63-cdf2-0310-b900-c93c546f37ac

--- synfig-core/trunk/src/modules/example/metaballs.cpp
+++ synfig-core/trunk/src/modules/example/metaballs.cpp
@@ -54,7 +54,7 @@ using namespace synfig;
 SYNFIG_LAYER_INIT(Metaballs);
 SYNFIG_LAYER_SET_NAME(Metaballs,"metaballs");
 SYNFIG_LAYER_SET_LOCAL_NAME(Metaballs,N_("Metaballs"));
-SYNFIG_LAYER_SET_CATEGORY(Metaballs,N_("Default"));
+SYNFIG_LAYER_SET_CATEGORY(Metaballs,N_("Example"));
 SYNFIG_LAYER_SET_VERSION(Metaballs,"0.1");
 SYNFIG_LAYER_SET_CVS_ID(Metaballs,"$Id$");
 
--- synfig-core/trunk/src/modules/example/simplecircle.cpp
+++ synfig-core/trunk/src/modules/example/simplecircle.cpp
@@ -54,7 +54,7 @@ using namespace synfig;
 SYNFIG_LAYER_INIT(SimpleCircle);
 SYNFIG_LAYER_SET_NAME(SimpleCircle,"simple_circle");
 SYNFIG_LAYER_SET_LOCAL_NAME(SimpleCircle,N_("Simple Circle"));
-SYNFIG_LAYER_SET_CATEGORY(SimpleCircle,CATEGORY_DO_NOT_USE);
+SYNFIG_LAYER_SET_CATEGORY(SimpleCircle,N_("Example"));
 SYNFIG_LAYER_SET_VERSION(SimpleCircle,"0.1");
 SYNFIG_LAYER_SET_CVS_ID(SimpleCircle,"$Id$");
 
