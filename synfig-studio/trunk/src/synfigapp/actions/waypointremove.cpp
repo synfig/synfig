synfig/synfig

commit 9d93930f1448d65494b8b4625635f7d14e678804

Fix 2256600: "Delete all waypoints of a valuenode unexports it".  When deleting the last waypoint of a valuenode, copy the exported name of the valuenode to the value of the last waypoint before replacing the valuenode with the waypoint's value.
git-svn-id: https://synfig.svn.sourceforge.net/svnroot/synfig@2205 1f10aa63-cdf2-0310-b900-c93c546f37ac

synfig-studio/trunk/src/synfigapp/actions/waypointremove.cpp
@@ -144,6 +144,10 @@ Action::WaypointRemove::perform()
 		value_node->replace(value_node_ref);
 		value_node->waypoint_list().clear();
 
+		// fix 2256600 : deleting the last waypoint of an exported valuenode unexported it
+		// if the waypoint's value isn't exported, set its id to be the id of the parent node
+		if (value_node_ref->get_id() == "") value_node_ref->set_id(value_node->get_id());
+
 		if(get_canvas_interface())
 		{
 			get_canvas_interface()->signal_value_node_replaced()(value_node,value_node_ref);

commit 1997822b3fc97e99a787c54d80c0a721ac750109

Fix 2321845: "Save after delete a keyframe crash in a particular file".  This bug was introduced by r2205.  Use canvas->add_value_node() to export values rather than setting the id explicitly.
git-svn-id: https://synfig.svn.sourceforge.net/svnroot/synfig@2260 1f10aa63-cdf2-0310-b900-c93c546f37ac

synfig-studio/trunk/src/synfigapp/actions/waypointremove.cpp
@@ -141,12 +141,18 @@ Action::WaypointRemove::perform()
 				throw Error(_("Unable to create ValueNode_Reference"));
 		}
 
+		// fix 2256600 (and 2321845) : deleting the last waypoint of an exported valuenode unexported it
+		// if the waypoint's value isn't exported, set its id to be the id of the parent node
+		if (value_node_ref->get_id() == "" && value_node->get_id() != "")
+		{
+			const String id(value_node->get_id());
+			Canvas::LooseHandle canvas(value_node->get_parent_canvas());
+			canvas->remove_value_node(value_node);
+			canvas->add_value_node(value_node_ref, id);
+		}
+
 		value_node->replace(value_node_ref);
 		value_node->waypoint_list().clear();
-
-		// fix 2256600 : deleting the last waypoint of an exported valuenode unexported it
-		// if the waypoint's value isn't exported, set its id to be the id of the parent node
-		if (value_node_ref->get_id() == "") value_node_ref->set_id(value_node->get_id());
 
 		if(get_canvas_interface())
 		{
@@ -164,6 +170,14 @@ Action::WaypointRemove::undo()
 	{
 		if(value_node->waypoint_list().size()!=0)
 			throw Error(_("This animated value node should be empty, but for some reason it isn't. This is a bug. (1)"));
+
+		if (value_node->get_id() == "" && value_node_ref->get_id() != "")
+		{
+			const String id(value_node_ref->get_id());
+			Canvas::LooseHandle canvas(value_node_ref->get_parent_canvas());
+			canvas->remove_value_node(value_node_ref);
+			canvas->add_value_node(value_node, id);
+		}
 
 		value_node_ref->replace(value_node);
 