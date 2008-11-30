3f8300638fa0f7f144427d4d8d2b57b1ae257884
Chris Moore <chris@pcnuevo.(none)>
2008-11-30 20:20:45 +0100

    Improve the indentation.

--- synfig-core/trunk/src/synfig/matrix.h
+++ synfig-core/trunk/src/synfig/matrix.h
@@ -88,81 +88,95 @@ private:
 
 public:
 	//!Deafult constructor makes a identity matrix
-	Matrix() {
+	Matrix()
+	{
 		m00=1.0; m01=0.0; m02=0.0;
 		m10=0.0; m11=1.0; m12=0.0;
 		m20=0.0; m21=0.0; m22=1.0;
-		}
+	}
 
 	//!Constructor from Angle create a rotate matrix
-	Matrix(const Angle &a){
+	Matrix(const Angle &a)
+	{
 		(*this).set_rotate(a);
-		}
+	}
+
 	//!set_identity member. Set an identity matrix
 	Matrix &
-	set_identity(){
+	set_identity()
+	{
 		m00=1.0; m01=0.0; m02=0.0;
 		m10=0.0; m11=1.0; m12=0.0;
 		m20=0.0; m21=0.0; m22=1.0;
-		 return (*this);
-		 }
+		return (*this);
+	}
+
 	//!set_scale member fucntion. Sets a scale matrix
 	//! @param sx Scale by X axis
 	//! @param sy Scale by Y axis
 	//! @return A matrix reference filled with the sx, sy values
 	Matrix &
-	set_scale(const value_type &sx, const value_type &sy){
+	set_scale(const value_type &sx, const value_type &sy)
+	{
 		m00=sx;  m01=0.0; m02=0.0;
 		m10=0.0; m11=sy;  m12=0.0;
 		m20=0.0; m21=0.0; m22=1.0;
-		 return (*this);
-		}
+		return (*this);
+	}
+
 	//!set_scale member fucntion. Sets a scale matrix
 	//! @param sxy Scale by X and Y axis
 	//! @return A matrix reference filled with the sxy values
 	Matrix &
-	set_scale(const value_type &sxy){
+	set_scale(const value_type &sxy)
+	{
 		m00=sxy; m01=0.0; m02=0.0;
 		m10=0.0; m11=sxy; m12=0.0;
 		m20=0.0; m21=0.0; m22=1.0;
-		 return (*this);
-		}
+		return (*this);
+	}
+
 	//!set_rotate member function. Sets a rotate matrix
 	//! @param a Rotation angle counterclock wise
 	//! @return A matrix reference filled with the proper rotation parameters
 	Matrix &
-	set_rotate(const Angle &a){
+	set_rotate(const Angle &a)
+	{
 		value_type c(Angle::cos(a).get());
 		value_type s(Angle::sin(a).get());
 		m00= c;     m01=s;    m02=0.0;
 		m10=-1.0*s; m11=c;    m12=0.0;
 		m20=0.0;    m21=0.0;  m22=1.0;
-		 return (*this);
-		}
+		return (*this);
+	}
+
 	//!traslate member function. Sets a translate matrix
 	//! @param t Vector that defines the translation
 	//! @return A matrix reference filled with the proper translation parameters
 	Matrix &
-	set_translate(const Vector &t){
+	set_translate(const Vector &t)
+	{
 		m00=1.0;  m01=0.0;  m02=0.0;
 		m10=0.0;  m11=1.0;  m12=0.0;
 		m20=t[0]; m21=t[1]; m22=1.0;
-		 return (*this);
-		}
+		return (*this);
+	}
 
 	//!get_transformed member function.
 	//! @param v 2D Vector to transform
 	//! @return The Vector result
 	Vector
-	get_transformed(const Vector &v){
-		 return Vector(v[0]*m00+v[1]*m10+m20,v[0]*m01+v[1]*m11+m21);
-		 }
+	get_transformed(const Vector &v)
+	{
+		return Vector(v[0]*m00+v[1]*m10+m20,v[0]*m01+v[1]*m11+m21);
+	}
 
 	//! operator *. Multiplication of one matrix by other
 	//! @param rhs the right hand side of the multiplication operation
 	//! @return the resulting multiplication matrix
 	Matrix
-	operator *(const Matrix &rhs){
+	operator *(const Matrix &rhs)
+	{
 		Matrix ret;
 		ret.m00=m00*rhs.m00 + m01*rhs.m10 + m02*rhs.m20;
 		ret.m01=m00*rhs.m01 + m01*rhs.m11 + m02*rhs.m21;
@@ -175,14 +189,16 @@ public:
 		ret.m20=m20*rhs.m00 + m21*rhs.m10 + m22*rhs.m20;
 		ret.m21=m20*rhs.m01 + m21*rhs.m11 + m22*rhs.m21;
 		ret.m22=m20*rhs.m02 + m21*rhs.m12 + m22*rhs.m22;
+
 		return ret;
-		}
+	}
 
 	//! operator *=. Multiplication and assign of one matrix by a number
 	//! @param rhs the number to multiply by
 	//! @return the modifed resulting multiplicated by number matrix
 	Matrix
-	operator *=(const value_type &rhs){
+	operator *=(const value_type &rhs)
+	{
 		m00*=rhs;
 		m01*=rhs;
 		m02*=rhs;
@@ -194,6 +210,7 @@ public:
 		m20*=rhs;
 		m21*=rhs;
 		m22*=rhs;
+
 		return *this;
 	}
 
@@ -201,7 +218,8 @@ public:
 	//! @param rhs the matrix to sum
 	//! @return modified matrix with the summed matrix
 	Matrix
-	operator +=(const Matrix &rhs){
+	operator +=(const Matrix &rhs)
+	{
 		m00+=rhs.m00;
 		m01+=rhs.m01;
 		m02+=rhs.m02;
@@ -214,24 +232,25 @@ public:
 		m21+=rhs.m21;
 		m22+=rhs.m22;
 		return *this;
-		}
+	}
 
 	//! operator *. Multiplication of one matrix by a number
 	//! @param rhs the number to multiply by
 	//! @return the resulting multiplicated by number matrix
 	Matrix
-	operator *(const value_type &rhs){
-	return Matrix(*this)*=rhs;
-		}
+	operator *(const value_type &rhs)
+	{
+		return Matrix(*this)*=rhs;
+	}
 
 	//! operator +=. Sum and assign of two matrixes
 	//! @param rhs the matrix to sum
 	//! @return modified matrix with the summed matrix
 	Matrix
-	operator +(const Matrix &rhs){
-	return Matrix(*this)+=rhs;
-		}
-
+	operator +(const Matrix &rhs)
+	{
+		return Matrix(*this)+=rhs;
+	}
 };
 
 }; // END of namespace synfig
