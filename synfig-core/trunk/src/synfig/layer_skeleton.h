1c8e28628bfddac0da08f6b76196ca14294d62c4
Chris Moore <chris@pcnuevo.(none)>
2008-12-01 20:29:48 +0100

    Initial revision - it's a copy of metaballs.

---
+++ synfig-core/trunk/src/synfig/layer_skeleton.h
@@ -0,0 +1,81 @@
+/* === S Y N F I G ========================================================= */
+/*!	\file layer_skeleton.h
+**	\brief Header file for implementation of the "Layer_Skeleton" layer
+**
+**	$Id$
+**
+**	\legal
+**	Copyright (c) 2002-2005 Robert B. Quattlebaum Jr., Adrian Bentley
+**
+**	This package is free software; you can redistribute it and/or
+**	modify it under the terms of the GNU General Public License as
+**	published by the Free Software Foundation; either version 2 of
+**	the License, or (at your option) any later version.
+**
+**	This package is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+**	General Public License for more details.
+**	\endlegal
+*/
+/* ========================================================================= */
+
+/* === S T A R T =========================================================== */
+
+#ifndef __SYNFIG_LAYER_SKELETON_H
+#define __SYNFIG_LAYER_SKELETON_H
+
+/* === H E A D E R S ======================================================= */
+
+#include <synfig/layer_composite.h>
+#include <synfig/gradient.h>
+#include <synfig/vector.h>
+#include <synfig/value.h>
+#include <vector>
+
+/* === M A C R O S ========================================================= */
+
+/* === T Y P E D E F S ===================================================== */
+
+/* === C L A S S E S & S T R U C T S ======================================= */
+class Layer_Skeleton : public synfig::Layer_Composite
+{
+	SYNFIG_LAYER_MODULE_EXT
+
+private:
+
+	synfig::Gradient gradient;
+
+	std::vector<synfig::Point>	centers;
+	std::vector<synfig::Real>	radii;
+	std::vector<synfig::Real>	weights;
+
+	synfig::Real	threshold;
+	synfig::Real	threshold2;
+
+	bool positive;
+
+	synfig::Real densityfunc(const synfig::Point &p, const synfig::Point &c, synfig::Real R)const;
+
+	synfig::Real totaldensity(const synfig::Point &pos)const;
+
+public:
+
+	Layer_Skeleton();
+
+	virtual bool set_param(const synfig::String & param, const synfig::ValueBase &value);
+
+	virtual synfig::ValueBase get_param(const synfig::String & param)const;
+
+	virtual synfig::Color get_color(synfig::Context context, const synfig::Point &pos)const;
+
+	virtual bool accelerated_render(synfig::Context context,synfig::Surface *surface,int quality, const synfig::RendDesc &renddesc, synfig::ProgressCallback *cb)const;
+
+	virtual Vocab get_param_vocab()const;
+
+	virtual synfig::Layer::Handle hit_check(synfig::Context context, const synfig::Point &point)const;
+}; // END of class Layer_Skeleton
+
+/* === E N D =============================================================== */
+
+#endif
