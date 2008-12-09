f97e1206aaec49b0a949b095e01bfc0da1c2f7bb
Chris Moore <chris@pcnuevo.(none)>
2008-12-10 00:59:43 +0100

    Don't auto-save if SYNFIG_DISABLE_AUTO_SAVE is set.

--- synfig-studio/trunk/src/gtkmm/autorecover.cpp
+++ synfig-studio/trunk/src/gtkmm/autorecover.cpp
@@ -124,7 +124,7 @@ AutoRecover::set_timeout(int milliseconds)
 {
 	timeout=milliseconds;
 	auto_backup_connect.disconnect();
-	if(timeout)
+	if(timeout && !getenv("SYNFIG_DISABLE_AUTO_SAVE"))
 		auto_backup_connect=Glib::signal_timeout().connect(sigc::ptr_fun(&AutoRecover::auto_backup),timeout);
 //		auto_backup_connect=App::main.get_context()->signal_timeout().connect(sigc::mem_fun(&AutoRecover::auto_backup),timeout);
 }
