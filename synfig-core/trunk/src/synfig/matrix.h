dd1b68c32c57b94f3a524468cb75333e25b604cc
Carlos Lopez <carlos@pcnuevo.(none)>
2008-12-13 01:19:04 +0100

    Remove Darco and Adrian copyrights and add dooglus one

--- synfig-core/trunk/src/synfig/matrix.h
+++ synfig-core/trunk/src/synfig/matrix.h
@@ -5,8 +5,7 @@
 **	$Id$
 **
 **	\legal
-**	Copyright (c) 2002-2005 Robert B. Quattlebaum Jr., Adrian Bentley
-**	Copyright (c) 2008 Carlos López
+**	Copyright (c) 2008 Carlos López & Chirs Moore
 **
 **	This package is free software; you can redistribute it and/or
 **	modify it under the terms of the GNU General Public License as
