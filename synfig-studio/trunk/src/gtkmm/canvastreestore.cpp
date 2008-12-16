a19824c567d6d4885227f16c07948d23eed915cd
Chris Moore <chris@pcnuevo.(none)>
2008-12-16 10:32:47 +0100

    Fix #18.

--- synfig-studio/trunk/src/gtkmm/canvastreestore.cpp
+++ synfig-studio/trunk/src/gtkmm/canvastreestore.cpp
@@ -79,6 +79,17 @@ CanvasTreeStore::~CanvasTreeStore()
 {
 }
 
+ValueNode::Handle
+CanvasTreeStore::expandable_bone_parent(ValueNode::Handle node)
+{
+	if ((!getenv("SYNFIG_DISABLE_EXPANDABLE_BONE_PARENTS")) &&
+		node->get_type() == ValueBase::TYPE_VALUENODE_BONE &&
+		(node->get_name() == "constant" || node->get_name() == "animated"))
+		if (ValueNode::Handle bone_node = (*node)(canvas_interface()->get_time()).get(ValueNode_Bone::Handle()))
+			return bone_node;
+	return node;
+}
+
 void
 CanvasTreeStore::get_value_vfunc(const Gtk::TreeModel::iterator& iter, int column, Glib::ValueBase& value)const
 {
@@ -364,14 +375,18 @@ CanvasTreeStore::find_next_value_desc(const synfigapp::ValueDesc& value_desc, Gt
 bool
 CanvasTreeStore::find_first_value_node(const synfig::ValueNode::Handle& value_node, Gtk::TreeIter& iter)
 {
+	// maybe replace the ValueNode_Const or ValueNode_Animated with the contained ValueNode_Bone
+	// todo: do we need to do this in find_next_value_node, and find_*_value_desc too?
+	synfig::ValueNode::Handle node(expandable_bone_parent(value_node));
+
 	iter=children().begin();
-	while(iter && value_node!=(ValueNode::Handle)(*iter)[model.value_node])
+	while(iter && node!=(ValueNode::Handle)(*iter)[model.value_node])
 	{
 		if(!iter->children().empty())
 		{
 			Gtk::TreeIter iter2(iter->children().begin());
 			//! \todo confirm that the && should be done before the ||
-			if((iter2 && value_node==(ValueNode::Handle)(*iter2)[model.value_node]) || find_next_value_node(value_node, iter2))
+			if((iter2 && node==(ValueNode::Handle)(*iter2)[model.value_node]) || find_next_value_node(node, iter2))
 			{
 				iter=iter2;
 				return true;
@@ -383,7 +398,7 @@ CanvasTreeStore::find_first_value_node(const synfig::ValueNode::Handle& value_no
 		else
 			iter=iter2;
 	}
-	return (bool)iter && value_node==(ValueNode::Handle)(*iter)[model.value_node];
+	return (bool)iter && node==(ValueNode::Handle)(*iter)[model.value_node];
 }
 
 bool
@@ -432,11 +447,9 @@ CanvasTreeStore::set_row(Gtk::TreeRow row,synfigapp::ValueDesc value_desc, bool 
 
 			// todo: if the parent is animated and expanded, and we drag the time slider so that it changes,
 			// it's not updated.  it still shows the previous bone valuenode.
-			if (!getenv("SYNFIG_DISABLE_EXPANDABLE_BONE_PARENTS") &&
-				value_node->get_type() == ValueBase::TYPE_VALUENODE_BONE &&
-				(value_node->get_name() == "constant" || value_node->get_name() == "animated"))
-				if (ValueNode::Handle bone_node = (*value_node)(canvas_interface()->get_time()).get(ValueNode_Bone::Handle()))
-					value_node = bone_node;
+
+			// maybe replace the ValueNode_Const or ValueNode_Animated with the contained ValueNode_Bone
+			value_node = expandable_bone_parent(value_node);
 
 			assert(value_node);
 
