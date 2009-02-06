fd3ba7778aeb88d16b8a6cbabea7b683b1db9c01
Uiomae <uiomae@gmail.com>
2009-02-06 02:52:54 +0000

    Add the rendered frame to the target using the appropiate method

--- synfig-core/src/synfig/target_scanline.cpp
+++ synfig-core/src/synfig/target_scanline.cpp
@@ -247,12 +247,33 @@ synfig::Target_Scanline::render_frame_(int quality, ProgressCallback *cb)
 			}
 			else
 			{
-				// Put the surface we renderer
-				// onto the target.
-				if(!add_frame(&surface))
-				{
-					if(cb)cb->error(_("Unable to put surface on target"));
+				// If the method requires it, copy the generated image to the surface
+				switch (method) {
+				case SOFTWARE:
+					// Put the surface we renderer
+					// onto the target.
+					if(!add_frame(&surface))
+					{
+						if(cb)cb->error(_("Unable to put surface on target"));
+						return false;
+					}
+					break;
+				case OPENGL:
+					/*
+					const unsigned char *data; = (const unsigned char*)renderer_opengl().get_data(target_format_);
+					// Put the surface we renderer
+					// onto the target.
+					if(!add_frame(data, desc.get_w(), desc.get_h()))
+					{
+						if(cb)cb->error(_("Unable to put surface on target"));
+						return false;
+					}
+					*/
 					return false;
+					break;
+				case CAIRO:
+					return false;
+					break;
 				}
 			}
 		#if USE_PIXELRENDERING_LIMIT
