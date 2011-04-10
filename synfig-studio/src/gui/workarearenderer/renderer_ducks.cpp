08794d48991de01b185d78c3a329ca116097faeb
Nikita Kitaev <nikitakit@gmail.com>
2011-04-10 15:02:59 -0700

    Add a fallback when CAIRO_OPERATOR_DIFFERENCE is not supported

--- synfig-studio/src/gui/workarearenderer/renderer_ducks.cpp
+++ synfig-studio/src/gui/workarearenderer/renderer_ducks.cpp
@@ -406,9 +406,16 @@ Renderer_Ducks::render_vfunc(
 			else
 			{
 				cr->set_source_rgb(1.0,1.0,1.0);
-				// OPERATOR_DIFFERENCE does not currently have a C++ wrapper
-				//cr->set_operator(Cairo::OPERATOR_DIFFERENCE);
+
+				// Operator difference was added in Cairo 1.9.4
+				// It currently isn't supported by Cairomm
+#if CAIRO_VERSION >= 10904
 				cairo_set_operator(cr->cobj(), CAIRO_OPERATOR_DIFFERENCE);
+#else
+				// Fallback: set color to black
+				cr->set_source_rgb(0,0,0);
+#endif
+
 			}
 
 			cr->set_line_width(1.0);
