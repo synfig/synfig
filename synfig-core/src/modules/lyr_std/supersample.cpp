3953e9ebc84d9ccc248fe6d2f3edc595554266a1
Carlos Lopez <genetita@gmail.com>
2011-06-26 18:57:23 +0200

    Don't allow supersample have zero or negative values in the height or width parameters.It only causes problems.Fixes bug 3147736

--- synfig-core/src/modules/lyr_std/supersample.cpp
+++ synfig-core/src/modules/lyr_std/supersample.cpp
@@ -73,9 +73,16 @@ SuperSample::SuperSample():width(2),height(2)
 bool
 SuperSample::set_param(const String & param, const ValueBase &value)
 {
-
-	IMPORT(width);
-	IMPORT(height);
+	IMPORT_PLUS(width,
+		if(value.get(int()) < 1) width = 1;
+		else width=value.get(int());
+		return true;
+		)
+	IMPORT_PLUS(height,
+		if(value.get(int()) < 1) height = 1;
+		else height=value.get(int());
+		return true;
+		)
 	IMPORT(scanline);
 	IMPORT(alpha_aware);
 
