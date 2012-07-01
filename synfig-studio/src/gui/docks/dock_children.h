26ba72fe04dc4695c6c987485968a895b5cbba89
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2012-07-01 22:19:44 +0700

    Fix on-close bug: Crash when file is closed and child panel is scrolled down.

--- synfig-studio/src/gui/docks/dock_children.h
+++ synfig-studio/src/gui/docks/dock_children.h
@@ -31,6 +31,7 @@
 #include <gtkmm/treeview.h>
 #include "instance.h"
 #include "docks/dock_canvasspecific.h"
+#include "trees/childrentree.h"
 
 /* === M A C R O S ========================================================= */
 
@@ -42,6 +43,7 @@ namespace studio {
 
 class Dock_Children : public Dock_CanvasSpecific
 {
+	ChildrenTree *tree_view;
 protected:
 	virtual void init_canvas_view_vfunc(etl::loose_handle<CanvasView> canvas_view);
 	virtual void changed_canvas_view_vfunc(etl::loose_handle<CanvasView> canvas_view);
