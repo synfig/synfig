515f1c9490d56cb1614c79e7a721fbfe92f92b79
Carlos Lopez <genetita@gmail.com>
2012-08-21 18:38:39 +0200

    Zoom layer: add support for Cairo render.

--- synfig-core/src/modules/lyr_std/zoom.cpp
+++ synfig-core/src/modules/lyr_std/zoom.cpp
@@ -157,6 +157,25 @@ Zoom::accelerated_render(Context context,Surface *surface,int quality, const Ren
 	return context.accelerated_render(surface,quality,desc,cb);
 }
 
+/////
+bool
+Zoom::accelerated_cairorender(Context context,cairo_surface_t *surface,int quality, const RendDesc &renddesc, ProgressCallback *cb)const
+{
+	Vector::value_type zoomfactor=1.0/exp(amount);
+	RendDesc desc(renddesc);
+	desc.clear_flags();
+	
+    // Adjust the top_left and bottom_right points
+	// for our zoom amount
+	desc.set_tl((desc.get_tl()-center)*zoomfactor+center);
+	desc.set_br((desc.get_br()-center)*zoomfactor+center);
+	
+	// Render the scene
+	return context.accelerated_cairorender(surface,quality,desc,cb);
+}
+/////
+
+
 synfig::Rect
 Zoom::get_full_bounding_rect(synfig::Context context)const
 {
