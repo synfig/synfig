a60f1a73a383b71c841b49d588aab766e8968c5a
Carlos Lopez <genetita@gmail.com>
2012-09-16 20:57:45 +0200

    Blur_Layer: add support for Cairo render. Gaussian blur doesn't work yet. Some changes are needed in etl::gaussian

--- synfig-core/src/modules/mod_filter/blur.h
+++ synfig-core/src/modules/mod_filter/blur.h
@@ -37,7 +37,7 @@ using namespace synfig;
 using namespace std;
 using namespace etl;
 
-class Blur_Layer : public synfig::Layer_Composite
+class Blur_Layer : public Layer_Composite
 {
 	SYNFIG_LAYER_MODULE_EXT
 private:
@@ -47,13 +47,14 @@ private:
 public:
 	Blur_Layer();
 
-	virtual bool set_param(const String & param, const synfig::ValueBase &value);
+	virtual bool set_param(const String & param, const ValueBase &value);
 
 	virtual ValueBase get_param(const String & param)const;
 
 	virtual Color get_color(Context context, const Point &pos)const;
 
 	virtual bool accelerated_render(Context context,Surface *surface,int quality, const RendDesc &renddesc, ProgressCallback *cb)const;
+	virtual bool accelerated_cairorender(Context context,cairo_surface_t *surface,int quality, const RendDesc &renddesc, ProgressCallback *cb)const;
 	virtual synfig::Rect get_full_bounding_rect(Context context)const;
 
 	virtual Vocab get_param_vocab()const;
