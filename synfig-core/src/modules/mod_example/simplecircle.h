572c02c79cec3c57f405feba642e2c72688f2b3f
Carlos Lopez <genetita@gmail.com>
2013-05-14 20:22:37 +0200

    Partially fix bug #416  -  Some layers implemented only via get_color in Cairo. (II) Implemented Simple Circle

--- synfig-core/src/modules/mod_example/simplecircle.h
+++ synfig-core/src/modules/mod_example/simplecircle.h
@@ -39,31 +39,36 @@
 
 /* === C L A S S E S & S T R U C T S ======================================= */
 
-class SimpleCircle : public synfig::Layer_Composite, public synfig::Layer_NoDeform
+using namespace synfig;
+using namespace std;
+using namespace etl;
+
+class SimpleCircle : public Layer_Composite, public Layer_NoDeform
 {
 	SYNFIG_LAYER_MODULE_EXT
 
 private:
 
-	synfig::Color color;
+	Color color;
 
-	synfig::Point center;
+	Point center;
 
-	synfig::Real radius;
+	Real radius;
 
 public:
 
 	SimpleCircle();
 
-	virtual bool set_param(const synfig::String & param, const synfig::ValueBase &value);
+	virtual bool set_param(const String & param, const ValueBase &value);
 
-	virtual synfig::ValueBase get_param(const synfig::String & param)const;
+	virtual ValueBase get_param(const String & param)const;
 
-	virtual synfig::Color get_color(synfig::Context context, const synfig::Point &pos)const;
+	virtual Color get_color(Context context, const Point &pos)const;
 
-	//virtual bool accelerated_render(synfig::Context context,synfig::Surface *surface,int quality, const synfig::RendDesc &renddesc, synfig::ProgressCallback *cb)const;
+	//virtual bool accelerated_render(Context context,Surface *surface,int quality, const RendDesc &renddesc, ProgressCallback *cb)const;
+	virtual bool accelerated_cairorender(Context context, cairo_surface_t *surface,int quality, const RendDesc &renddesc, ProgressCallback *cb)const;
 
-	synfig::Layer::Handle hit_check(synfig::Context context, const synfig::Point &point)const;
+	Layer::Handle hit_check(Context context, const Point &point)const;
 
 	virtual Vocab get_param_vocab()const;
 }; // END of class SimpleCircle
