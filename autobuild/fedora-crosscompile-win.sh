84a5ba53fdc8cc84ea71c8c6464b6187181b91b4
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2013-05-28 14:54:06 +0400

    Update windows build script: add studio

--- autobuild/fedora-crosscompile-win.sh
+++ autobuild/fedora-crosscompile-win.sh
@@ -2,7 +2,7 @@
 
 export SCRIPTPATH=`dirname "$0"`
 
-yum install -y mingw32-gcc-c++ mingw32-libxml++ mingw32-cairo mingw32-pango mingw32-boost mingw32-libjpeg-turbo
+su -c yum install -y mingw32-gcc-c++ mingw32-libxml++ mingw32-cairo mingw32-pango mingw32-boost mingw32-libjpeg-turbo mingw32-gtkmm24 || true
 
 export PREFIX=~/synfig-win
 export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig:$PKG_CONFIG_PATH
@@ -10,7 +10,7 @@ export PKG_CONFIG_LIBDIR=${PREFIX}/lib/pkgconfig:$PKG_CONFIG_LIBDIR
 export PATH=${PREFIX}/bin:$PATH
 export LD_LIBRARY_PATH=${PREFIX}/lib:$LD_LIBRARY_PATH
 export LDFLAGS="-Wl,-rpath -Wl,\\\$\$ORIGIN/lib"
-	
+
 #ETL
 cd $SCRIPTPATH/../ETL
 
@@ -30,12 +30,16 @@ chmod +x ./configure
 mingw32-configure --prefix=${PREFIX} --includedir=${PREFIX}/include --disable-static --enable-shared --with-magickpp --without-libavcodec --libdir=${PREFIX}/lib --bindir=${PREFIX}/bin --sysconfdir=${PREFIX}/etc --with-boost=/usr/i686-w64-mingw32/sys-root/mingw/ --enable-warnings=minimum $DEBUG
 make install -j4
 
+
 #synfig-studio
 
 cd ../synfig-studio/
 
-#TODO
-
+/bin/sh ./bootstrap.sh
+cp ./configure ./configure.real && echo -e "#/bin/sh \n export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig:$PKG_CONFIG_PATH \n ./configure.real \$@  \n " > ./configure && \
+chmod +x ./configure
+mingw32-configure --prefix=${PREFIX} --includedir=${PREFIX}/include --disable-static --enable-shared --libdir=${PREFIX}/lib --bindir=${PREFIX}/bin --sysconfdir=${PREFIX}/etc --datadir=${PREFIX}/share  $DEBUG
+make install -j4
 
 #pack
 
