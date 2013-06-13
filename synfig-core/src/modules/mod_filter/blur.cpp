d33a97ac36ba16e8a013525c59940597c7781290
Carlos Lopez <genetita@gmail.com>
2013-06-13 19:42:38 +0200

    Create a new function cairo_renddesc_untransform and use it in Blur layer

--- synfig-core/src/modules/mod_filter/blur.cpp
+++ synfig-core/src/modules/mod_filter/blur.cpp
@@ -42,6 +42,7 @@
 #include <synfig/value.h>
 #include <synfig/valuenode.h>
 #include <synfig/segment.h>
+#include <synfig/cairo_renddesc.h>
 
 #include <cstring>
 #include <ETL/pen>
@@ -392,100 +393,12 @@ Blur_Layer::accelerated_cairorender(Context context, cairo_t *cr, int quality, c
 		stageone = SuperCallback(cb,0,9999,10000);
 		stagetwo = SuperCallback(cb,9999,10000,10000);
 	}
-	
-	const Real	pw = renddesc.get_pw(),	ph = renddesc.get_ph();
-	
-	const Point	tl(renddesc.get_tl());
-	const Point br(renddesc.get_br());
-	
-	double tl_x, tl_y, tr_x, tr_y, bl_x, bl_y, br_x, br_y;
-	double mtlx, mtly, mbrx, mbry;
-	double pminx, pminy, pmaxx, pmaxy;
-	tl_x=tl[0];
-	tl_y=tl[1];
-	br_x=br[0];
-	br_y=br[1];
-	tr_x=br_x;
-	tr_y=tl_y;
-	bl_x=tl_x;
-	bl_y=br_y;
 
 	RendDesc	workdesc(renddesc);
 	cairo_surface_t	*worksurface, *blurred;
 	
-	// In this block we are going to calculate the inversed transform of the
-	// workdesc but not applying the transformation to convert the surface to
-	// device space (See the cairo translate and scale on Target_Cairo::render and
-	// Target_Cairo_Tile::render)
-	
-	// Extract the matrix from the current context
-	cairo_matrix_t cr_matrix, cr_result;
-	cairo_get_matrix(cr, &cr_matrix);
-	
-	// Now create three matrixes with the following values:
-	// resulting matrix result=i_translate*i_scale
-	// inverse translation i_translate = inverse translation from -renddesc_tl
-	// inverse scale i_scale = inverse scale of 1/pw and 1/ph
-	
-	cairo_matrix_t i_scale, i_translate, result;
-	cairo_matrix_init_translate(&i_translate, tl[0], tl[1]);
-	cairo_matrix_init_scale(&i_scale, pw, ph);
-	
-	// Now multiply the two matrixes, the order is important!
-	// first apply scale and then rotate, the inverse than done in Target_Cairo::render
-	
-	cairo_matrix_multiply(&result, &i_scale, &i_translate);
-	
-	// Now let's multiply the cr matrix retrieved and the result matrix
-	
-	cairo_matrix_multiply(&cr_result, &cr_matrix, &result);
-	
-	// Explanation:
-	// Current cairo context matrix is this of this form:
-	// [T][S][DRAW] where the [T][S] parts corresponds to convert the cairo operations
-	// in DRAW part into the device space (usually the image surface of size w, h)
-	// DRAW matrix is the result of the layer transformations stack (rotate, zoom, etc.)
-	// But we want to transformm the render desc with the inverse of the DRAW part only,
-	// not the inverse of the T and S part because we are transforming user coordinates
-	// the renddesc and not pixels.
-	// So we retrieve the cairo context matrix: [CR]=[T][S][DRAW] and remove the [T] and
-	// [S] matrixes by applying its inverses: (the notation ' denotes inverse)
-	// [S'][T'][CR]=[S'][T'][T][S][DRAW]=[S'][I][S][DRAW]=[I][DRAW]=[DRAW] as we wanted.
-	// [M'][M]=[I] where I is the identity matrix.
-	
-	
-	// Now let's invert the result matrix, that is calculate [DRAW']
-	cairo_status_t status;
-	status=cairo_matrix_invert(&cr_result);
-	if(status) // doh! the matrix can't be inverted! I can't render the surface!
-	{
-		synfig::error("Can't invert current Cairo matrix!");
+	if(!cairo_renddesc_untransform(cr, workdesc))
 		return false;
-	}
-	
-	// Now let's tranform the renddesc corners with the calculated matrix
-	cairo_matrix_transform_point(&cr_result, &tl_x, &tl_y);
-	cairo_matrix_transform_point(&cr_result, &tr_x, &tr_y);
-	cairo_matrix_transform_point(&cr_result, &bl_x, &bl_y);
-	cairo_matrix_transform_point(&cr_result, &br_x, &br_y);
-
-	// Now let's figure out the rounding box of the transformed renddesc
-	pminx=min(min(min(tl_x, tr_x), bl_x), br_x);
-	pminy=min(min(min(tl_y, tr_y), bl_y), br_y);
-	pmaxx=max(max(max(tl_x, tr_x), bl_x), br_x);
-	pmaxy=max(max(max(tl_y, tr_y), bl_y), br_y);
-	// let's assign the right values to the meaningfull variables :)
-	mtlx=pminx;
-	mtly=pmaxy;
-	mbrx=pmaxx;
-	mbry=pminy;
-	
-	// Now apply the new tl and br values to the workdesc
-	// Before we will keep the pixel aspect and so when we modify the tl and br
-	// the width and height will be modified as well.
-	workdesc.set_flags(RendDesc::PX_ASPECT);
-	// finally apply the new desc values!
-	workdesc.set_tl_br(Point(mtlx, mtly), Point(mbrx, mbry));
 	
 	// Expand the working surface to accommodate the blur
 	//the expanded size = 1/2 the size in each direction rounded up
