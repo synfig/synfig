43ce6f94b349fca234abdc3c3b1cbd4ca45cc752
Carlos Lopez <genetita@gmail.com>
2013-06-16 19:45:35 +0200

    Layer_FreeType: add support for Cairo render using cairo_t*.

--- synfig-core/src/modules/lyr_freetype/lyr_freetype.h
+++ synfig-core/src/modules/lyr_freetype/lyr_freetype.h
@@ -148,6 +148,7 @@ public:
 	virtual Color get_color(Context context, const synfig::Point &pos)const;
 	virtual bool accelerated_render(Context context,Surface *surface,int quality, const RendDesc &renddesc, ProgressCallback *cb)const;
 	virtual bool accelerated_cairorender(Context context,cairo_surface_t *surface,int quality, const RendDesc &renddesc, ProgressCallback *cb)const;
+	virtual bool accelerated_cairorender(Context context,cairo_t *cr, int quality, const RendDesc &renddesc, ProgressCallback *cb)const;
 
 	virtual Vocab get_param_vocab()const;
 
