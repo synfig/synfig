ac84270b785e8c2492ace34d5f09799c6f1d4fab
Ivan Mahonin <bh@icystar.com>
2013-08-06 13:58:59 +0700

    New action LayerAutlineRegion, which creates new outline layer. Not creates vertices link. No GUI entry.

---
+++ synfig-studio/src/synfigapp/actions/layeroutlineregion.cpp
@@ -0,0 +1,154 @@
+/* === S Y N F I G ========================================================= */
+/*!	\file layeroutlineregion.cpp
+**	\brief Template File
+**
+**	$Id$
+**
+**	\legal
+**	......... ... 2013 Ivan Mahonin
+**
+**	This package is free software; you can redistribute it and/or
+**	modify it under the terms of the GNU General Public License as
+**	published by the Free Software Foundation; either version 2 of
+**	the License, or (at your option) any later version.
+**
+**	This package is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+**	General Public License for more details.
+**	\endlegal
+*/
+/* ========================================================================= */
+
+/* === H E A D E R S ======================================================= */
+
+#ifdef USING_PCH
+#	include "pch.h"
+#else
+#ifdef HAVE_CONFIG_H
+#	include <config.h>
+#endif
+
+#include "layeroutlineregion.h"
+#include "layeradd.h"
+#include <synfig/context.h>
+#include <synfigapp/canvasinterface.h>
+
+#include <synfigapp/general.h>
+
+#endif
+
+using namespace std;
+using namespace etl;
+using namespace synfig;
+using namespace synfigapp;
+using namespace Action;
+
+/* === M A C R O S ========================================================= */
+
+ACTION_INIT_NO_GET_LOCAL_NAME(Action::LayerOutlineRegion);
+ACTION_SET_NAME(Action::LayerOutlineRegion,"LayerOutlineRegion");
+ACTION_SET_LOCAL_NAME(Action::LayerOutlineRegion,N_("Outline Region"));
+ACTION_SET_TASK(Action::LayerOutlineRegion,"outlineregion");
+ACTION_SET_CATEGORY(Action::LayerOutlineRegion,Action::CATEGORY_LAYER);
+ACTION_SET_PRIORITY(Action::LayerOutlineRegion,0);
+ACTION_SET_VERSION(Action::LayerOutlineRegion,"0.0");
+ACTION_SET_CVS_ID(Action::LayerOutlineRegion,"$Id$");
+
+/* === G L O B A L S ======================================================= */
+
+/* === P R O C E D U R E S ================================================= */
+
+/* === M E T H O D S ======================================================= */
+
+Action::LayerOutlineRegion::LayerOutlineRegion()
+{
+}
+
+synfig::String
+Action::LayerOutlineRegion::get_local_name()const
+{
+	if (region)
+		return strprintf("%s '%s'", _("Outline Region"), region->get_non_empty_description().c_str());
+	else
+		return _("Outline Region");
+}
+
+Action::ParamVocab
+Action::LayerOutlineRegion::get_param_vocab()
+{
+	ParamVocab ret(Action::CanvasSpecific::get_param_vocab());
+
+	ret.push_back(ParamDesc("region",Param::TYPE_LAYER)
+		.set_local_name(_("Region"))
+		.set_desc(_("Region to be outlined"))
+		.set_supports_multiple()
+	);
+
+	return ret;
+}
+
+bool
+Action::LayerOutlineRegion::is_candidate(const ParamList &x)
+{
+	return candidate_check(get_param_vocab(),x);
+}
+
+bool
+Action::LayerOutlineRegion::set_param(const synfig::String& name, const Action::Param &param)
+{
+	if(name=="region" && param.get_type()==Param::TYPE_LAYER)
+	{
+		if (param.get_layer()->get_name() == "region")
+		{
+			region = param.get_layer();
+			return true;
+		}
+		else return false;
+	}
+
+	return Action::CanvasSpecific::set_param(name,param);
+}
+
+bool
+Action::LayerOutlineRegion::is_ready()const
+{
+	if(!region)
+		return false;
+	return Action::CanvasSpecific::is_ready();
+}
+
+void
+Action::LayerOutlineRegion::prepare()
+{
+	//todo: why?
+	if(!first_time())
+		return;
+
+	Canvas::Handle subcanvas(region->get_canvas());
+
+	// Find the iterator for the region
+	Canvas::iterator iter=find(subcanvas->begin(),subcanvas->end(),region);
+
+	// If we couldn't find the region in the canvas, then bail
+	if(*iter!=region)
+		throw Error(_("This region doesn't exist anymore."));
+
+	// If the subcanvas isn't the same as the canvas,
+	// then it had better be an inline canvas. If not,
+	// bail
+	if(get_canvas()!=subcanvas && !subcanvas->is_inline())
+		throw Error(_("This region doesn't belong to this canvas anymore"));
+
+	// todo: which canvas should we use?  subcanvas is the region's canvas, get_canvas() is the canvas set in the action
+	Layer::Handle outline( get_canvas_interface()->add_layer_to("outline", subcanvas, region->get_depth()) );
+	{
+		Action::Handle action(Action::create("LayerAdd"));
+
+		action->set_param("canvas",subcanvas);
+		action->set_param("canvas_interface",get_canvas_interface());
+		action->set_param("new",outline);
+
+		add_action_front(action);
+	}
+}
