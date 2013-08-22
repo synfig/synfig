c9f8c287242f666299835bfd5ee05fe4aada325c
Ivan Mahonin <bh@icystar.com>
2013-08-22 16:07:04 +0700

    Wrapper to work with files in container and files in common file system.

---
+++ synfig-core/src/synfig/filesystemgroup.h
@@ -0,0 +1,72 @@
+/* === S Y N F I G ========================================================= */
+/*!	\file filesystemgroup.h
+**	\brief FileSystemGroup
+**
+**	$Id$
+**
+**	\legal
+**	......... ... 2013 Ivan Mahonin
+**
+**	This package is free software; you can redistribute it and/or
+**	modify it under the terms of the GNU General Public License as
+**	published by the Free Software Foundation; either version 2 of
+**	the License, or (at your option) any later version.
+**
+**	This package is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+**	General Public License for more details.
+**	\endlegal
+*/
+/* ========================================================================= */
+
+/* === S T A R T =========================================================== */
+
+#ifndef __SYNFIG_FILESYSTEMGROUP_H
+#define __SYNFIG_FILESYSTEMGROUP_H
+
+/* === H E A D E R S ======================================================= */
+
+#include <list>
+#include "filesystem.h"
+
+/* === M A C R O S ========================================================= */
+
+/* === T Y P E D E F S ===================================================== */
+
+/* === C L A S S E S & S T R U C T S ======================================= */
+
+namespace synfig
+{
+
+	class FileSystemGroup : public FileSystem
+	{
+	public:
+		struct Entry
+		{
+			std::string prefix;
+			FileSystem::Handle file_system;
+			inline Entry() { }
+			inline Entry(const std::string &prefix, const Handle &file_system):
+				prefix(prefix), file_system(file_system) { }
+		};
+
+	private:
+		std::list< Entry > entries_;
+
+		bool find_system(const std::string &filename, FileSystem::Handle &out_file_system, std::string &out_filename);
+
+	public:
+		void register_system(const std::string &prefix, FileSystem::Handle file_system);
+		void unregister_system(const std::string &prefix);
+
+		virtual bool file_remove(const std::string &filename);
+		virtual ReadStreamHandle get_read_stream(const std::string &filename);
+		virtual WriteStreamHandle get_write_stream(const std::string &filename);
+	};
+
+}
+
+/* === E N D =============================================================== */
+
+#endif
---
+++ synfig-core/src/synfig/filesystemnative.cpp
@@ -0,0 +1,103 @@
+/* === S Y N F I G ========================================================= */
+/*!	\file filesystemnative.cpp
+**	\brief FileSystemNative
+**
+**	$Id$
+**
+**	\legal
+**	......... ... 2013 Ivan Mahonin
+**
+**	This package is free software; you can redistribute it and/or
+**	modify it under the terms of the GNU General Public License as
+**	published by the Free Software Foundation; either version 2 of
+**	the License, or (at your option) any later version.
+**
+**	This package is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+**	General Public License for more details.
+**	\endlegal
+*/
+/* ========================================================================= */
+
+/* === H E A D E R S ======================================================= */
+
+#ifdef USING_PCH
+#	include "pch.h"
+#else
+#ifdef HAVE_CONFIG_H
+#	include <config.h>
+#endif
+
+#include "filesystemnative.h"
+
+#endif
+
+/* === U S I N G =========================================================== */
+
+using namespace std;
+using namespace etl;
+using namespace synfig;
+
+/* === M A C R O S ========================================================= */
+
+/* === G L O B A L S ======================================================= */
+
+/* === P R O C E D U R E S ================================================= */
+
+/* === M E T H O D S ======================================================= */
+
+// ReadStream
+
+FileSystemNative::ReadStream::ReadStream(Handle file_system, FILE *file):
+	FileSystem::ReadStream(file_system), file_(file) { }
+
+FileSystemNative::ReadStream::~ReadStream()
+	{ fclose(file_); }
+
+size_t FileSystemNative::ReadStream::read(void *buffer, size_t size)
+	{ return fread(buffer, 1, size, file_); }
+
+
+// WriteStream
+
+FileSystemNative::WriteStream::WriteStream(Handle file_system, FILE *file):
+	FileSystem::WriteStream(file_system), file_(file) { }
+
+FileSystemNative::WriteStream::~WriteStream()
+	{ fclose(file_); }
+
+size_t FileSystemNative::WriteStream::write(void *buffer, size_t size)
+	{ return fwrite(buffer, 1, size, file_); }
+
+
+// FileSystemNative
+
+FileSystemNative::FileSystemNative() { }
+FileSystemNative::~FileSystemNative() { }
+
+bool FileSystemNative::file_remove(const std::string &filename)
+{
+	return 0 == remove(filename.c_str());
+}
+
+FileSystem::ReadStreamHandle FileSystemNative::get_read_stream(const std::string &filename)
+{
+	FILE *f = fopen(filename.c_str(), "rb");
+	return f == NULL
+	     ? ReadStreamHandle()
+	     : ReadStreamHandle(new ReadStream(this, f));
+}
+
+FileSystem::WriteStreamHandle FileSystemNative::get_write_stream(const std::string &filename)
+{
+	FILE *f = fopen(filename.c_str(), "rb");
+	return f == NULL
+	     ? WriteStreamHandle()
+	     : WriteStreamHandle(new WriteStream(this, f));
+}
+
+
+/* === E N T R Y P O I N T ================================================= */
+
+
