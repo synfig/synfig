94b9b1f1e190a846cdc1e5475d3cd791d02f2d30
Ivan Mahonin <bh@icystar.com>
2013-08-30 22:36:54 +0700

    Remove unused variables from sinfig-studio code again

--- synfig-studio/src/synfigapp/pluginmanager.cpp
+++ synfig-studio/src/synfigapp/pluginmanager.cpp
@@ -235,7 +235,6 @@ PluginManager::load_dir( const std::string &pluginsprefix )
 				std::string pluginpath;
 				pluginpath = pluginsprefix+ETL_DIRECTORY_SEPARATOR+entry->d_name;
 				struct stat sb;
-				int rc = stat(pluginpath.c_str(), &sb);
 				// error handling if stat failed
 				if (S_ISDIR(sb.st_mode)) {
 					// checking if directory contains a plugin...
