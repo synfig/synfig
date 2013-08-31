synfig/synfig

commit 94b9b1f1e190a846cdc1e5475d3cd791d02f2d30

Remove unused variables from sinfig-studio code again

synfig-studio/src/synfigapp/pluginmanager.cpp
@@ -235,7 +235,6 @@ PluginManager::load_dir( const std::string &pluginsprefix )
 				std::string pluginpath;
 				pluginpath = pluginsprefix+ETL_DIRECTORY_SEPARATOR+entry->d_name;
 				struct stat sb;
-				int rc = stat(pluginpath.c_str(), &sb);
 				// error handling if stat failed
 				if (S_ISDIR(sb.st_mode)) {
 					// checking if directory contains a plugin...

synfig-studio/src/gui/dials/keyframedial.cpp
@@ -51,8 +51,6 @@ using namespace studio;
 
 KeyFrameDial::KeyFrameDial(): Gtk::Table(2, 1, false)
 {
-	Gtk::IconSize iconsize = Gtk::IconSize::from_name("synfig-small_icon_16x16");
-
 	toggle_keyframe_past = create_icon(Gtk::ICON_SIZE_BUTTON, "synfig-keyframe_lock_past_on",_("Unlock past keyframe"));
 	toggle_keyframe_future = create_icon(Gtk::ICON_SIZE_BUTTON, "synfig-keyframe_lock_future_on",_("Unlock future keyframe"));
 	attach(*toggle_keyframe_past, 0, 1, 0, 1, Gtk::SHRINK, Gtk::SHRINK, 0, 0);

synfig-studio/src/synfigapp/wplistconverter.cpp
@@ -59,8 +59,6 @@ WPListConverter::WPListConverter()
 void
 WPListConverter::operator()(std::list<synfig::WidthPoint> &wp_out, const std::list<synfig::Point> &p, const std::list<synfig::Real> &w)
 {
-	// number of data (float format)
-	Real nf;
 	// indexes k1, k2 for the interval considered, kem where the error
 	// is maximum
 	unsigned int k1, k2, kem;
@@ -89,7 +87,6 @@ WPListConverter::operator()(std::list<synfig::WidthPoint> &wp_out, const std::li
 		}
 	// once removed the duplicated then get the sizes of the work vectors
 	n=points.size();
-	nf=Real(n);
 	// Calculate the cumulative distances
 	Point p1(points[0]), p2;
 	Real d(0);

commit b2570b9e93ae8dfa201ad0149dc2477425da7457

Fix plugin loading (accidentaly broken in 94b9b1f)

synfig-studio/src/synfigapp/pluginmanager.cpp
@@ -235,6 +235,7 @@ PluginManager::load_dir( const std::string &pluginsprefix )
 				std::string pluginpath;
 				pluginpath = pluginsprefix+ETL_DIRECTORY_SEPARATOR+entry->d_name;
 				struct stat sb;
+				stat(pluginpath.c_str(), &sb);
 				// error handling if stat failed
 				if (S_ISDIR(sb.st_mode)) {
 					// checking if directory contains a plugin...