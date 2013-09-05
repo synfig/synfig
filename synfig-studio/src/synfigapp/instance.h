15c777fa9a3f8fac30395671c4e70e3fb60c04ca
Ivan Mahonin <bh@icystar.com>
2013-09-05 18:25:44 +0700

    Relative paths for extracted references

--- synfig-studio/src/synfigapp/instance.h
+++ synfig-studio/src/synfigapp/instance.h
@@ -101,6 +101,7 @@ private:
 
 	bool save_canvas_into_container_;
 	std::string save_canvas_reference_directory_;
+	std::string save_canvas_reference_local_directory_;
 	FileReferenceList save_canvas_references_;
 	static bool save_canvas_callback(void *instance_ptr, synfig::Layer::ConstHandle layer, const std::string &param_name, std::string &filename);
 	void update_references_in_canvas();
