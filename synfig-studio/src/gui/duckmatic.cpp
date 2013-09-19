c2c3c80d1cc88e7a525c06a31b28a2a9e1527a25
Carlos Lopez <genetita@gmail.com>
2013-09-19 19:39:11 +0200

    When drag a bezier, update the tangents of the surrounding bezier if applicable.

--- synfig-studio/src/gui/duckmatic.cpp
+++ synfig-studio/src/gui/duckmatic.cpp
@@ -618,6 +618,110 @@ Duckmatic::update_ducks()
 			}
 		}
 	}
+	if(get_selected_bezier())
+	{
+		etl::handle<Duck> c1(get_selected_bezier()->c1);
+		etl::handle<Duck> c2(get_selected_bezier()->c2);
+		if(c1->get_value_desc().parent_is_linkable_value_node())
+		{
+			ValueNode_Composite::Handle composite(ValueNode_Composite::Handle::cast_dynamic(c1->get_value_desc().get_parent_value_node()));
+			LinkableValueNode::Handle duck_value_node(LinkableValueNode::Handle::cast_dynamic(c1->get_value_desc().get_value_node()));
+			// it belongs to a composite and it is a BLinePoint
+			if(composite && composite->get_type() == ValueBase::TYPE_BLINEPOINT && duck_value_node)
+			{
+				int index(c1->get_value_desc().get_index());
+				etl::handle<Duck> origin_duck=c1->get_origin_duck();
+				// Search all the rest of ducks
+				DuckList::iterator iter;
+				for (iter=duck_list.begin(); iter!=duck_list.end(); iter++)
+					// if the other duck has the same origin and it is tangent type
+					if ( (*iter)->get_origin_duck()==origin_duck && (*iter)->get_type() == Duck::TYPE_TANGENT)
+					{
+						ValueNode_Composite::Handle iter_composite;
+						iter_composite=ValueNode_Composite::Handle::cast_dynamic((*iter)->get_value_desc().get_parent_value_node());
+						// and their parent valuenode are the same
+						if(iter_composite.get() == composite.get())
+						{
+							BLinePoint bp=(*composite)(time);
+							int t1_index=composite->get_link_index_from_name("t1");
+							int t2_index=composite->get_link_index_from_name("t2");
+							if(index==t1_index && (*iter)->get_value_desc().get_index()!=t1_index)
+							{
+								bp.set_tangent1(c1->get_point());
+								Vector t2(bp.get_tangent2());
+								(*iter)->set_point(Point(t2));
+							}
+							else if(index==t2_index && (*iter)->get_value_desc().get_index()!=t2_index)
+							{
+								// Create a new BLinePoint
+								BLinePoint nbp;
+								// Terporary set the flags for the new BLinePoint to all split
+								nbp.set_split_tangent_both(true);
+								// Now we can set the tangents. Tangent2 won't be modified by tangent1
+								nbp.set_tangent1(c1->get_point());
+								nbp.set_tangent2(bp.get_tangent1());
+								// Now update the flags
+								nbp.set_split_tangent_radius(bp.get_split_tangent_radius());
+								nbp.set_split_tangent_angle(bp.get_split_tangent_angle());
+								// Now retrieve the updated tangent2 (which will be stored as t1, see below)
+								Vector t1(nbp.get_tangent2());
+								(*iter)->set_point(Point(t1));
+							}
+						}
+					}
+			}
+		}
+		if(c2->get_value_desc().parent_is_linkable_value_node())
+		{
+			ValueNode_Composite::Handle composite(ValueNode_Composite::Handle::cast_dynamic(c2->get_value_desc().get_parent_value_node()));
+			LinkableValueNode::Handle duck_value_node(LinkableValueNode::Handle::cast_dynamic(c2->get_value_desc().get_value_node()));
+			// it belongs to a composite and it is a BLinePoint
+			if(composite && composite->get_type() == ValueBase::TYPE_BLINEPOINT && duck_value_node)
+			{
+				int index(c2->get_value_desc().get_index());
+				etl::handle<Duck> origin_duck=c2->get_origin_duck();
+				// Search all the rest of ducks
+				DuckList::iterator iter;
+				for (iter=duck_list.begin(); iter!=duck_list.end(); iter++)
+					// if the other duck has the same origin and it is tangent type
+					if ( (*iter)->get_origin_duck()==origin_duck && (*iter)->get_type() == Duck::TYPE_TANGENT)
+					{
+						ValueNode_Composite::Handle iter_composite;
+						iter_composite=ValueNode_Composite::Handle::cast_dynamic((*iter)->get_value_desc().get_parent_value_node());
+						// and their parent valuenode are the same
+						if(iter_composite.get() == composite.get())
+						{
+							BLinePoint bp=(*composite)(time);
+							int t1_index=composite->get_link_index_from_name("t1");
+							int t2_index=composite->get_link_index_from_name("t2");
+							if(index==t1_index && (*iter)->get_value_desc().get_index()!=t1_index)
+							{
+								bp.set_tangent1(c2->get_point());
+								Vector t2(bp.get_tangent2());
+								(*iter)->set_point(Point(t2));
+							}
+							else if(index==t2_index && (*iter)->get_value_desc().get_index()!=t2_index)
+							{
+								// Create a new BLinePoint
+								BLinePoint nbp;
+								// Terporary set the flags for the new BLinePoint to all split
+								nbp.set_split_tangent_both(true);
+								// Now we can set the tangents. Tangent2 won't be modified by tangent1
+								nbp.set_tangent1(c2->get_point());
+								nbp.set_tangent2(bp.get_tangent1());
+								// Now update the flags
+								nbp.set_split_tangent_radius(bp.get_split_tangent_radius());
+								nbp.set_split_tangent_angle(bp.get_split_tangent_angle());
+								// Now retrieve the updated tangent2 (which will be stored as t1, see below)
+								Vector t1(nbp.get_tangent2());
+								(*iter)->set_point(Point(t1));
+							}
+						}
+					}
+			}
+		}
+
+	}
 }
 
 
@@ -819,6 +923,8 @@ BezierDrag_Default::bezier_drag(Duckmatic* duckmatic, const synfig::Vector& vect
 	duckmatic->get_selected_bezier()->c2->set_trans_point(c2_initial+c2_offset, time);
 
 	last_translate_=vect;
+	
+	duckmatic->update_ducks();
 }
 
 bool
