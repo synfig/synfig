cc4c9edadb9eafaad4b1afeef9dee293aa7ef3aa
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2013-10-10 22:12:51 +0700

    Add translation maintenance scripts

---
+++ autobuild/transifex-push-translations.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+SCRIPTPATH="${BASH_SOURCE[0]}";
+pushd `dirname "${SCRIPTPATH}"` > /dev/null
+SCRIPTPATH=`pwd`;
+popd  > /dev/null
+
+cd "$SCRIPTPATH/.."
+for POTFILE in `ls synfig-core/po/*.po`; do echo $POTFILE; msgfmt -c $POTFILE; done
+for POTFILE in `ls synfig-studio/po/*.po`; do echo $POTFILE; msgfmt -c $POTFILE; done
+tx push -t
