901ccd63cda216c66c0a37bb2030e7514d5aa072
Ivan Mahonin <bh@icystar.com>
2014-03-20 23:39:21 +0700

    Fix warning about resampling

--- synfig-core/src/synfig/layer.cpp
+++ synfig-core/src/synfig/layer.cpp
@@ -557,7 +557,7 @@ Layer::hit_check(synfig::Context context, const synfig::Point &pos)const
 bool
 Layer::render_transformed(const Layer *layer, Context context,Surface *surface,int quality, const RendDesc &renddesc, ProgressCallback *cb)
 {
-	warning("Resampling used while rendering - possible overhead", __FILE__, __LINE__);
+	warning("%s:%d Resampling used while rendering - possible overhead", __FILE__, __LINE__);
 
 	Transformation transformation(renddesc.get_transformation_matrix());
 
