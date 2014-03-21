c61a94d2de33cd654196f43b6582c02ec6cefb82
Yu Chen <jcomee@gmail.com>
2014-03-21 13:05:20 +0800

    More flexible code for reusing

--- synfig-studio/src/gui/states/state_circle.cpp
+++ synfig-studio/src/gui/states/state_circle.cpp
@@ -762,7 +762,8 @@ StateCircle_Context::StateCircle_Context(CanvasView* canvas_view):
 	options_table.set_row_spacings(GAP); // row gap
 	options_table.set_row_spacing(0, GAP*2); // the gap between first and second row.
 	options_table.set_row_spacing(2, 1); // row gap between label and icon of layer type
-	options_table.set_row_spacing(13, 0); // the final row using border width of table
+	// the final row using border width of table
+	options_table.set_row_spacing(options_table.property_n_rows(), 0);
 
 	options_table.show_all();
 
