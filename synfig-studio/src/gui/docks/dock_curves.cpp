416639aea4afe397a3c4392917b164912490856c
[d.j.a.y] Jerome Blanchi <d.j.a.y@free.fr>
2014-04-25 13:33:43 +0200

    Fix161 - Align rows of Params Panel and Timetrack Panel - 2
    - Replace unuseful comments by useful ones

--- synfig-studio/src/gui/docks/dock_curves.cpp
+++ synfig-studio/src/gui/docks/dock_curves.cpp
@@ -106,7 +106,10 @@ _curve_selection_changed(Gtk::TreeView* param_tree_view,Widget_Curves* curves)
 void
 Dock_Curves::init_canvas_view_vfunc(etl::loose_handle<CanvasView> canvas_view)
 {
-	// FIXME Memory leak ? When curves is formally deleted ?
+	//! Curves is registred thrue CanvasView::set_ext_widget
+	//! and will be deleted during CanvasView::~CanvasView()
+	//! \see CanvasView::set_ext_widget
+	//! \see CanvasView::~CanvasView
 	Widget_Curves* curves(new Widget_Curves());
 	curves->set_time_adjustment(canvas_view->time_adjustment());
 
