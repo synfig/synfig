d5027f57d906449b03e63e9482b4ff143b1516f3
Diego Barrios Romero <eldruin@eldruin.com>
2014-12-20 13:56:27 +0100

    Fix checking permissions of output directory

--- synfig-core/src/tool/joblistprocessor.cpp
+++ synfig-core/src/tool/joblistprocessor.cpp
@@ -139,7 +139,7 @@ bool setup_job(Job& job, const TargetParam& target_parameters)
 	VERBOSE_OUT(4) << "Outfilename = " << job.outfilename.c_str() << std::endl;
 
 	// Check permissions
-	if (access(bfs::path(job.outfilename).string().c_str(), W_OK) == -1)
+	if (access(bfs::path(job.outfilename).parent_path().string().c_str(), W_OK) == -1)
 	{
 		synfig::error(strprintf(_("Unable to create output for \"%s\": %s"), job.filename.c_str(), strerror(errno)));
 		synfig::error(_("Throwing out job..."));
