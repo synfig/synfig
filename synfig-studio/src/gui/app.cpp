4d95377e53b03b184ab76fabcce3fcc20768e1c2
Yu Chen <jcomee@gmail.com>
2014-12-23 13:13:52 +0800

    Typo

--- synfig-studio/src/gui/app.cpp
+++ synfig-studio/src/gui/app.cpp
@@ -2344,7 +2344,7 @@ App::dialog_open_file_image(const std::string &title, std::string &filename, std
 	dialog->add_button(Gtk::Stock::CANCEL, Gtk::RESPONSE_CANCEL);
 	dialog->add_button(Gtk::StockID(_("Load")), Gtk::RESPONSE_ACCEPT);
 
-	// show only Synfig color palette file (*.spal)
+	// show only images
 	Glib::RefPtr<Gtk::FileFilter> filter_image = Gtk::FileFilter::create();
 	filter_image->set_name("Images and sequence files (*.png, *.jpeg, *.bmp, *.svg, *.list)");
 	filter_image->add_pattern("*.png");
