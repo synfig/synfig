58172d80db901e638cf2ab5cd2e6430f835f949b
[d.j.a.y] Jerome Blanchi <d.j.a.y@free.fr>
2014-12-23 16:55:54 +0100

    Fix OptionsProcessor::process_debug_options() defintion

--- synfig-core/src/tool/optionsprocessor.h
+++ synfig-core/src/tool/optionsprocessor.h
@@ -40,7 +40,7 @@ public:
 					 const boost::program_options::options_description& po_visible);
 
 #ifdef _DEBUG
-	void process_debug_options();
+	void process_debug_options() throw (SynfigToolException&);
 #endif
 
 	/// Settings options
