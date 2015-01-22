ad08ea1053b79b6e08a91db7699ee14fae2b9f15
[d.j.a.y] Jerome Blanchi <d.j.a.y@free.fr>
2015-01-22 20:24:54 +0100

    Typo on jpeg (was peg) and lst (was list) file type

--- synfig-studio/src/gui/app.cpp
+++ synfig-studio/src/gui/app.cpp
@@ -2354,9 +2354,9 @@ App::dialog_open_file_image(const std::string &title, std::string &filename, std
 
 	// show only images
 	Glib::RefPtr<Gtk::FileFilter> filter_image = Gtk::FileFilter::create();
-	filter_image->set_name("Images and sequence files (*.png, *.jpeg, *.bmp, *.svg, *.list)");
+	filter_image->set_name("Images and sequence files (*.png, *.jpeg, *.bmp, *.svg, *.lst)");
 	filter_image->add_pattern("*.png");
-	filter_image->add_pattern("*.peg");
+	filter_image->add_pattern("*.jpeg");
 	filter_image->add_pattern("*.jpg");
 	filter_image->add_pattern("*.bmp");
 	filter_image->add_pattern("*.svg");
