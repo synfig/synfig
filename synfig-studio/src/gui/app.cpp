0148eefa4ce6464b374238bfdbd2edecdb44e763
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2015-01-22 12:35:54 +0600

    Use 1.0 suffix for settings file

--- synfig-studio/src/gui/app.cpp
+++ synfig-studio/src/gui/app.cpp
@@ -1814,7 +1814,7 @@ App::save_settings()
 			for(iter=recent_files.rbegin();iter!=recent_files.rend();iter++)
 				file<<(*iter).c_str()<<endl;
 		}while(0);
-		std::string filename=get_config_file("settings-0.65");
+		std::string filename=get_config_file("settings-1.0");
 		synfigapp::Main::settings().save_to_file(filename);
 
 	}
@@ -1831,7 +1831,7 @@ App::load_settings(const synfig::String& key_filter)
 	try
 	{
 		synfig::ChangeLocale change_locale(LC_NUMERIC, "C");
-		std::string filename=get_config_file("settings-0.65");
+		std::string filename=get_config_file("settings-1.0");
 		ret=synfigapp::Main::settings().load_from_file(filename, key_filter);
 	}
 	catch(...)
