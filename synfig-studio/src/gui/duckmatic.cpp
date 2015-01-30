synfig/synfig

commit e0198d888dc7671781a368a0293486e008c02f09

Remove "Enable Transformation" parameter from Group Layer

synfig-studio/src/gui/duckmatic.cpp
@@ -1958,134 +1958,12 @@ Duckmatic::add_to_ducks(const synfigapp::ValueDesc& value_desc,etl::handle<Canva
 		}
 	}
 	else
-	if (type == type_vector)
-	{
-		etl::handle<Layer_PasteCanvas> layer;
-		if (value_desc.parent_is_layer())
-			layer = etl::handle<Layer_PasteCanvas>::cast_dynamic(value_desc.get_layer());
-		bool enable = !layer || !layer->get_enable_transformation();
-		if (enable) {
-			etl::handle<Duck> duck=new Duck();
-			set_duck_value_desc(*duck, value_desc, transform_stack);
-			ValueNode_Composite::Handle blinepoint_value_node;
-			int index;
-			bool done(false);
-			if(value_desc.parent_is_linkable_value_node()
-			   &&
-			   value_desc.get_parent_value_node()->get_type() == type_bline_point)
-			{
-				blinepoint_value_node=ValueNode_Composite::Handle::cast_dynamic(value_desc.get_parent_value_node());
-				if(blinepoint_value_node)
-				{
-					index=blinepoint_value_node->get_link_index_from_name("t2");
-					if(index==value_desc.get_index())
-					{
-						BLinePoint bp=(*blinepoint_value_node)(get_time()).get(BLinePoint());
-						Vector t2=bp.get_tangent2();
-						duck->set_point(t2);
-						done=true;
-					}
-				}
-			}
-			if(!done)
-				duck->set_point(value_desc.get_value(get_time()).get(Point()));
-
-			if(value_desc.is_value_node())
-			{
-				// if the vertex is converted to 'bone influence', add the bones' ducks
-				if (ValueNode_BoneInfluence::Handle bone_influence_vertex_value_node =
-					ValueNode_BoneInfluence::Handle::cast_dynamic(value_desc.get_value_node()))
-					add_to_ducks(synfigapp::ValueDesc(bone_influence_vertex_value_node,
-													  bone_influence_vertex_value_node->get_link_index_from_name("bone_weight_list")),
-								 canvas_view, transform_stack);
-
-				// If the ValueNode can be directly manipulated,
-				// then set it as so.
-				duck->set_editable(synfigapp::is_editable(value_desc.get_value_node()));
-			}
-			else
-			{
-				//duck->set_point(value_desc.get_value().get(Point()));
-				//duck->set_name(strprintf("%x",value_desc.get_layer().get())+value_desc.get_param_name());
-				duck->set_editable(true);
-			}
-
-			// If we were passed a parameter description
-			if(param_desc)
-			{
-				if(!param_desc->get_connect().empty())
-				{
-					synfigapp::ValueDesc value_desc_origin(value_desc.get_layer(),param_desc->get_connect());
-					Duck::Handle connect_duck;
-					if(duck_map.find(calc_duck_guid(value_desc_origin, transform_stack))!=duck_map.end())
-					{
-						connect_duck=duck_map[calc_duck_guid(value_desc_origin, transform_stack)];
-					}
-					else
-					{
-						add_to_ducks(value_desc_origin,canvas_view, transform_stack);
-						connect_duck=last_duck();
-					}
-					duck->set_connect_duck(connect_duck);
-				}
-				if(!param_desc->get_box().empty())
-				{
-					synfigapp::ValueDesc value_desc_origin(value_desc.get_layer(),param_desc->get_box());
-					add_to_ducks(value_desc_origin,canvas_view, transform_stack);
-					duck->set_box_duck(last_duck());
-				}
-
-				// If we have an origin
-				if(!param_desc->get_origin().empty())
-				{
-					synfigapp::ValueDesc value_desc_origin(value_desc.get_layer(),param_desc->get_origin());
-					/*
-					duck->set_origin(value_desc_origin.get_value(get_time()).get(synfig::Point()));
-					*/
-					add_to_ducks(value_desc_origin,canvas_view, transform_stack);
-					duck->set_origin(last_duck());
-					duck->set_type(Duck::TYPE_VERTEX);
-				}
-				else
-					duck->set_type(Duck::TYPE_POSITION);
-
-				duck->set_scalar(param_desc->get_scalar());
-			}
-			else
-				duck->set_type(Duck::TYPE_POSITION);
-
-			duck->signal_edited().clear(); // value_desc.get_value_type() == type_vector:
-			duck->signal_edited().connect(
-				sigc::bind(
-					sigc::mem_fun(
-						*this,
-						&studio::Duckmatic::on_duck_changed),
-					value_desc));
-
-			duck->signal_user_click(2).connect(
-				sigc::bind(
-					sigc::bind(
-						sigc::bind(
-							sigc::mem_fun(
-								*canvas_view,
-								&studio::CanvasView::popup_param_menu),
-							false),
-						1.0f),
-					value_desc));
-
-			add_duck(duck);
-
-			return true;
-		}
-	}
-	else
 	if (type == type_transformation)
 	{
 		if (value_desc.parent_is_layer() && param_desc != NULL)
 		{
 			etl::handle<Layer_PasteCanvas> layer = etl::handle<Layer_PasteCanvas>::cast_dynamic(value_desc.get_layer());
-			bool enable_transformation = layer && layer->get_enable_transformation();
-			if (enable_transformation)
+			if (layer)
 			{
 				synfigapp::ValueDesc alternative_value_desc(value_desc.get_layer(), "origin");
 				Transformation transformation = value_desc.get_value(get_time()).get(Transformation());

synfig-core/src/synfig/layer_pastecanvas.cpp
@@ -81,7 +81,6 @@ public:
 Layer_PasteCanvas::Layer_PasteCanvas():
 	param_origin(Point()),
 	param_transformation(Transformation()),
-	param_enable_transformation(ValueBase(true)),
 	param_time_offset (Time(0)),
 	depth(0),
 	extra_reference(false)
@@ -134,11 +133,6 @@ Layer_PasteCanvas::get_param_vocab()const
 		.set_description(_("Position, rotation, skew and scale"))
 	);
 
-	ret.push_back(ParamDesc("enable_transformation")
-		.set_local_name(_("Enable Transformation"))
-		.set_description(_("Enables or disables transformation"))
-	);
-
 	ret.push_back(ParamDesc("canvas")
 		.set_local_name(_("Canvas"))
 		.set_description(_("Group content"))
@@ -184,7 +178,6 @@ Layer_PasteCanvas::set_param(const String & param, const ValueBase &value)
 {
 	IMPORT_VALUE(param_origin);
 	IMPORT_VALUE(param_transformation);
-	IMPORT_VALUE(param_enable_transformation);
 
 	// IMPORT(canvas);
 	if(param=="canvas" && value.can_get(Canvas::Handle()))
@@ -293,7 +286,6 @@ Layer_PasteCanvas::get_param(const String& param)const
 {
 	EXPORT_VALUE(param_origin);
 	EXPORT_VALUE(param_transformation);
-	EXPORT_VALUE(param_enable_transformation);
 	if (param=="canvas")
 	{
 		synfig::ValueBase ret(canvas);

synfig-core/src/synfig/layer_pastecanvas.h
@@ -56,8 +56,6 @@ private:
 	ValueBase param_origin;
 	//! Parameter: (Transfromation) Position, rotation and scale of the paste canvas layer
 	ValueBase param_transformation;
-	//! Parameter: (Enable Transfromation)
-	ValueBase param_enable_transformation;
 	//! Parameter: (etl::loose_handle<synfig::Canvas>) The canvas parameter
 	etl::loose_handle<synfig::Canvas> canvas;
 	//! Parameter: (Time) Time offset of the paste canvas layer
@@ -150,14 +148,10 @@ public:
 	Point get_origin()const { return param_origin.get(Point()); }
 	//! Get transformation parameter
 	Transformation get_transformation()const { return param_transformation.get(Transformation()); }
-	//! Get enable transformation parameter
-	bool get_enable_transformation()const { return param_enable_transformation.get(true); }
 	//! Get summary transformation
 	Transformation get_summary_transformation()const
 	{
-		return get_enable_transformation()
-			 ? get_transformation().transform( Transformation(-get_origin()) )
-			 : Transformation();
+		return get_transformation().transform( Transformation(-get_origin()) );
 	}
 
 	//! Default constructor

commit d14e70e86964e2a4f8e144f3ea43bb339489cecf

Fix green handles not displayed (regression introduced by e0198d888)

synfig-studio/src/gui/duckmatic.cpp
@@ -1958,6 +1958,126 @@ Duckmatic::add_to_ducks(const synfigapp::ValueDesc& value_desc,etl::handle<Canva
 		}
 	}
 	else
+	if (type == type_vector)
+	{
+		etl::handle<Layer_PasteCanvas> layer;
+		if (value_desc.parent_is_layer())
+			layer = etl::handle<Layer_PasteCanvas>::cast_dynamic(value_desc.get_layer());
+		if (!layer) {
+			etl::handle<Duck> duck=new Duck();
+			set_duck_value_desc(*duck, value_desc, transform_stack);
+			ValueNode_Composite::Handle blinepoint_value_node;
+			int index;
+			bool done(false);
+			if(value_desc.parent_is_linkable_value_node()
+			   &&
+			   value_desc.get_parent_value_node()->get_type() == type_bline_point)
+			{
+				blinepoint_value_node=ValueNode_Composite::Handle::cast_dynamic(value_desc.get_parent_value_node());
+				if(blinepoint_value_node)
+				{
+					index=blinepoint_value_node->get_link_index_from_name("t2");
+					if(index==value_desc.get_index())
+					{
+						BLinePoint bp=(*blinepoint_value_node)(get_time()).get(BLinePoint());
+						Vector t2=bp.get_tangent2();
+						duck->set_point(t2);
+						done=true;
+					}
+				}
+			}
+			if(!done)
+				duck->set_point(value_desc.get_value(get_time()).get(Point()));
+
+			if(value_desc.is_value_node())
+			{
+				// if the vertex is converted to 'bone influence', add the bones' ducks
+				if (ValueNode_BoneInfluence::Handle bone_influence_vertex_value_node =
+					ValueNode_BoneInfluence::Handle::cast_dynamic(value_desc.get_value_node()))
+					add_to_ducks(synfigapp::ValueDesc(bone_influence_vertex_value_node,
+													  bone_influence_vertex_value_node->get_link_index_from_name("bone_weight_list")),
+								 canvas_view, transform_stack);
+
+				// If the ValueNode can be directly manipulated,
+				// then set it as so.
+				duck->set_editable(synfigapp::is_editable(value_desc.get_value_node()));
+			}
+			else
+			{
+				//duck->set_point(value_desc.get_value().get(Point()));
+				//duck->set_name(strprintf("%x",value_desc.get_layer().get())+value_desc.get_param_name());
+				duck->set_editable(true);
+			}
+
+			// If we were passed a parameter description
+			if(param_desc)
+			{
+				if(!param_desc->get_connect().empty())
+				{
+					synfigapp::ValueDesc value_desc_origin(value_desc.get_layer(),param_desc->get_connect());
+					Duck::Handle connect_duck;
+					if(duck_map.find(calc_duck_guid(value_desc_origin, transform_stack))!=duck_map.end())
+					{
+						connect_duck=duck_map[calc_duck_guid(value_desc_origin, transform_stack)];
+					}
+					else
+					{
+						add_to_ducks(value_desc_origin,canvas_view, transform_stack);
+						connect_duck=last_duck();
+					}
+					duck->set_connect_duck(connect_duck);
+				}
+				if(!param_desc->get_box().empty())
+				{
+					synfigapp::ValueDesc value_desc_origin(value_desc.get_layer(),param_desc->get_box());
+					add_to_ducks(value_desc_origin,canvas_view, transform_stack);
+					duck->set_box_duck(last_duck());
+				}
+
+				// If we have an origin
+				if(!param_desc->get_origin().empty())
+				{
+					synfigapp::ValueDesc value_desc_origin(value_desc.get_layer(),param_desc->get_origin());
+					/*
+					duck->set_origin(value_desc_origin.get_value(get_time()).get(synfig::Point()));
+					*/
+					add_to_ducks(value_desc_origin,canvas_view, transform_stack);
+					duck->set_origin(last_duck());
+					duck->set_type(Duck::TYPE_VERTEX);
+				}
+				else
+					duck->set_type(Duck::TYPE_POSITION);
+
+				duck->set_scalar(param_desc->get_scalar());
+			}
+			else
+				duck->set_type(Duck::TYPE_POSITION);
+
+			duck->signal_edited().clear(); // value_desc.get_value_type() == type_vector:
+			duck->signal_edited().connect(
+				sigc::bind(
+					sigc::mem_fun(
+						*this,
+						&studio::Duckmatic::on_duck_changed),
+					value_desc));
+
+			duck->signal_user_click(2).connect(
+				sigc::bind(
+					sigc::bind(
+						sigc::bind(
+							sigc::mem_fun(
+								*canvas_view,
+								&studio::CanvasView::popup_param_menu),
+							false),
+						1.0f),
+					value_desc));
+
+			add_duck(duck);
+
+			return true;
+		}
+	}
+	else
 	if (type == type_transformation)
 	{
 		if (value_desc.parent_is_layer() && param_desc != NULL)