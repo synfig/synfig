78db8b4a0017384e3c282650cdfc9da6ba6d3a6c
Ivan Mahonin <bh@icystar.com>
2015-05-23 17:32:05 +0600

    Declaration of common rendering optimizers

---
+++ synfig-core/src/synfig/rendering/common/optimizer/optimizerblur.cpp
@@ -0,0 +1,73 @@
+/* === S Y N F I G ========================================================= */
+/*!	\file synfig/rendering/common/optimizer/optimizerblur.cpp
+**	\brief OptimizerBlur
+**
+**	$Id$
+**
+**	\legal
+**	......... ... 2015 Ivan Mahonin
+**
+**	This package is free software; you can redistribute it and/or
+**	modify it under the terms of the GNU General Public License as
+**	published by the Free Software Foundation; either version 2 of
+**	the License, or (at your option) any later version.
+**
+**	This package is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+**	General Public License for more details.
+**	\endlegal
+*/
+/* ========================================================================= */
+
+/* === H E A D E R S ======================================================= */
+
+#ifdef USING_PCH
+#	include "pch.h"
+#else
+#ifdef HAVE_CONFIG_H
+#	include <config.h>
+#endif
+
+#ifndef WIN32
+#include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+#endif
+
+#include "optimizerblur.h"
+
+#endif
+
+using namespace synfig;
+using namespace rendering;
+
+/* === M A C R O S ========================================================= */
+
+/* === G L O B A L S ======================================================= */
+
+/* === P R O C E D U R E S ================================================= */
+
+/* === M E T H O D S ======================================================= */
+
+bool
+OptimizerBlur::run(const RunParams& params) const
+{
+	// TODO: Create transformation
+		// if task is Blur
+		//    and not sub_task_resized
+	    //    and surface is set
+		// then do following:
+		// create transformation
+		// create TaskTransformation
+		// assign subtask
+		// clone Blur
+		// assign TaskTransformation to new Blur
+		// set sub_task_resized
+		// return new Blur
+	// TODO: Optimize scale transformation
+	// TODO: Optimize blur to blur
+	return false;
+}
+
+/* === E N T R Y P O I N T ================================================= */
