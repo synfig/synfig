853fdb74c950793c78ed85555d5245746f4f2a1d
Ivan Mahonin <bh@icystar.com>
2015-05-27 12:47:37 +0600

    OpenGL rendering declarations

---
+++ synfig-core/src/synfig/rendering/opengl/renderergl.cpp
@@ -0,0 +1,60 @@
+/* === S Y N F I G ========================================================= */
+/*!	\file synfig/rendering/software/renderersw.cpp
+**	\brief RendererSW
+**
+**	$Id$
+**
+**	\legal
+**	......... ... 2014 Ivan Mahonin
+**
+**	This package is free software; you can redistribute it and/or
+**	modify it under the terms of the GNU General Public License as
+**	published by the Free Software Foundation; either version 2 of
+**	the License, or (at your option) any later version.
+**
+**	This package is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+**	General Public License for more details.
+**	\endlegal
+*/
+/* ========================================================================= */
+
+/* === H E A D E R S ======================================================= */
+
+#ifdef USING_PCH
+#	include "pch.h"
+#else
+#ifdef HAVE_CONFIG_H
+#	include <config.h>
+#endif
+
+#ifndef WIN32
+#include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+#endif
+
+#include <synfig/rendering/opengl/renderergl.h>
+
+#endif
+
+using namespace synfig;
+using namespace rendering;
+
+/* === M A C R O S ========================================================= */
+
+/* === G L O B A L S ======================================================= */
+
+/* === P R O C E D U R E S ================================================= */
+
+/* === M E T H O D S ======================================================= */
+
+RendererSW::RendererSW()
+{
+	// TODO: register optimizers
+}
+
+RendererSW::~RendererSW() { }
+
+/* === E N T R Y P O I N T ================================================= */
