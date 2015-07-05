cab90d672257622ca420ecbb9ee27e745a356609
[d.j.a.y] Jerome Blanchi <d.j.a.y@free.fr>
2015-07-05 13:40:58 +0200

    Build : Comment code to remove warnings

--- synfig-studio/src/gui/preview.cpp
+++ synfig-studio/src/gui/preview.cpp
@@ -1067,10 +1067,11 @@ void studio::Widget_Preview::pause()
 void studio::Widget_Preview::on_play_pause_pressed()
 {
 	bool play_flag;
-	float begin = preview->get_begintime();
-	float end = preview->get_endtime();
-	float current = adj_time_scrub->get_value();
-	Gtk::Image *icon;
+	//! Commented out , build warnings
+//	float begin = preview->get_begintime();
+//	float end = preview->get_endtime();
+//	float current = adj_time_scrub->get_value();
+//	Gtk::Image *icon;
 
 	play_flag = !playing;
 	
