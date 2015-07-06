bac702fb4eb3734efeff1ad5754cef036934c1ed
Ivan Mahonin <bh@icystar.com>
2015-07-06 19:16:36 +0600

    Update LayerShape - use internal functionality of TaskContourSW

--- synfig-core/src/synfig/layers/layer_shape.h
+++ synfig-core/src/synfig/layers/layer_shape.h
@@ -34,6 +34,8 @@
 #include <synfig/vector.h>
 #include <synfig/blur.h>
 
+#include <synfig/rendering/software/task/taskcontoursw.h>
+
 #include <vector>
 
 /* === M A C R O S ========================================================= */
@@ -50,19 +52,6 @@ class Layer_Shape : public Layer_Composite, public Layer_NoDeform
 {
 	SYNFIG_LAYER_MODULE_EXT
 
-	enum WindingStyle
-	{
-	    WINDING_NON_ZERO=0,			//!< less than -1 --> 1;  -1 --> 1;   0 --> 0;   1 --> 1;  greater than 1 --> 1
-	    WINDING_EVEN_ODD=1,			//!< add or subtract multiples of 2 to get into range -1:1, then as above
-
-	    WINDING_END=2				//!< \internal
-	};
-
-private:
-
-	//internal caching
-	struct Intersector;
-	Intersector	*edge_table;
 protected:
 	//!Parameter: (Color) Color of the shape
 	ValueBase   param_color;
@@ -77,16 +66,15 @@ protected:
 	//!Parameter: (Real) Amount of feather of the shape
 	ValueBase   param_feather;
 	//!Parameter: (WindingStyle) How shape is rendered when crosses it self
-	ValueBase param_winding_style;
-private:
-	std::vector< char > 	bytestream;
+	ValueBase	param_winding_style;
 
-	//for use in creating the bytestream
-	int						lastbyteop;
-	int						lastoppos;
+private:
+	//internal caching
+	struct Intersector;
+	Intersector	*edge_table;
+	rendering::Contour::Handle contour;
 
 protected:
-
 	Layer_Shape(const Real &a = 1.0, const Color::BlendMethod m = Color::BLEND_COMPOSITE);
 
 public:
@@ -97,16 +85,11 @@ public:
 	/*!	Also clears out the Intersector
 	*/
 	void clear();
-	//void sync();
-
 	void move_to(Real x, Real y);
 	void line_to(Real x, Real y);
 	void conic_to(Real x1, Real y1, Real x, Real y);
-	void conic_to_smooth(Real x, Real y);				//x1,y1 derived from current tangent
 	void curve_to(Real x1, Real y1, Real x2, Real y2, Real x, Real y);
-	void curve_to_smooth(Real x2, Real y2, Real x, Real y);	//x1,y1 derived from current tangent
 	void close();
-	void endpath();
 
 	virtual bool set_param(const String & param, const synfig::ValueBase &value);
 	virtual ValueBase get_param(const String & param)const;
@@ -115,27 +98,17 @@ public:
 
 	virtual Color get_color(Context context, const Point &pos)const;
 	virtual bool accelerated_render(Context context, Surface *surface,int quality, const RendDesc &renddesc, ProgressCallback *cb)const;
-	virtual bool accelerated_cairorender(Context context, cairo_t *cr, int quality, const RendDesc &renddesc, ProgressCallback *cb)const;
 	virtual synfig::Layer::Handle hit_check(synfig::Context context, const synfig::Point &point)const;
 	virtual Rect get_bounding_rect()const;
 	//This function translates Shape primitives to Cairo primitives. Currently only supported move_to and line_to.
 	bool shape_to_cairo(cairo_t* cr)const;
 	bool feather_cairo_surface(cairo_surface_t* surface, RendDesc renddesc, int quality)const;
 
+protected:
+	virtual rendering::Task::Handle build_rendering_task_vfunc(Context context)const;
+
 private:
-	class 		PolySpan;
-	bool render_polyspan(
-		Surface *surface,
-		PolySpan &polyspan,
-		Color::value_type amount,
-		Color::BlendMethod blend_method,
-		const Color &color,
-		bool invert,
-		bool antialias,
-		WindingStyle winding_style) const;
-	bool render_polyspan(etl::surface<float> *surface,PolySpan &polyspan)const;
-	virtual bool render_shape(Surface *surface,bool useblend,int quality,const RendDesc &renddesc, ProgressCallback *cb)const;
-	virtual bool render_shape(etl::surface<float> *surface,int quality,const RendDesc &renddesc, ProgressCallback *cb)const;
+	bool render_shape(Surface *surface, bool useblend, const RendDesc &renddesc) const;
 }; // END of Layer_Shape
 
 }; // END of namespace synfig
