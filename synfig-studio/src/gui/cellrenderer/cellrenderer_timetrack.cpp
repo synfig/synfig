synfig/synfig

commit a3e9cff193216d4b9c3b88f0c7495a2603c62fc4

Stretch child waypoints of Group Layer if Speed parameter is not = 1.

synfig-studio/src/gui/cellrenderer/cellrenderer_timetrack.cpp
@@ -117,6 +117,31 @@ CellRenderer_TimeTrack::is_selected(const Waypoint& waypoint)const
 	return selected==waypoint;
 }
 
+const synfig::Time get_time_dilation_from_vdesc(const synfigapp::ValueDesc &v)
+{
+#ifdef ADJUST_WAYPOINTS_FOR_TIME_OFFSET
+	if(getenv("SYNFIG_SHOW_CANVAS_PARAM_WAYPOINTS") ||
+	   v.get_value_type() != synfig::type_canvas)
+		return synfig::Time::zero();
+
+	synfig::Canvas::Handle canvasparam = v.get_value().get(Canvas::Handle());
+	if(!canvasparam)
+		return synfig::Time::zero();
+
+	if (!v.parent_is_layer())
+		return synfig::Time::zero();
+
+	synfig::Layer::Handle layer = v.get_layer();
+
+	if (!etl::handle<Layer_PasteCanvas>::cast_dynamic(layer))
+		return synfig::Time::zero();
+
+	return layer->get_param("time_dilation").get(Time());
+#else // ADJUST_WAYPOINTS_FOR_TIME_OFFSET
+	return synfig::Time::zero();
+#endif
+}
+
 const synfig::Time get_time_offset_from_vdesc(const synfigapp::ValueDesc &v)
 {
 #ifdef ADJUST_WAYPOINTS_FOR_TIME_OFFSET
@@ -275,6 +300,7 @@ CellRenderer_TimeTrack::render_vfunc(
 		if(tset)
 		{
 			const synfig::Time time_offset = get_time_offset_from_vdesc(value_desc);
+			const synfig::Time time_dilation = get_time_dilation_from_vdesc(value_desc);
 			synfig::Node::time_set::const_iterator	i = tset->begin(), end = tset->end();
 
 			float 	lower = adjustment->get_lower(),
@@ -295,6 +321,9 @@ CellRenderer_TimeTrack::render_vfunc(
 				Time t_orig = i->get_time();
 				if(!t_orig.is_valid()) continue;
 				Time t = t_orig - time_offset;
+				if (time_dilation!=0)
+					t = t / time_dilation;
+				// todo: What if time_dilation == 0?
 				if(t<adjustment->get_lower() || t>adjustment->get_upper()) continue;
 
 				//if it found it... (might want to change comparison, and optimize
@@ -336,7 +365,15 @@ CellRenderer_TimeTrack::render_vfunc(
 					area.get_height()-2,
 					area.get_height()-2
 				);
-				render_time_point_to_window(cr,area2,*i - time_offset,selected);
+				if (time_dilation!=0)
+				{
+					TimePoint tp = *i;
+					tp.set_time((tp.get_time() - time_offset) / time_dilation);
+					render_time_point_to_window(cr,area2,tp,selected);
+				} else {
+					// todo: What if time_dilation == 0?
+					render_time_point_to_window(cr,area2,*i - time_offset,selected);
+				}
 			}
 
 			{
@@ -592,8 +629,9 @@ CellRenderer_TimeTrack::activate_vfunc(
 			synfigapp::ValueDesc valdesc = property_value_desc().get_value();
 			const Node::time_set *tset = get_times_from_vdesc(valdesc);
 			const synfig::Time time_offset = get_time_offset_from_vdesc(valdesc);
+			const synfig::Time time_dilation = get_time_dilation_from_vdesc(valdesc);
 
-			bool clickfound = tset && get_closest_time(*tset,actual_time+time_offset,pixel_width*cell_area.get_height(),stime);
+			bool clickfound = tset && get_closest_time(*tset,actual_time * time_dilation+time_offset,pixel_width*cell_area.get_height(),stime);
 			bool selectmode = mode & SELECT_MASK;
 
 			//NOTE LATER ON WE SHOULD MAKE IT SO MULTIPLE VALUENODES CAN BE SELECTED AT ONCE
@@ -675,8 +713,9 @@ CellRenderer_TimeTrack::activate_vfunc(
 				synfigapp::ValueDesc valdesc = property_value_desc().get_value();
 				const Node::time_set *tset = get_times_from_vdesc(valdesc);
 				synfig::Time time_offset = get_time_offset_from_vdesc(valdesc);
+				const synfig::Time time_dilation = get_time_dilation_from_vdesc(valdesc);
 
-				bool clickfound = tset && get_closest_time(*tset,actual_time+time_offset,pixel_width*cell_area.get_height(),stime);
+				bool clickfound = tset && get_closest_time(*tset,actual_time * time_dilation +time_offset,pixel_width*cell_area.get_height(),stime);
 
 				etl::handle<synfig::Node> node;
 				if(!getenv("SYNFIG_SHOW_CANVAS_PARAM_WAYPOINTS") &&
@@ -690,7 +729,7 @@ CellRenderer_TimeTrack::activate_vfunc(
 				}
 
 				if(clickfound && node)
-					signal_waypoint_clicked_cellrenderer()(node, stime, time_offset, 2);
+					signal_waypoint_clicked_cellrenderer()(node, stime, time_offset, time_dilation, 2);
 			}
 
 		break;

synfig-studio/src/gui/cellrenderer/cellrenderer_timetrack.h
@@ -84,7 +84,7 @@ private:
 	Glib::RefPtr<Gtk::Adjustment> adjustment_;
 
 	//! Signal for when the user clicks on a waypoint
-	sigc::signal<void, const etl::handle<synfig::Node>&, const synfig::Time&, const synfig::Time&, int> signal_waypoint_clicked_cellrenderer_;
+	sigc::signal<void, const etl::handle<synfig::Node>&, const synfig::Time&, const synfig::Time&, const synfig::Time&, int> signal_waypoint_clicked_cellrenderer_;
 
 	sigc::signal<void, synfig::Waypoint, synfig::ValueNode::Handle> signal_waypoint_changed_;
 
@@ -167,7 +167,7 @@ public:
 
 public:
 
-	sigc::signal<void, const etl::handle<synfig::Node>&, const synfig::Time&, const synfig::Time&, int> &signal_waypoint_clicked_cellrenderer()
+	sigc::signal<void, const etl::handle<synfig::Node>&, const synfig::Time&, const synfig::Time&, const synfig::Time&, int> &signal_waypoint_clicked_cellrenderer()
 	{return signal_waypoint_clicked_cellrenderer_; }
 
 	sigc::signal<void, synfig::Waypoint, synfig::ValueNode::Handle> &signal_waypoint_changed()

synfig-studio/src/gui/docks/dock_timetrack.cpp
@@ -378,6 +378,7 @@ public:
 	on_waypoint_clicked_timetrackview(const etl::handle<synfig::Node>& node,
 									  const synfig::Time& time,
 									  const synfig::Time& time_offset __attribute__ ((unused)),
+									  const synfig::Time& time_dilation __attribute__ ((unused)),
 									  int button)
 	{
 		std::set<synfig::Waypoint, std::less<UniqueID> > waypoint_set;

synfig-studio/src/gui/trees/childrentree.cpp
@@ -280,6 +280,7 @@ void
 ChildrenTree::on_waypoint_clicked_childrentree(const etl::handle<synfig::Node>& node __attribute__ ((unused)),
 											   const synfig::Time& time __attribute__ ((unused)),
 											   const synfig::Time& time_offset __attribute__ ((unused)),
+											   const synfig::Time& time_dilation __attribute__ ((unused)),
 											   int button __attribute__ ((unused)))
 {
 	std::set<synfig::Waypoint, std::less<UniqueID> > waypoint_set;

synfig-studio/src/gui/trees/childrentree.h
@@ -118,7 +118,7 @@ private:
 
 	void on_edited_value(const Glib::ustring&path_string,synfig::ValueBase value);
 
-	void on_waypoint_clicked_childrentree(const etl::handle<synfig::Node>& node,const synfig::Time&,const synfig::Time&,int button);
+	void on_waypoint_clicked_childrentree(const etl::handle<synfig::Node>& node,const synfig::Time&,const synfig::Time&, const synfig::Time&,int button);
 
 	bool on_tree_view_query_tooltip(int x, int y, bool keyboard_tooltip, const Glib::RefPtr<Gtk::Tooltip>& tooltip);
 

synfig-studio/src/gui/trees/layertree.cpp
@@ -776,6 +776,7 @@ void
 LayerTree::on_waypoint_clicked_layertree(const etl::handle<synfig::Node>& node __attribute__ ((unused)),
 										 const synfig::Time& time __attribute__ ((unused)),
 										 const synfig::Time& time_offset __attribute__ ((unused)),
+										 const synfig::Time& time_dilation __attribute__ ((unused)),
 										 int button __attribute__ ((unused)))
 {
 	std::set<synfig::Waypoint, std::less<UniqueID> > waypoint_set;

synfig-studio/src/gui/trees/layertree.h
@@ -181,7 +181,7 @@ private:
 
 	void on_layer_toggle(const Glib::ustring& path_string);
 
-	void on_waypoint_clicked_layertree(const etl::handle<synfig::Node>& node, const synfig::Time&, const synfig::Time&, int button);
+	void on_waypoint_clicked_layertree(const etl::handle<synfig::Node>& node, const synfig::Time&, const synfig::Time&, const synfig::Time&, int button);
 
 	void on_waypoint_changed( synfig::Waypoint waypoint , synfig::ValueNode::Handle value_node);
 

commit 9f153fc3c62fb5a492b7ea8f9e7270a9e08c1c8f

Fix moving waypoints for regular layers (was broken in a3e9cff19)

synfig-studio/src/gui/cellrenderer/cellrenderer_timetrack.cpp
@@ -122,19 +122,19 @@ const synfig::Time get_time_dilation_from_vdesc(const synfigapp::ValueDesc &v)
 #ifdef ADJUST_WAYPOINTS_FOR_TIME_OFFSET
 	if(getenv("SYNFIG_SHOW_CANVAS_PARAM_WAYPOINTS") ||
 	   v.get_value_type() != synfig::type_canvas)
-		return synfig::Time::zero();
+		return synfig::Time(1.0);
 
 	synfig::Canvas::Handle canvasparam = v.get_value().get(Canvas::Handle());
 	if(!canvasparam)
-		return synfig::Time::zero();
+		return synfig::Time(1.0);
 
 	if (!v.parent_is_layer())
-		return synfig::Time::zero();
+		return synfig::Time(1.0);
 
 	synfig::Layer::Handle layer = v.get_layer();
 
 	if (!etl::handle<Layer_PasteCanvas>::cast_dynamic(layer))
-		return synfig::Time::zero();
+		return synfig::Time(1.0);
 
 	return layer->get_param("time_dilation").get(Time());
 #else // ADJUST_WAYPOINTS_FOR_TIME_OFFSET