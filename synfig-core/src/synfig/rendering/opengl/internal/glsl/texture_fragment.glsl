80f140a9022f9b6d80556064c0f3b1de687caabb
Ivan Mahonin <bh@icystar.com>
2015-09-27 23:53:25 +0600

    GLSL files for texture shaders

---
+++ synfig-core/src/synfig/rendering/opengl/internal/glsl/texture_fragment.glsl
@@ -0,0 +1,32 @@
+/* === S Y N F I G ========================================================= */
+/*!	\file synfig/rendering/opengl/internal/glsl/color_fragment.glsl
+**	\brief Color Fragment Shader
+**
+**	$Id$
+**
+**	\legal
+**	......... ... 2015 Ivan Mahonin
+**
+**	This package is free software; you can redistribute it and/or
+**	modify it under the terms of the GNU General Public License as
+**	published by the Free Software Foundation; either version 2 of
+**	the License, or (at your option) any later version.
+**
+**	This package is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+**	General Public License for more details.
+**	\endlegal
+*/
+/* ========================================================================= */
+
+#version 330 core
+
+uniform sampler2D sampler;
+in vec2 texcoord;
+layout(location = 0) out vec4 out_color;
+
+void main()
+{
+	out_color = texture(sampler, texcoord);
+}
