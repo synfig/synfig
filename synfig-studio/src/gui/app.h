4c599d20eb4ade211e52586e2fbda76bc01808f4
Ivan Mahonin <bh@icystar.com>
2015-10-01 21:59:06 +0600

    Enable multithreading for tile rendering in GUI (old engine only)

--- synfig-studio/src/gui/app.h
+++ synfig-studio/src/gui/app.h
@@ -59,9 +59,9 @@
 #define RENDER_DIR_PREFERENCE		"render_dir"
 
 // uncomment define SINGLE_THREADED to use a single thread, and hopefully get more stability (dooglus) - changed to be default if using windows (Nov 2009 pixelgeek)
-//#ifdef WIN32
+#ifdef WIN32
 #define SINGLE_THREADED
-//#endif
+#endif
 
 /* === T Y P E D E F S ===================================================== */
 
