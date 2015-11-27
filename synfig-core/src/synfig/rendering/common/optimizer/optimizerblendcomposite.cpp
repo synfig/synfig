e02e4fe3c01747214ab687cbcd85bbd3fee36ba3
Ivan Mahonin <bh@icystar.com>
2015-11-27 20:27:46 +0600

    Improve OptimizerBlendComposite

--- synfig-core/src/synfig/rendering/common/optimizer/optimizerblendcomposite.cpp
+++ synfig-core/src/synfig/rendering/common/optimizer/optimizerblendcomposite.cpp
@@ -43,6 +43,8 @@
 #include "../task/taskblend.h"
 #include "../task/taskcomposite.h"
 #include "../task/tasklist.h"
+#include "../task/tasksurface.h"
+#include "../task/tasksurfaceempty.h"
 
 #endif
 
@@ -64,15 +66,19 @@ OptimizerBlendComposite::run(const RunParams& params) const
 	if ( blend
 	  && blend->target_surface
 	  && blend->sub_task_a()
-	  && blend->sub_task_a()->target_surface
+	  && blend->sub_task_a()->target_surface == blend->target_surface
 	  && blend->sub_task_b()
 	  && blend->sub_task_b()->target_surface )
 	{
+		// TODO: declare set of blend methods which may be optimized by way:
+		// blend(amount1, target1, blend(amount2, target2, source))
+		//     -> blend(amount1*amount2, target1, source)
+
 		// remove blend if task_b supports blending
 		TaskComposite *composite = blend->sub_task_b().type_pointer<TaskComposite>();
 		if ( composite
 		  && composite->is_blend_method_supported(blend->blend_method)
-		  && !composite->blend )
+		  && (!composite->blend || (blend->blend_method == Color::BLEND_COMPOSITE && composite->blend_method == blend->blend_method)) )
 		{
 			Task::Handle task_a = blend->sub_task_a()->clone();
 			task_a->target_surface = blend->target_surface;
@@ -95,17 +101,59 @@ OptimizerBlendComposite::run(const RunParams& params) const
 				task_b->target_rect ));
 
 			composite = task_b.type_pointer<TaskComposite>();
-			composite->blend = true;
-			composite->blend_method = blend->blend_method;
-			composite->amount = blend->amount;
+			if (composite->blend)
+			{
+				composite->amount *= blend->amount;
+			}
+			else
+			{
+				composite->blend = true;
+				composite->blend_method = blend->blend_method;
+				composite->amount = blend->amount;
+			}
 
-			TaskList::Handle task = new TaskList();
-			assign(task, Task::Handle(blend));
-			task->sub_tasks.clear();
-			task->sub_task(0) = task_a;
-			task->sub_task(1) = task_b;
+			TaskList::Handle task;
+			if (task_b.type_is<TaskSurface>() || task_b.type_is<TaskSurfaceEmpty>())
+			{
+				task = task_b;
+			}
+			else
+			{
+				task = new TaskList();
+				assign(task, Task::Handle(blend));
+				task->sub_tasks.clear();
+				task->sub_task(0) = task_a;
+				task->sub_task(1) = task_b;
+			}
 
 			apply(params, task);
+			return;
+		}
+
+		// TODO: Derive TaskBlend from TaskComposite,
+		// possible problems: fields TaskBlend::offset_*, we cannot change target_rect
+
+		// remove blend if task_b is also TaskBlend
+		TaskBlend::Handle sub_blend = TaskBlend::Handle::cast_dynamic(blend->sub_task_b());
+		if ( sub_blend
+		  && blend->blend_method == Color::BLEND_COMPOSITE
+		  && sub_blend->blend_method == blend->blend_method
+		  && (sub_blend->sub_task_a().type_is<TaskSurface>() || sub_blend->sub_task_a().type_is<TaskSurfaceEmpty>()) )
+		{
+			TaskBlend::Handle new_blend = TaskBlend::Handle::cast_dynamic(sub_blend->clone());
+			new_blend->target_surface = blend->target_surface;
+			new_blend->target_rect += blend->target_rect.get_min() + blend->offset_b;
+			new_blend->offset_a = -new_blend->target_rect.get_min();
+
+			new_blend->sub_task_a() = new TaskSurface();
+			assign(new_blend->sub_task_a(), sub_blend->sub_task_a());
+			new_blend->sub_task_a()->target_surface = blend->target_surface;
+			new_blend->sub_task_a()->target_rect += sub_blend->offset_a - new_blend->offset_a;
+
+			new_blend->amount *= blend->amount;
+
+			apply(params, new_blend);
+			return;
 		}
 	}
 }
