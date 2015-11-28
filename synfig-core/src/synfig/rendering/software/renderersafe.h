6d9726794061b4fb37847a72bf5dccc2fd4f03bb
Ivan Mahonin <bh@icystar.com>
2015-11-28 16:41:11 +0600

    RendererSafe

---
+++ synfig-core/src/synfig/rendering/software/renderersafe.h
@@ -0,0 +1,62 @@
+/* === S Y N F I G ========================================================= */
+/*!	\file synfig/rendering/software/renderersafe.h
+**	\brief RendererSafe Header
+**
+**	$Id$
+**
+**	\legal
+**	......... ... 2015 Ivan Mahonin
+**
+**	This package is free software; you can redistribute it and/or
+**	modify it under the terms of the GNU General Public License as
+**	published by the Free Software Foundation; either version 2 of
+**	the License, or (at your option) any later version.
+**
+**	This package is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+**	General Public License for more details.
+**	\endlegal
+*/
+/* ========================================================================= */
+
+/* === S T A R T =========================================================== */
+
+#ifndef __SYNFIG_RENDERING_RENDERERSAFE_H
+#define __SYNFIG_RENDERING_RENDERERSAFE_H
+
+/* === H E A D E R S ======================================================= */
+
+#include "../renderer.h"
+
+/* === M A C R O S ========================================================= */
+
+/* === T Y P E D E F S ===================================================== */
+
+/* === C L A S S E S & S T R U C T S ======================================= */
+
+namespace synfig
+{
+namespace rendering
+{
+
+class RendererSafe: public Renderer
+{
+public:
+	typedef etl::handle<RendererSafe> Handle;
+
+	RendererSafe();
+	~RendererSafe();
+
+	virtual String get_name() const;
+
+	static void initialize() { }
+	static void deinitialize() { }
+};
+
+}; /* end namespace rendering */
+}; /* end namespace synfig */
+
+/* -- E N D ----------------------------------------------------------------- */
+
+#endif
