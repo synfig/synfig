58ef4d7a932f9654d39f535e255b167201323e9f
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2015-12-22 22:43:30 +0600

    Linux build: Add libfftw3 dependency (Debian/Ubuntu)

--- autobuild/synfigstudio-linux-build.sh
+++ autobuild/synfigstudio-linux-build.sh
@@ -969,6 +969,7 @@ initialize()
 					libtiff-devel \
 					libjasper-devel \
 					libdirectfb-devel \
+					libfftw3-dev \
 					libXfixes-devel \
 					libXinerama-devel \
 					libXdamage-devel \
