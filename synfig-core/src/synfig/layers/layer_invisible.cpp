75fae5b243e75155901960da85014d1955967202
Ivan Mahonin <bh@icystar.com>
2016-02-13 14:37:07 +0600

    Add missing files

---
+++ synfig-core/src/synfig/layers/layer_invisible.cpp
@@ -0,0 +1,67 @@
+/* === S Y N F I G ========================================================= */
+/*!	\file layer_invisible.cpp
+**	\brief Template File
+**
+**	$Id$
+**
+**	\legal
+**	......... ... 2016 Ivan Mahonin
+**
+**	This package is free software; you can redistribute it and/or
+**	modify it under the terms of the GNU General Public License as
+**	published by the Free Software Foundation; either version 2 of
+**	the License, or (at your option) any later version.
+**
+**	This package is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+**	General Public License for more details.
+**	\endlegal
+*/
+/* ========================================================================= */
+
+/* === H E A D E R S ======================================================= */
+
+#ifdef USING_PCH
+#	include "pch.h"
+#else
+#ifdef HAVE_CONFIG_H
+#	include <config.h>
+#endif
+
+#include <synfig/general.h>
+#include <synfig/localization.h>
+#include <synfig/context.h>
+
+#include "layer_invisible.h"
+
+#endif
+
+/* === U S I N G =========================================================== */
+
+using namespace std;
+using namespace etl;
+using namespace synfig;
+
+/* === M A C R O S ========================================================= */
+
+/* === G L O B A L S ======================================================= */
+
+/* === P R O C E D U R E S ================================================= */
+
+/* === M E T H O D S ======================================================= */
+
+Layer_Invisible::Layer_Invisible() { }
+
+bool
+Layer_Invisible::accelerated_render(Context context, Surface *surface,int quality, const RendDesc &renddesc, ProgressCallback *cb)const
+	{ return context.accelerated_render(surface,quality,renddesc,cb); }
+
+
+bool
+Layer_Invisible::accelerated_cairorender(Context context, cairo_t *cr, int quality, const RendDesc &renddesc, ProgressCallback *cb)const
+	{ return context.accelerated_cairorender(cr,quality,renddesc,cb); }
+
+rendering::Task::Handle
+Layer_Invisible::build_rendering_task_vfunc(Context context)const
+	{ return context.build_rendering_task(); }
---
+++ synfig-core/src/synfig/layers/layer_invisible.h
@@ -0,0 +1,62 @@
+/* === S Y N F I G ========================================================= */
+/*!	\file layer_invisible.h
+**	\brief Invisible Layer Class Implementation
+**
+**	$Id$
+**
+**	\legal
+**	......... ... 2016 Ivan Mahonin
+**
+**	This package is free software; you can redistribute it and/or
+**	modify it under the terms of the GNU General Public License as
+**	published by the Free Software Foundation; either version 2 of
+**	the License, or (at your option) any later version.
+**
+**	This package is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+**	General Public License for more details.
+**	\endlegal
+*/
+/* ========================================================================= */
+
+/* === S T A R T =========================================================== */
+
+#ifndef __SYNFIG_LAYER_INVISIBLE_H
+#define __SYNFIG_LAYER_INVISIBLE_H
+
+/* === H E A D E R S ======================================================= */
+
+#include <synfig/layer.h>
+
+/* === M A C R O S ========================================================= */
+
+/* === T Y P E D E F S ===================================================== */
+
+/* === C L A S S E S & S T R U C T S ======================================= */
+
+namespace synfig {
+
+/*!	\class Layer_Invisible
+**	\brief Base class for invisible layers
+*/
+class Layer_Invisible : public Layer
+{
+protected:
+	//! Default constructor. Not used directly.
+	Layer_Invisible();
+
+public:
+	//! Renders the layer composited on the context and puts it on the target surface.
+	virtual bool accelerated_render(Context context,Surface *surface,int quality, const RendDesc &renddesc, ProgressCallback *cb)const;
+	virtual bool accelerated_cairorender(Context context, cairo_t *cr, int quality, const RendDesc &renddesc, ProgressCallback *cb)const;
+
+protected:
+	virtual rendering::Task::Handle build_rendering_task_vfunc(Context context)const;
+}; // END of class Layer_Composite
+
+}; // END of namespace synfig
+
+/* === E N D =============================================================== */
+
+#endif
