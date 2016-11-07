b661cd797f04a4e0175e23d8d50b29dc49a7ca1e
caryoscelus <caryoscelus@gmx.com>
2016-11-07 12:21:38 +0300

    Synfig core CMake: install more headers in a more proper way

---
+++ synfig-core/src/cmake/install_header.cmake
@@ -0,0 +1,9 @@
+## TODO: deduce DEST from CMAKE_CURRENT_LIST_DIR
+function(install_all_headers DEST)
+    file(GLOB HEADERS "${CMAKE_CURRENT_LIST_DIR}/*.h")
+
+    install(
+        FILES ${HEADERS}
+        DESTINATION include/synfig/${DEST}
+    )
+endfunction(install_all_headers)
