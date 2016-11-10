synfig/synfig

commit f05e096dc31f0906c5bd8b2929728994c185575e

Windows build: remove -allow-multiple-definition flag

autobuild/fedora-crosscompile-win.sh
@@ -612,7 +612,7 @@ make clean || true
 [ ! -e config.cache ] || rm config.cache
 /bin/sh ./bootstrap.sh
 cp ./configure ./configure.real
-echo -e "#/bin/sh \n export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig:/usr/${TOOLCHAIN_HOST}/sys-root/mingw/lib/pkgconfig/:$PKG_CONFIG_PATH \n export CXXFLAGS=\" -Wl,-allow-multiple-definition \" \n ./configure.real \$@  \n " > ./configure
+echo -e "#/bin/sh \n export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig:/usr/${TOOLCHAIN_HOST}/sys-root/mingw/lib/pkgconfig/:$PKG_CONFIG_PATH \n " > ./configure
 chmod +x ./configure
 ${TOOLCHAIN}-configure \
 --prefix=${PREFIX} \

commit 20924fab86551538e3ab2fc15b5dc437191058e2

Windows build: Fix error introduced in f05e096d

autobuild/fedora-crosscompile-win.sh
@@ -612,7 +612,7 @@ make clean || true
 [ ! -e config.cache ] || rm config.cache
 /bin/sh ./bootstrap.sh
 cp ./configure ./configure.real
-echo -e "#/bin/sh \n export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig:/usr/${TOOLCHAIN_HOST}/sys-root/mingw/lib/pkgconfig/:$PKG_CONFIG_PATH \n " > ./configure
+echo -e "#/bin/sh \n export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig:/usr/${TOOLCHAIN_HOST}/sys-root/mingw/lib/pkgconfig/:$PKG_CONFIG_PATH \n ./configure.real \$@  \n " > ./configure
 chmod +x ./configure
 ${TOOLCHAIN}-configure \
 --prefix=${PREFIX} \