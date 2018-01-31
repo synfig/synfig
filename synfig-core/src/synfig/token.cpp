29b3123d1864ca8dc37737ffd99df699f1216b51
Ivan Mahonin <bh@icystar.com>
2018-01-31 22:21:25 +0700

    refactor rendering: token

---
+++ synfig-core/src/synfig/token.cpp
@@ -0,0 +1,173 @@
+/* === S Y N F I G ========================================================= */
+/*!	\file token.cpp
+**	\brief Token File
+**
+**	$Id$
+**
+**	\legal
+**	......... ... 2018 Ivan Mahonin
+**
+**	This package is free software; you can redistribute it and/or
+**	modify it under the terms of the GNU General Public License as
+**	published by the Free Software Foundation; either version 2 of
+**	the License, or (at your option) any later version.
+**
+**	This package is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+**	General Public License for more details.
+**	\endlegal
+*/
+/* ========================================================================= */
+
+/* === H E A D E R S ======================================================= */
+
+#ifdef USING_PCH
+#	include "pch.h"
+#else
+#ifdef HAVE_CONFIG_H
+#	include <config.h>
+#endif
+
+#include <synfig/localization.h>
+#include <synfig/general.h>
+
+#include "token.h"
+
+#endif
+
+/* === U S I N G =========================================================== */
+
+using namespace std;
+using namespace synfig;
+
+/* === M A C R O S ========================================================= */
+
+/* === G L O B A L S ======================================================= */
+
+/* === M E T H O D S ======================================================= */
+
+Token Token::token;
+
+Token::Token(const Token&):
+	previous_(), next_(), in_process_(), prepared_() { }
+
+Token::Token(Handle parent):
+	previous_(last_),
+	next_(),
+	in_process_()
+{
+	parents_.insert(parent);
+	init();
+}
+
+Token::Token(const Set &parents):
+	previous_(last_),
+	next_(),
+	in_process_(),
+	parents_(parents)
+{ init(); }
+
+void
+Token::init()
+{
+	ready_ = false;
+	(previous_ ? previous_->next_ : first_) = this;
+	last_ = this;
+}
+
+Token::~Token()
+{
+	ready_ = false;
+	(previous_ ? previous_->next_ : first_) = next_;
+	(next_     ? next_->previous_ : last_ ) = previous_;
+
+	// remove all references
+	for(Token* token = first_; token; token = token->next_)
+		token->parents_.erase(handle());
+}
+
+void
+Token::fill_all_parents()
+{
+	if (parents_.empty() || !all_parents_.empty())
+		return;
+
+	if (in_process_)
+	{
+		error(String(_("Loop in hierarchy of tokens detected")));
+		assert(false);
+		return;
+	}
+
+	in_process_ = true;
+	for(Set::iterator i = parents_.begin(); i != parents_.end(); ++i)
+	{
+		Token &t = cast_const(*i);
+		all_parents_.insert( t.handle() );
+		t.fill_all_parents();
+		all_parents_.insert( t.all_parents_.begin(), t.all_parents_.end() );
+	}
+	in_process_ = false;
+}
+
+void
+Token::prepare()
+{
+	if (prepared_) return;
+	if (in_process_)
+	{
+		error(String(_("Loop detected while tokens preparing")));
+		assert(false);
+		return;
+	}
+
+	prepare_vfunc();
+
+	in_process_ = true;
+	prepared_ = true;
+	in_process_ = false;
+}
+
+void
+Token::rebuild()
+{
+	if (ready_) return;
+	assert(root_exists_);
+
+	// clear
+	for(Token* t = first_; t; t = t->next_)
+	{
+		t->unprepare_vfunc();
+		t->prepared_ = false;
+		t->children_.clear();
+		t->all_parents_.clear();
+		t->all_children_.clear();
+	}
+
+	// add root
+	if (root_exists_)
+		for(Token* t = first_; t; t = t->next_)
+			if ( t->parents_.empty()
+			  && t->handle() != token.handle() )
+				t->parents_.insert( Token::token.handle() );
+
+	// fill parents
+	for(Token* t = first_; t; t = t->next_)
+		t->fill_all_parents();
+
+	// fill children
+	for(Token* t = first_; t; t = t->next_)
+	{
+		for(Set::iterator i = t->parents_.begin(); i != t->parents_.end(); ++i)
+			cast_const(*i).children_.insert( t->handle() );
+		for(Set::iterator i = t->all_parents_.begin(); i != t->all_parents_.end(); ++i)
+			cast_const(*i).all_children_.insert( t->handle() );
+	}
+
+	// prepare
+	for(Token* t = first_; t; t = t->next_)
+		t->prepare();
+
+	ready_ = true;
+}
---
+++ synfig-core/src/synfig/token.h
@@ -0,0 +1,233 @@
+/* === S Y N F I G ========================================================= */
+/*!	\file token.h
+**	\brief Token Header
+**
+**	$Id$
+**
+**	\legal
+**	......... ... 2018 Ivan Mahonin
+**
+**	This package is free software; you can redistribute it and/or
+**	modify it under the terms of the GNU General Public License as
+**	published by the Free Software Foundation; either version 2 of
+**	the License, or (at your option) any later version.
+**
+**	This package is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+**	General Public License for more details.
+**	\endlegal
+*/
+/* ========================================================================= */
+
+/* === S T A R T =========================================================== */
+
+#ifndef __SYNFIG_TOKEN_H
+#define __SYNFIG_TOKEN_H
+
+/* === H E A D E R S ======================================================= */
+
+#include <set>
+#include <map>
+#include <cassert>
+
+#include "string.h"
+
+/* === M A C R O S ========================================================= */
+
+/* === T Y P E D E F S ===================================================== */
+
+/* === C L A S S E S & S T R U C T S ======================================= */
+
+namespace synfig {
+
+template<typename T>
+class ConstRef {
+public:
+	typedef T Type;
+
+private:
+	const Type *pointer;
+
+public:
+	inline ConstRef():
+		pointer() { }
+	inline explicit ConstRef(const Type &x):
+		pointer(&x) { }
+
+	template<typename TT>
+	inline ConstRef(const ConstRef<TT> &x):
+		pointer(x.pointer) { }
+
+	template<typename TT>
+	inline ConstRef<TT> as() const
+	{
+		TT *p = dynamic_cast<TT*>(pointer);
+		return p ? ConstRef<TT>(*p) : ConstRef<TT>();
+	}
+
+	inline void reset()
+		{ *pointer = NULL; }
+	inline bool is_valid() const
+		{ return pointer; }
+
+	inline const Type* operator-> () const
+		{ assert(pointer); return pointer; }
+	inline operator bool() const
+		{ return is_valid; }
+
+	inline bool operator== (const ConstRef& other) const
+		{ return pointer == other.pointer; }
+	inline bool operator!= (const ConstRef& other) const
+		{ return pointer != other.pointer; }
+	inline bool operator< (const ConstRef& other) const
+		{ return pointer < other.pointer; }
+};
+
+
+class Token {
+public:
+	typedef ConstRef<Token> Handle;
+	typedef std::set<Handle> Set;
+	typedef std::map<String, String> MetaData;
+
+	class SetBuilder {
+	public:
+		Set set;
+		inline SetBuilder() { }
+		inline explicit SetBuilder(Handle x)
+			{ add(x); }
+		inline SetBuilder& add(Handle x)
+			{ set.insert(x); return *this; }
+	};
+
+
+private:
+	static bool ready_;
+	static bool root_exists_;
+	static Token *first_;
+	static Token *last_;
+	Token *previous_;
+	Token *next_;
+
+	bool in_process_;
+	bool prepared_;
+
+	Set parents_;
+	Set children_;
+	Set all_parents_;
+	Set all_children_;
+
+	Token(const Token&);
+	Token& operator= (const Token&) { return *this; }
+
+	inline Token& cast_const(Handle handle) const
+		{ return *const_cast<Token*>(this); }
+
+	void init();
+	void fill_all_parents();
+
+protected:
+	virtual void prepare_vfunc();
+	virtual void unprepare_vfunc();
+
+public:
+	static Token token;
+
+	Token();
+	explicit Token(Handle parent);
+	explicit Token(const Set &parents);
+	virtual ~Token();
+
+	inline Handle handle() const
+		{ return Handle(*this); }
+
+	inline const Set& parents() const
+		{ assert(ready_); return parents_; }
+	inline const Set& children() const
+		{ assert(ready_); return children_; }
+	inline const Set& all_parents() const
+		{ assert(ready_); return all_parents_; }
+	inline const Set& all_children() const
+		{ assert(ready_); return all_children_; }
+
+	inline bool is_parent_of(Handle token) const
+		{ return all_children().count(token); }
+	inline bool is_child_of(Handle token) const
+		{ return all_parents().count(token); }
+	inline bool is_equal(Handle token) const
+		{ return handle() == token; }
+	inline bool is(Handle token) const
+		{ return is_equal(token) || is_child_of(token); }
+
+	void prepare();
+	static void rebuild();
+};
+
+/*
+===============================================================================
+
+Sample implementation for Layers
+
+===============================================================================
+
+class MyLayer {
+public:
+	typedef MyLayer* (*Fabric)();
+
+	class Token: public synfig::Token {
+	public:
+		typedef ConstRef<Token> Handle;
+
+		const Fabric fabric;
+		const String name;
+		const String category;
+
+	private:
+		template<typename T>
+		T* fabric_template()
+			{ return new T(); }
+
+	public:
+		template<typename TypeThis, typename TypeParent>
+		Token(
+			const String &name,
+			const String &category = String()
+		):
+			synfig::Token(TypeParent::token.handle()),
+			fabric(&fabric_template<TypeThis>),
+			name(name),
+			category(category)
+		{ }
+
+		inline Handle handle() const
+			{ return Handle(*this); }
+	};
+
+	static synfig::Token token;
+
+	virtual ~MyLayer() { }
+	virtual Token::Handle get_token() = 0;
+};
+
+synfig::Token MyLayer::token;
+
+class MyRegion: public MyLayer {
+public:
+	static Token token;
+	virtual Token::Handle get_token()
+		{ return token.handle(); }
+};
+
+MyLayer::Token MyRegion::token<MyRegion, MyLayer>(
+	"region",
+	"common" );
+
+}; // END of namespace synfig
+
+===============================================================================
+*/
+
+/* === E N D =============================================================== */
+
+#endif
