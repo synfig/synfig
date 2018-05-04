fe46b9f31deb8864d999f8cbaae9466c9bb630f6
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2018-05-04 16:45:30 +0700

    Utility script to bump version number in source files
    Usage:
    release-bump.sh NEW_VERSION_NUMBER

---
+++ release-bump.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+set -e
+
+DIRNAME=`dirname "$0"`
+cd $DIRNAME
+DIRNAME=`pwd`
+
+if [ -z "$1" ]; then
+	echo "USAGE: $0 VERSION"
+	exit 1
+fi
+
+VERSION_NEW="$1"
+
+# Get current version
+VERSION_CURRENT=`cat synfig-studio/configure.ac | grep "AC_INIT(\[Synfig Studio\]" | sed 's/.*Studio\],\[\(.*\)\],\[http.*/\1/'`
+
+sed -i "s|Library\],\[${VERSION_CURRENT}\],\[http|Library\],\[${VERSION_NEW}\],\[http|" ETL/configure.ac
+
+sed -i "s|Core\],\[${VERSION_CURRENT}\],\[http|Core\],\[${VERSION_NEW}\],\[http|" synfig-core/configure.ac
+sed -i "s|ETL >= ${VERSION_CURRENT}|ETL >= ${VERSION_NEW}|" synfig-core/configure.ac
+sed -i "s|version ${VERSION_CURRENT} or|version ${VERSION_NEW} or|" synfig-core/configure.ac
+
+
+sed -i "s|Studio\],\[${VERSION_CURRENT}\],\[http|Studio\],\[${VERSION_NEW}\],\[http|" synfig-studio/configure.ac
+sed -i "s|ETL >= ${VERSION_CURRENT}|ETL >= ${VERSION_NEW}|" synfig-studio/configure.ac
+sed -i "s|synfig >= ${VERSION_CURRENT}|synfig >= ${VERSION_NEW}|" synfig-studio/configure.ac
+
+
+sed -i "s|STUDIO_VERSION \"${VERSION_CURRENT}\"|STUDIO_VERSION \"${VERSION_NEW}\"|" synfig-studio/src/gui/CMakeLists.txt
+
+git add \
+	ETL/configure.ac \
+	synfig-core/configure.ac \
+	synfig-studio/configure.ac \
+	synfig-studio/src/gui/CMakeLists.txt \
+	# end
+
+git commit -m "Development version ${VERSION_NEW}"
