8815a722e7888c2c4ac41d146796fb84fad84e36
ice0 <konoplin@gmail.com>
2018-08-04 16:58:51 +0600

    Fixed static analyzer errors (Accelerator key, DockManager and LayerEncapsulate)

--- synfig-studio/src/synfigapp/actions/layerencapsulate.cpp
+++ synfig-studio/src/synfigapp/actions/layerencapsulate.cpp
@@ -166,7 +166,7 @@ Action::LayerEncapsulate::prepare()
 	if (!description.empty()) new_layer->set_description(description);
 	new_layer->set_param("canvas",child_canvas);
         
-        new_layer->set_param("children_lock",children_lock);
+        new_layer->set_param("children_lock", ValueBase(children_lock));
 
 	int target_depth(lowest_depth());
 
@@ -195,11 +195,11 @@ Action::LayerEncapsulate::prepare()
 		add_action(action);
 	}
 
-	std::list<synfig::Layer::Handle>::reverse_iterator iter;
+	std::list<synfig::Layer::Handle>::reverse_iterator i;
 
-	for(iter=layers.rbegin();iter!=layers.rend();++iter)
+	for(i=layers.rbegin();i!=layers.rend();++i)
 	{
-		Layer::Handle layer(*iter);
+		Layer::Handle layer(*i);
 
 		Canvas::Handle subcanvas(layer->get_canvas());
 
