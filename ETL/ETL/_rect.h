f809b0189d6d4acc156aec064fd8b54a8de365a3
Ivan Mahonin <bh@icystar.com>
2018-08-06 20:18:54 +0700

    Refactor etl::rect

--- ETL/ETL/_rect.h
+++ ETL/ETL/_rect.h
@@ -4,6 +4,7 @@
 ** $Id$
 **
 ** Copyright (c) 2002 Adrian Bentley
+** ......... ... 2018 Ivan Mahonin
 **
 ** This package is free software; you can redistribute it and/or
 ** modify it under the terms of the GNU General Public License as
@@ -40,144 +41,134 @@
 
 _ETL_BEGIN_NAMESPACE
 
-template < typename T >
+template<typename T>
 class rect
 {
-public: //type niceties
-	typedef T	value_type;
+public:
+	typedef T value_type;
 
-public: //representation
+	value_type minx, miny, maxx, maxy;
 
-	value_type	minx,maxx,miny,maxy;
+	rect():
+		minx(), miny(), maxx(), maxy() { }
+	rect(const value_type &x, const value_type &y):
+		minx(x), miny(y), maxx(x), maxy(y) { }
 
-public: //interface
+	template<typename U>
+	rect(const rect<U> &o):
+		minx(o.minx), miny(o.miny), maxx(o.maxx), maxy(o.maxy) { }
+	rect(const rect<T> &o):
+		minx(o.minx), miny(o.miny), maxx(o.maxx), maxy(o.maxy) { }
 
-	rect() {}
+	template<typename F>
+	rect(
+		const value_type &x0,
+		const value_type &y0,
+		const value_type &x1,
+		const value_type &y1,
+		const F &less
+	):
+		minx(x0), miny(y0), maxx(x0), maxy(y0)
+		{ expand(x1, y1, less); }
+	rect(
+		const value_type &x0,
+		const value_type &y0,
+		const value_type &x1,
+		const value_type &y1
+	):
+		minx(x0), miny(y0), maxx(x0), maxy(y0)
+		{ expand(x1, y1); }
 
-	rect(const value_type &x1,const value_type &y1)
-	{
-		set_point(x1,y1);
-	}
-
-	rect(const value_type &x1,const value_type &y1,
-			const value_type &x2,const value_type &y2)
-	{
-		set_point(x1,y1);
-		expand(x2,y2);
-	}
-
-	rect(const rect<T> &o)
-	:minx(o.minx),maxx(o.maxx),miny(o.miny),maxy(o.maxy)
-	{}
-
-	template < typename U >
-	rect(const rect<U> &o)
-	:minx(o.minx),maxx(o.maxx),miny(o.miny),maxy(o.maxy)
-	{}
-
-	void set_point(const value_type &x1,const value_type &y1)
-	{
-		minx = maxx = x1;
-		miny = maxy = y1;
-	}
-
-	void expand(const value_type &x1,const value_type &y1)
-	{
-		minx = std::min(minx,x1);
-		maxx = std::max(maxx,x1);
-		miny = std::min(miny,y1);
-		maxy = std::max(maxy,y1);
-	}
-
-	void set(const value_type &x1,const value_type &y1,
-			const value_type &x2,const value_type &y2)
-	{
-		minx = x1; maxx = x2;
-		miny = y1; maxy = y2;
-	}
-
-	//HACK HACK HACK (stupid compiler doesn't like default arguments of any type)
+	template<typename F>
+	bool valid(const F &less) const
+		{ return less(minx, maxx) && less(miny, maxy); }
 	bool valid() const
-	{
-		return valid(std::less<T>());
-	}
+		{ return valid(std::less<T>()); }
 
-	template < typename F >
-	bool valid(const F & func) const
+	void set(
+		const value_type &x0,
+		const value_type &y0,
+		const value_type &x1,
+		const value_type &y1 )
+		{ minx = x0; miny = y0; maxx = x1; maxy = y1; }
+	void set_point(const value_type &x, const value_type &y)
+		{ minx = maxx = x; miny = maxy = y; }
+
+	template<typename F>
+	void expand(const value_type &x1, const value_type &y1, const F &less)
 	{
-		return func(minx,maxx) && func(miny,maxy);
+		minx = std::min(minx, x1, less);
+		miny = std::min(miny, y1, less);
+		maxx = std::max(maxx, x1, less);
+		maxy = std::max(maxy, y1, less);
 	}
+	void expand(const value_type &x1, const value_type &y1)
+		{ expand(x1, y1, std::less<T>()); }
 };
 
-template < typename T, typename F >
-inline bool intersect(const rect<T> &r1, const rect<T> &r2, const F & func)
+
+//! We wan to do the edge compare test
+//!          |-----|
+//!    |------|        intersecting
+//!
+//!    |-----|
+//!            |-----| not intersecting
+//!
+//! So we want to compare the mins of the one against the maxs of the other, and visa versa.
+//! By default (exclude edge sharing) less will not be true if they are equal...
+template<typename T, typename F>
+inline bool intersect(const rect<T> &r1, const rect<T> &r2, const F &less)
 {
-	/* We wan to do the edge compare test
-			  |-----|
-		|------|	 intersecting
-
-		|-----|
-				|-----| not intersecting
-
-		So we want to compare the mins of the one against the maxs of the other, and
-		visa versa
-
-		by default (exclude edge sharing) less will not be true if they are equal...
-	*/
-
-	return func(r1.minx,r2.maxx) &&
-           func(r2.minx,r1.maxx) &&
-           func(r1.miny,r2.maxy) &&
-           func(r2.miny,r1.maxy);
+	return less(r1.minx, r2.maxx) &&
+		   less(r2.minx, r1.maxx) &&
+		   less(r1.miny, r2.maxy) &&
+		   less(r2.miny, r1.maxy);
 }
-
-template < typename T >
+template<typename T>
 inline bool intersect(const rect<T> &r1, const rect<T> &r2)
+	{ return intersect(r1, r2, std::less<T>()); }
+
+
+template<typename T, typename F>
+inline bool contains(const rect<T> &big, const rect<T> &small, const F &less)
 {
-	return intersect(r1,r2,std::less<T>());
+	return !less(small.minx, big.minx) &&
+		   !less(big.maxx, small.maxx) &&
+		   !less(small.miny, big.miny) &&
+		   !less(big.maxy, small.maxy);
 }
-
-
-template < typename T, typename F >
-inline bool contains(const rect<T> &big, const rect<T> &small, const F & func)
-{
-	return !func(small.minx, big.minx) &&
-		   !func(big.maxx, small.maxx) &&
-		   !func(small.miny, big.miny) &&
-		   !func(big.maxy, small.maxy);
-}
-
-template < typename T >
+template<typename T>
 inline bool contains(const rect<T> &big, const rect<T> &small)
+	{ return contains(big, small, std::less<T>()); }
+
+
+//! Takes the intersection of the two rectangles
+template<typename T, typename F>
+void set_intersect(rect<T> &rout, const rect<T> &r1, const rect<T> &r2, const F &less)
 {
-	return contains(big,small,std::less<T>());
+	rout.minx = std::max(r1.minx, r2.minx, less);
+	rout.miny = std::max(r1.miny, r2.miny, less);
+	rout.maxx = std::min(r1.maxx, r2.maxx, less);
+	rout.maxy = std::min(r1.maxy, r2.maxy, less);
 }
-
-
-template < typename T >
+template<typename T>
 void set_intersect(rect<T> &rout, const rect<T> &r1, const rect<T> &r2)
-{
-	//takes the intersection of the two rectangles
-	rout.minx = std::max(r1.minx,r2.minx);
-	rout.miny = std::max(r1.miny,r2.miny);
-	rout.maxx = std::min(r1.maxx,r2.maxx);
-	rout.maxy = std::min(r1.maxy,r2.maxy);
-}
+	{ return set_intersect(rout, r1, r2, std::less<T>()); }
 
-template < typename T >
-void set_union(rect<T> &rout, const rect<T> &r1, const rect<T> &r2)
+
+//! Takes the union of the two rectangles (bounds both... will contain extra info, but that's ok)
+template<typename T, typename F>
+void set_union(rect<T> &rout, const rect<T> &r1, const rect<T> &r2, const F &less)
 {
-	//takes the union of the two rectangles (bounds both... will contain extra info, but that's ok)
-	rout.set(
-		std::min(r1.minx,r2.minx),
-		std::min(r1.miny,r2.miny),
-		std::max(r1.maxx,r2.maxx),
-		std::max(r1.maxy,r2.maxy));
-	/*rect<T> local = r1;
-	rout.expand(r2.minx,r2.miny);
-	rout.expand(r2.maxx,r2.maxy);
-	rout = local;*/
+	rout.minx = std::min(r1.minx, r2.minx, less);
+	rout.miny = std::min(r1.miny, r2.miny, less);
+	rout.maxx = std::max(r1.maxx, r2.maxx, less);
+	rout.maxy = std::max(r1.maxy, r2.maxy, less);
 }
+template<typename T>
+void set_union(rect<T> &rout, const rect<T> &r1, const rect<T> &r2)
+	{ set_union(rout, r1, r2, std::less<T>()); }
+
 
 template<typename List, typename T, typename F>
 void rects_subtract(List &list, const rect<T> &r, const F &less)
@@ -185,52 +176,39 @@ void rects_subtract(List &list, const rect<T> &r, const F &less)
 	typedef typename List::value_type Rect;
 
 	if (!r.valid(less)) return;
-	for(typename List::iterator i = list.begin(); i != list.end();)
-	{
-		if (intersect(*i, r))
-		{
-			Rect &x = *i;
-			Rect y;
-			y.minx = std::max(x.minx, r.maxx);
-			y.maxx = std::min(x.maxx, r.minx);
-			y.miny = std::max(x.miny, r.maxy);
-			y.maxy = std::min(x.maxy, r.miny);
-
+	for(typename List::iterator i = list.begin(); i != list.end(); ) {
+		Rect &x = *i;
+		Rect y;
+		set_intersect(y, x, r, less);
+		if (less(y.minx, y.maxx) && less(y.miny, y.maxy)) {
 			T rects[][4] = {
-				{ x.minx, y.maxx, x.miny, x.maxy },
-				{ y.minx, x.maxx, x.miny, x.maxy },
-				{ y.minx, y.maxx, x.miny, y.maxy },
-				{ y.minx, y.maxx, y.miny, x.maxy }
-			};
-
+				{ x.minx, x.miny, y.minx, x.maxy },
+				{ y.maxx, x.miny, x.maxx, x.maxy },
+				{ y.minx, x.miny, y.maxx, y.miny },
+				{ y.minx, y.maxy, y.maxx, x.maxy } };
 			const int count = sizeof(rects)/sizeof(rects[0]);
 
-			bool inserted = false;
-			for(int j = 0; j < count; ++j)
-			{
-				if ( less(rects[j][0], rects[j][1])
-				  && less(rects[j][2], rects[j][3]) )
+			i = list.erase(i);
+			for(int j = 0; j < count; ++j) {
+				if ( less(rects[j][0], rects[j][2])
+				  && less(rects[j][1], rects[j][3]) )
 				{
 					Rect rr;
 					rr.minx = rects[j][0];
-					rr.maxx = rects[j][1];
-					rr.miny = rects[j][2];
+					rr.miny = rects[j][1];
+					rr.maxx = rects[j][2];
 					rr.maxy = rects[j][3];
-					if (inserted)
-						i = list.insert(++i, rr);
-					else
-						*i = rr, inserted = true;
+					i = list.insert(i, rr);
+					++i;
 				}
 			}
-			if (!inserted) { i = list.erase(i); continue; }
-		}
-		++i;
+		} else ++i;
 	}
 }
-
 template<typename List, typename T>
 void rects_subtract(List &list, const rect<T> &r)
 	{ rects_subtract(list, r, std::less<T>()); }
+
 
 template<typename List, typename T, typename F>
 void rects_add(List &list, const rect<T> &r, const F &less)
@@ -239,25 +217,38 @@ void rects_add(List &list, const rect<T> &r, const F &less)
 	rects_subtract(list, r, less);
 	list.insert(list.end(), r);
 }
-
 template<typename List, typename T>
 void rects_add(List &list, const rect<T> &r)
 	{ rects_add(list, r, std::less<T>()); }
 
+
 template<typename List, typename F>
 void rects_merge(List &list, const F &less)
 {
-	for(typename List::iterator i = list.begin(); i != list.end();)
-		if (!i->valid(less)) i = list.erase(i); else ++i;
+	for(typename List::iterator i = list.begin(); i != list.end(); )
+		if (!less(i->minx, i->maxx) || !less(i->miny, i->maxy))
+			i = list.erase(i); else ++i;
 
 	bool merged_any = true;
-	while(merged_any)
-	{
+	while(merged_any) {
 		merged_any = false;
-		for(typename List::iterator i = list.begin(); i != list.end();)
+		for(typename List::iterator i = list.begin(); i != list.end(); )
 		{
 			bool merged_current = false;
-			for(typename List::iterator j = list.begin(); j != list.end(); ++j)
+			for(typename List::iterator j = list.begin(); j != list.end(); ++j) {
+				if (i == j) continue;
+
+				// merge horizontal
+				if ( !less(i->maxx, j->minx) && !less(j->minx, i->maxx)
+				  && !less(i->miny, j->miny) && !less(j->miny, i->miny) )
+				{
+					j->miny = i->miny;
+					i = list.erase(i);
+					merged_current = true;
+					break;
+				}
+
+				// merge vertical
 				if ( !less(i->minx, j->minx) && !less(j->minx, i->minx)
 				  && !less(i->maxy, j->miny) && !less(j->miny, i->maxy) )
 				{
@@ -266,11 +257,11 @@ void rects_merge(List &list, const F &less)
 					merged_current = true;
 					break;
 				}
+			}
 			if (merged_current) merged_any = true; else ++i;
 		}
 	}
 }
-
 template<typename List>
 void rects_merge(List &list)
 {
