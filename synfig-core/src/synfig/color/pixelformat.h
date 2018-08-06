c43d12fe90eecf1f5e1a48f725faf3fedc87abcb
Ivan Mahonin <bh@icystar.com>
2018-08-07 00:28:03 +0700

    PixelFormat with premulted alpha

--- synfig-core/src/synfig/color/pixelformat.h
+++ synfig-core/src/synfig/color/pixelformat.h
@@ -44,6 +44,8 @@ enum PixelFormat
 ** 6    Alpha/ZDepth Arrangement (ZA,AZ)
 ** 7    Alpha Range (Inverted,Normal)
 ** 8    Z Range (Inverted,Normal)
+** 9    Raw Color (not conversion)
+** 10   Premult Alpha
 */
     PF_RGB       = 0,
     PF_GRAY      = (1<<0), //!< If set, use one grayscale channel. If clear, use three channels for RGB
@@ -55,7 +57,8 @@ enum PixelFormat
     PF_ZA        = (1<<6), //!< If set, the ZDepth channel will be in front of the alpha channel. If clear, they are reversed.
     PF_A_INV     = (1<<7), //!< If set, the alpha channel is stored as 1.0-a
     PF_Z_INV     = (1<<8), //!< If set, the ZDepth channel is stored as 1.0-z
-    PF_RAW_COLOR = (1<<9)+(1<<1) //!< If set, the data represents a raw Color data structure, and all other bits are ignored.
+    PF_RAW_COLOR = (1<<9)+(1<<1), //!< If set, the data represents a raw Color data structure, and all other bits are ignored.
+    PF_A_PREMULT = (1<<10) //!< If set, the encoded color channels are alpha-premulted
 };
 
 inline PixelFormat operator|(PixelFormat lhs, PixelFormat rhs)
@@ -66,215 +69,155 @@ inline PixelFormat operator&(PixelFormat lhs, PixelFormat rhs)
 #define FLAGS(x,y)        (((x)&(y))==(y))
 
 //! Returns the number of channels that the given PixelFormat calls for
-inline int channels(const PixelFormat x)
+inline int
+channels(const PixelFormat x)
 {
-    int chan = 0;
-    if(FLAGS(x, PF_GRAY))
-    {
-        ++chan;
-    }
-    else
-    {
-        chan += 3;
-    }
+    if (FLAGS(x, PF_RAW_COLOR))
+    	return sizeof(Color);
 
-    if(FLAGS(x, PF_A))
-    {
-        ++chan;
-    }
-    if(FLAGS(x, PF_Z))
-    {
-        ++chan;
-    }
-    if(FLAGS(x, PF_RAW_COLOR))
-    {
-        chan = sizeof(Color);
-    }
-
+    int chan = FLAGS(x, PF_GRAY) ? 1 : 3;
+    if (FLAGS(x, PF_A)) ++chan;
+    if (FLAGS(x, PF_Z)) ++chan;
     return chan;
 }
 
-inline unsigned char * Color2PixelFormat(const Color &color, const PixelFormat &pf,
-                                         unsigned char *out, const Gamma &gamma)
+inline unsigned char*
+Color2PixelFormat(
+	const Color &color,
+	const PixelFormat &pf,
+	unsigned char *out,
+	const Gamma &gamma )
 {
-    if(FLAGS(pf, PF_RAW_COLOR))
-    {
-        Color *outcol = reinterpret_cast<Color *>(out);
-        *outcol = color;
-        out += sizeof(color);
-        return out;
-    }
+	static int yuv_r = (int)(EncodeYUV[0][0]*256.f);
+	static int yuv_g = (int)(EncodeYUV[0][1]*256.f);
+	static int yuv_b = 256 - yuv_r - yuv_g;
 
-    int alpha = 0;
-    if (FLAGS(pf, PF_A_INV))
-    {
-        alpha = (-(float)color.get_a()+1) * 255;
-    }
-    else
-    {
-        alpha = (float)color.get_a() * 255;
-    }
+	if(FLAGS(pf, PF_RAW_COLOR)) {
+		// just copy raw color data
+		*reinterpret_cast<Color*>(out) = color;
+		return out + sizeof(color);
+	}
 
-    if(alpha < 0)
-    {
-        alpha=0;
-    }
-    if(alpha > 255)
-    {
-        alpha=255;
-    }
+	// get alpha value
+	ColorReal a = std::max(ColorReal(0.0), std::min(ColorReal(1.0), color.get_a()));
+	unsigned char ac = (unsigned char)(a*ColorReal(255.9));
+	if (FLAGS(pf, PF_A_INV)) ac = 255 - ac;
 
-    if(FLAGS(pf, PF_ZA|PF_A_START|PF_Z_START))
-    {
-        if(FLAGS(pf, PF_Z_START))
-        {
-            out++;
-        }
-        if(FLAGS(pf, PF_A_START))
-        {
-            *out++ = static_cast<unsigned char>(alpha);
-        }
-    }
-    else
-    {
-        if(FLAGS(pf, PF_A_START))
-        {
-            *out++ = static_cast<unsigned char>(alpha);
-        }
-        if(FLAGS(pf, PF_Z_START))
-        {
-            out++;
-        }
-    }
+	// get color values
+	int ri = gamma.r_F32_to_U16(color.get_r());
+	int gi = gamma.g_F32_to_U16(color.get_g());
+	int bi = gamma.b_F32_to_U16(color.get_b());
+	if (FLAGS(pf, PF_A_PREMULT)) {
+		int ai = ((int)ac) + 1;
+		ri = (ri*ai) >> 8;
+		gi = (gi*ai) >> 8;
+		bi = (bi*ai) >> 8;
+	}
 
-    if(FLAGS(pf,PF_GRAY))
-    {
-        *out++ = static_cast<unsigned char>(gamma.g_F32_to_U8(color.get_y()));
-    }
-    else
-    {
-        if(FLAGS(pf,PF_BGR))
-        {
-            *out++ = static_cast<unsigned char>(gamma.r_F32_to_U8(color.get_b()));
-            *out++ = static_cast<unsigned char>(gamma.g_F32_to_U8(color.get_g()));
-            *out++ = static_cast<unsigned char>(gamma.b_F32_to_U8(color.get_r()));
-        }
-        else
-        {
-            *out++ = static_cast<unsigned char>(gamma.r_F32_to_U8(color.get_r()));
-            *out++ = static_cast<unsigned char>(gamma.g_F32_to_U8(color.get_g()));
-            *out++ = static_cast<unsigned char>(gamma.b_F32_to_U8(color.get_b()));
-        }
-    }
+	// put alpha and z-depth before color channels if need
+	if (FLAGS(pf, PF_ZA)) {
+		if (FLAGS(pf, PF_Z_START)) ++out;
+		if (FLAGS(pf, PF_A_START)) *out++ = ac;
+	} else {
+		if(FLAGS(pf, PF_A_START)) *out++ = ac;
+		if(FLAGS(pf, PF_Z_START)) ++out;
+	}
 
-    //if(FLAGS(pf, PF_ZA))
-    {
-        if(!FLAGS(pf, PF_Z_START) && FLAGS(pf, PF_Z))
-        {
-            out++;
-        }
-        if(!FLAGS(pf, PF_A_START) && FLAGS(pf, PF_A))
-        {
-            *out++ = static_cast<unsigned char>(alpha);
-        }
-    }
-    /*else
-    {
-        if(!FLAGS(pf, PF_Z_START) && FLAGS(pf, PF_Z))
-        {
-            out++;
-        }
-        if(!FLAGS(pf, PF_A_START) && FLAGS(pf, PF_A))
-        {
-            *out++ = static_cast<unsigned char>(alpha);
-        }
-    }*/
+	// put color channels
+	if (FLAGS(pf, PF_GRAY)) {
+		*out++ = (unsigned char)((ri*yuv_r + gi*yuv_g + bi*yuv_b) >> 16);
+	} else
+	if (FLAGS(pf, PF_BGR)) {
+		*out++ = (unsigned char)(bi >> 8);
+		*out++ = (unsigned char)(gi >> 8);
+		*out++ = (unsigned char)(ri >> 8);
+	} else {
+		*out++ = (unsigned char)(ri >> 8);
+		*out++ = (unsigned char)(gi >> 8);
+		*out++ = (unsigned char)(bi >> 8);
+	}
 
-    return out;
+	// put alpha and z-depth after color channels if need
+	if (FLAGS(pf, PF_ZA)) {
+		if (!FLAGS(pf, PF_Z_START) && FLAGS(pf, PF_Z)) ++out;
+		if (!FLAGS(pf, PF_A_START) && FLAGS(pf, PF_A)) *out++ = ac;
+	} else {
+		if (!FLAGS(pf, PF_A_START) && FLAGS(pf, PF_A)) *out++ = ac;
+		if (!FLAGS(pf, PF_Z_START) && FLAGS(pf, PF_Z)) ++out;
+	}
+
+	return out;
 }
 
-inline void convert_color_format(unsigned char *dest, const Color *src,
-                                 int w, PixelFormat pf,const Gamma &gamma)
+inline unsigned char*
+convert_color_format(
+	unsigned char *dest,
+	const Color *src,
+	int w,
+	PixelFormat pf,
+	const Gamma &gamma )
 {
-    assert(w >= 0);
-    while(w--)
-    {
-        dest = Color2PixelFormat((*(src++)).clamped(),
-                                 pf, dest, gamma);
-    }
+	assert(w >= 0);
+	while (w--)
+		dest = Color2PixelFormat((*(src++)).clamped(), pf, dest, gamma);
+	return dest;
 }
 
-inline const unsigned char * PixelFormat2Color(Color &color,
-                                               const PixelFormat &pf,
-                                               const unsigned char *out)
+inline const unsigned char*
+PixelFormat2Color(
+	Color &color,
+	const PixelFormat &pf,
+	const unsigned char *in )
 {
-    if(FLAGS(pf, PF_ZA|PF_A_START|PF_Z_START))
-    {
-        if(FLAGS(pf, PF_Z_START))
-        {
-            out++;
-        }
-        if(FLAGS(pf,PF_A_START))
-        {
-            color.set_a((float)*out++ / 255);
-        }
-    }
-    else
-    {
-        if(FLAGS(pf, PF_A_START))
-        {
-            color.set_a((float)*out++ / 255);
-        }
-        if(FLAGS(pf, PF_Z_START))
-        {
-            out++;
-        }
-    }
+	const ColorReal k(1.0/255.0);
 
-    if(FLAGS(pf, PF_GRAY))
-    {
-        color.set_yuv((float)*out++ / 255, 0, 0);
-    }
-    else
-    {
-        if(FLAGS(pf, PF_BGR))
-        {
-            color.set_b((float)*out++ / 255);
-            color.set_g((float)*out++ / 255);
-            color.set_r((float)*out++ / 255);
-        }
-        else
-        {
-            color.set_r((float)*out++ / 255);
-            color.set_g((float)*out++ / 255);
-            color.set_b((float)*out++ / 255);
-        }
-    }
+	if(FLAGS(pf, PF_RAW_COLOR)) {
+		// just copy raw color data
+		color = *reinterpret_cast<const Color*>(in);
+		return in + sizeof(color);
+	}
 
-    if(FLAGS(pf, PF_ZA))
-    {
-        if(!FLAGS(pf, PF_Z_START) && FLAGS(pf, PF_Z))
-        {
-            out++;
-        }
-        if(!FLAGS(pf, PF_A_START) && FLAGS(pf, PF_A))
-        {
-            color.set_a((float)*out++ / 255);
-        }
-    }
-    else
-    {
-        if(!FLAGS(pf, PF_A_START) && FLAGS(pf, PF_A))
-        {
-            color.set_a((float)*out++ / 255);
-        }
-        if(!FLAGS(pf, PF_Z_START) && FLAGS(pf, PF_Z))
-        {
-            out++;
-        }
-    }
+	// read alpha and z-depth at begin if need
+	if (FLAGS(pf, PF_ZA)) {
+		if (FLAGS(pf, PF_Z_START)) ++in;
+		if (FLAGS(pf, PF_A_START)) color.set_a(k*ColorReal(*in++));
+	} else {
+		if (FLAGS(pf, PF_A_START)) color.set_a(k*ColorReal(*in++));
+		if (FLAGS(pf, PF_Z_START)) ++in;
+	}
 
-    return out;
+	// read color channels
+	if (FLAGS(pf, PF_GRAY)) {
+		color.set_yuv(k*ColorReal(*in++), 0, 0);
+	} else
+	if(FLAGS(pf, PF_BGR)) {
+		color.set_b(k*ColorReal(*in++));
+		color.set_g(k*ColorReal(*in++));
+		color.set_r(k*ColorReal(*in++));
+	} else {
+		color.set_r(k*ColorReal(*in++));
+		color.set_g(k*ColorReal(*in++));
+		color.set_b(k*ColorReal(*in++));
+	}
+
+	// read alpha and z-depth at end if need
+	if (FLAGS(pf, PF_ZA)) {
+		if (!FLAGS(pf, PF_Z_START) && FLAGS(pf, PF_Z)) ++in;
+		if (!FLAGS(pf, PF_A_START) && FLAGS(pf, PF_A)) color.set_a(k*(float)(*in++));
+	} else {
+		if (!FLAGS(pf, PF_A_START) && FLAGS(pf, PF_A)) color.set_a(k*(float)(*in++));
+		if (!FLAGS(pf, PF_Z_START) && FLAGS(pf, PF_Z)) ++in;
+	}
+
+	// invert alpha
+	if (FLAGS(pf, PF_A_INV))
+		color.set_a(ColorReal(1.0) - color.get_a());
+
+	// demult alpha
+	if (FLAGS(pf, PF_A_PREMULT))
+		color = color.demult_alpha();
+
+	return in;
 }
 
 } // synfig namespace
