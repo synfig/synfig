8cebc45bed67ac1fb32e394e2b73861b874566a7
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2018-08-10 23:27:53 +0700

    Use NIX for building dev environment. See instructions - https://synfig-docs-dev.readthedocs.io/en/latest/common/building.html

--- autobuild/default.nix
+++ autobuild/default.nix
@@ -14,65 +14,36 @@ To initialise it:
 
 $ source ~/.nix-profile/etc/profile.d/nix.sh
 
-To build synfig, from the current directory:
-
-$ nix-build
-
-To run the newly compiled synfigstudio:
-
-$ ./result/bin/synfigstudio
 */
 
 with import <nixpkgs> {};
 
 let
   version = "git";
-
-  ETL = stdenv.mkDerivation rec {
-    name = "ETL-${version}";
-
-    src = ../ETL;
-
-    nativeBuildInputs = [ autoreconfHook ];
-  };
-
-  synfig = stdenv.mkDerivation rec {
-    name = "synfig-${version}";
-
-    src = ../synfig-core;
-
-    configureFlags = [
-      "--with-boost=${boost.dev}"
-      "--with-boost-libdir=${boost.out}/lib"
-    ];
-
-    nativeBuildInputs = [ pkgconfig autoreconfHook gettext ];
-
-    buildInputs = [
-      ETL boost cairo fftw glibmm intltool libjpeg libsigcxx libxmlxx
-      mlt imagemagick pango which
-    ];
-    
-    preConfigure = "./bootstrap.sh";
-
-  };
 in
 stdenv.mkDerivation rec {
   name = "synfigstudio-${version}";
 
-  src = ../synfig-studio;
+  src = "../";
 
-  preConfigure = "./bootstrap.sh";
-
-  nativeBuildInputs = [ pkgconfig autoreconfHook gettext ];
+  nativeBuildInputs = [ pkgconfig autoreconfHook gettext gnumake ];
   buildInputs = [
-    ETL boost cairo fftw glibmm gnome3.defaultIconTheme gtk3 gtkmm3
-    imagemagick intltool libjack2 libsigcxx libxmlxx makeWrapper mlt
-    synfig which
+    boost cairo fftw intltool glibmm gnome3.defaultIconTheme gtk3 gtkmm3
+    imagemagick intltool libjpeg libjack2 libsigcxx libxmlxx makeWrapper mlt
+    pango which
   ];
+  
+  preConfigure = "";
+  configureScript = "";
+  
+  BOOST_CONFIGURE_OPTIONS = " --with-boost=${boost.dev} --with-boost-libdir=${boost.out}/lib ";
+  
+  buildPhase = ''
+    ./build-production.sh
+  '';
 
   postInstall = ''
-    wrapProgram "$out/bin/synfigstudio" \
+    wrapProgram "./_production/build/bin/synfigstudio" \
       --prefix XDG_DATA_DIRS : "$XDG_ICON_DIRS:$GSETTINGS_SCHEMAS_PATH" \
       --prefix XCURSOR_PATH : "${gnome3.adwaita-icon-theme.out}/share/icons" \
       --set XCURSOR_THEME "Adwaita"
