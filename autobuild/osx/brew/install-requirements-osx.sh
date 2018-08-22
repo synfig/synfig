c1e589c5aa882c6bc370f316527034f6054a883a
ice0 <konoplin@gmail.com>
2018-08-22 19:22:04 +0700

    disable brew auto-update for travis-ci

--- autobuild/osx/brew/install-requirements-osx.sh
+++ autobuild/osx/brew/install-requirements-osx.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # zlib not required because it already part of OSX
-brew install autoconf automake libtool intltool gettext pkg-config glibmm libxml++ cairo fftw pango mlt boost gtkmm3
+# disable brew auto-update for travis-ci
+HOMEBREW_NO_AUTO_UPDATE=1 brew install autoconf automake libtool intltool gettext pkg-config glibmm libxml++ cairo fftw pango mlt boost gtkmm3
