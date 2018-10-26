b100b685f139557c682ffd441d018a1f2a8a3717
Reshabh Sharma <reshabhsh@gmail.com>
2018-10-26 11:12:56 +0530

    move ccache stat here

--- autobuild/synfigrenderer-regression-test.sh
+++ autobuild/synfigrenderer-regression-test.sh
@@ -2,5 +2,6 @@
 
 set -x 
 
+ccache -s # show ccache stats
 git clone -b update-for-result-generation https://github.com/reSHARMA/synfig-tests-regressions.git
 bash ./synfig-tests-regressions/sources/force-render-png.sh results
