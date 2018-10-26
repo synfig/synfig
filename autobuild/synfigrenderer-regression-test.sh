85e5f2f67af1d3ea014d2958f219a2dadbbb4483
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2018-10-26 13:33:30 +0700

    Update source for synfig-tests-regressions repo

--- autobuild/synfigrenderer-regression-test.sh
+++ autobuild/synfigrenderer-regression-test.sh
@@ -3,5 +3,5 @@
 set -x 
 
 ccache -s # show ccache stats
-git clone -b update-for-result-generation https://github.com/reSHARMA/synfig-tests-regressions.git
+git clone https://github.com/synfig/synfig-tests-regressions.git
 bash ./synfig-tests-regressions/sources/force-render-png.sh results
