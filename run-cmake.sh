63b825d2119bef4564ac098676ed4c6f2e1f6d40
Voldracarno Draconor <thafiredragonofdeath@gmail.com>
2019-01-10 12:12:49 +0100

    Removed now unused directory from gitignore and made run-cmake.sh able to run release builds

--- run-cmake.sh
+++ run-cmake.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 #
 # = Usage: =
-#    ./run-cmake.sh [command]
+#    ./run-cmake.sh [-r] [command]
+#
+# Options
+# -r = Use the release build (standard is debug build)
 #
 # = Examples: =
-# Run Synfig Studio
+# Run Synfig Studio debug build
 #    ./run-cmake.sh
-# Run Synfig CLI with parameters
+# Run Synfig Studio release build
+#    ./run-cmake.sh -r
+# Run Synfig CLI (debug build) with parameters
 #    ./run-cmake.sh "synfig --help-all"
-# Create a bash session for running the portable build
+# Create a bash session for running the portable build (debug build)
 #    ./run-cmake.sh bash
 
 #Define dir paths
@@ -20,6 +25,14 @@ cd ${absolute_base_dir}
 
 # Include build folder names and run function
 source ./autobuild/build-cmake-common.sh
+
+if [ "$1" == "-r" ]
+    then
+        cmake_build_dir="$cmake_release_build_dir"
+        shift
+    else
+        cmake_build_dir="$cmake_debug_build_dir"
+fi
 
 if [ ! -d "./${cmake_build_dir}/${out_dir}" ]
     then
