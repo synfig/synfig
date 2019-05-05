0d250755eaee7dfe6fe75fba15f76d5b4cf22057
AnishGulati <anish.gulati@research.iiit.ac.in>
2019-05-06 04:38:24 +0530

    Remove hardcode for time adjusting

--- synfig-studio/plugins/lottie-exporter/properties/timeAdjust.py
+++ synfig-studio/plugins/lottie-exporter/properties/timeAdjust.py
@@ -12,26 +12,40 @@ def time_adjust(lottie, animated):
         cur_get_after = animated[i].attrib["after"]
         next_get_before = animated[i+1].attrib["before"]
 
-                # prev              iter
+        # prev              iter
         # ANY/CONSTANT ---- ANY/ANY
         # ANY/ANY      ---- CONSTANT/ANY
         if cur_get_after == "constant" or next_get_before == "constant":
             continue
 
-        # prev    --- iter        --- next
-        # ANY/ANY --- ANY/!LINEAR --- ANY/ANY
-        if cur_get_after != "linear":
-            for dim in range(len(lottie["k"][i]["to"])):
-                lottie["k"][i]["to"][dim] = lottie["k"][i]["to"][dim] *\
-                (time_span_cur * (timeadjust + 1)) /\
-                (time_span_cur * timeadjust + time_span_prev)
-
-        # iter    --- next        --- after_next
-        # ANY/ANY --- !LINEAR/ANY --- ANY/ANY
-        if next_get_before != "linear":
-            for dim in range(len(lottie["k"][i]["to"])):
+        if animated.attrib["type"] == "real":
+            if cur_get_after != "linear":
+                lottie["k"][i]["o"]["x"][0] *= (time_span_cur * (timeadjust + 1)) /\
+                        (time_span_cur * timeadjust + time_span_prev)
+                lottie["k"][i]["o"]["y"][0] *= (time_span_cur * (timeadjust + 1)) /\
+                        (time_span_cur * timeadjust + time_span_prev)
+            if next_get_before != "linear":
                 if i + 2 <= len(animated) - 1:
                     time_span_next = lottie["k"][i+2]["t"] - lottie["k"][i+1]["t"]
-                    lottie["k"][i]["ti"][dim] = lottie["k"][i]["ti"][dim] *\
+                    lottie["k"][i]["i"]["x"][0] *= (time_span_cur * (timeadjust + 1)) /\
+                            (time_span_cur * timeadjust + time_span_next)
+
+        elif animated.attrib["type"] == "vector":
+
+            # prev    --- iter        --- next
+            # ANY/ANY --- ANY/!LINEAR --- ANY/ANY
+            if cur_get_after != "linear":
+                for dim in range(len(lottie["k"][i]["to"])):
+                    lottie["k"][i]["to"][dim] = lottie["k"][i]["to"][dim] *\
                     (time_span_cur * (timeadjust + 1)) /\
-                    (time_span_cur * timeadjust + time_span_next)
+                    (time_span_cur * timeadjust + time_span_prev)
+
+            # iter    --- next        --- after_next
+            # ANY/ANY --- !LINEAR/ANY --- ANY/ANY
+            if next_get_before != "linear":
+                for dim in range(len(lottie["k"][i]["to"])):
+                    if i + 2 <= len(animated) - 1:
+                        time_span_next = lottie["k"][i+2]["t"] - lottie["k"][i+1]["t"]
+                        lottie["k"][i]["ti"][dim] = lottie["k"][i]["ti"][dim] *\
+                        (time_span_cur * (timeadjust + 1)) /\
+                        (time_span_cur * timeadjust + time_span_next)
