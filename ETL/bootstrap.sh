0519a19bfdfe0a2dd51562871ccb60321e9f255d
Rodolfo Ribeiro Gomes <rodolforg@gmail.com>
2019-06-08 22:14:40 -0300

    standardize the three bootstrap.sh

--- ETL/bootstrap.sh
+++ ETL/bootstrap.sh
@@ -1,13 +1,17 @@
-#! /bin/sh
+#!/bin/sh
 
 set -e
 
-AUTORECONF=`command -v autoreconf || true`
-if test -z $AUTORECONF; then
+AUTORECONF=`command -v autoreconf || true` #if don't set true, script fails with no messages
+if [ -z $AUTORECONF ]; then
         echo "*** No autoreconf found, please install it ***"
         exit 1
 fi
 
 
 rm -f aclocal.m4
+
+echo "running autoreconf..."
 autoreconf --install --force
+
+echo "Done! Please run ./configure now."
