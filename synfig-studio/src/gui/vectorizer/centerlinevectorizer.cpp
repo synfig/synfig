ea8c7241039efb6045aabbfad8cc5202884022c6
Ankit Kumar Dwivedi <dwivedi.ankit21@gmail.com>
2019-06-20 16:46:09 +0530

    init centerlineadjustment.cpp

--- synfig-studio/src/gui/vectorizer/centerlinevectorizer.cpp
+++ synfig-studio/src/gui/vectorizer/centerlinevectorizer.cpp
@@ -103,10 +103,10 @@ void VectorizerCore::centerlineVectorize(etl::handle<synfig::Layer_Bitmap> &imag
     std::cout<<"CenterlineVectorize cancelled\n";
   }
 
-//   // step 4
-//   // The raw skeleton data obtained from StraightSkeletonizer
-//   // class need to be grouped in joints and sequences before proceding further
-//   organizeGraphs(skeletons, globals);
+  // step 4
+  // The raw skeleton data obtained from StraightSkeletonizer
+  // class need to be grouped in joints and sequences before proceding further
+  organizeGraphs(skeletons, globals);
 
 //   // junctionRecovery(polygons);   //Da' problemi per maxThickness<inf...
 //   // sarebbe da rendere compatibile
