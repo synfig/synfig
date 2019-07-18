c54de816172167e49745bffad148090c12daf4f3
Rodolfo Ribeiro Gomes <rodolforg@gmail.com>
2019-07-18 18:28:11 -0300

    create resource helper class
    in order to avoid code repetition

---
+++ synfig-studio/src/gui/resourcehelper.cpp
@@ -0,0 +1,69 @@
+/* === S Y N F I G ========================================================= */
+/*!	\file resourcehelper.h
+**	\brief Helper to retrieve the app resource paths, such as icons and plugins
+**
+**	$Id$
+**
+**	\legal
+**	Copyright (c) 2002-2005 Robert B. Quattlebaum Jr., Adrian Bentley
+**	Copyright (c) 2019 Rodolfo R Gomes
+**
+**	This package is free software; you can redistribute it and/or
+**	modify it under the terms of the GNU General Public License as
+**	published by the Free Software Foundation; either version 2 of
+**	the License, or (at your option) any later version.
+**
+**	This package is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+**	General Public License for more details.
+**	\endlegal
+*/
+
+/* ========================================================================= */
+
+#ifdef USING_PCH
+#	include "pch.h"
+#else
+#ifdef HAVE_CONFIG_H
+#	include <config.h>
+#endif
+
+#include "resourcehelper.h"
+
+#include "ETL/etl_config.h"
+#include "app.h"
+
+#endif
+
+synfig::String studio::ResourceHelper::get_image_path()
+{
+#ifdef _WIN32
+#	ifdef IMAGE_DIR
+#		undef IMAGE_DIR
+#		define IMAGE_DIR "share\\pixmaps"
+#	endif
+#endif
+
+#ifndef IMAGE_DIR
+#	define IMAGE_DIR "/usr/local/share/pixmaps"
+#endif
+
+	std::string imagepath;
+#ifdef _WIN32
+	imagepath=App::get_base_path()+ETL_DIRECTORY_SEPARATOR+IMAGE_DIR;
+#else
+	imagepath=IMAGE_DIR;
+#endif
+	char* synfig_root=getenv("SYNFIG_ROOT");
+	if(synfig_root) {
+		imagepath=synfig_root;
+		imagepath+="/share/pixmaps";
+	}
+	return imagepath;
+}
+
+synfig::String studio::ResourceHelper::get_image_path(const synfig::String& image_filename)
+{
+	return get_image_path() + '/' + image_filename;
+}
