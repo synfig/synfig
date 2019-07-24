5c7d6aeeef8de65beb8f81856226bdc4ae70474d
Ankit Kumar Dwivedi <dwivedi.ankit21@gmail.com>
2019-07-24 13:52:20 +0530

    init vectorization action

---
+++ synfig-studio/src/synfigapp/actions/vectorization.cpp
@@ -0,0 +1,65 @@
+/* === S Y N F I G ========================================================= */
+/*!	\file vectorization.cpp
+**	
+**
+**	$Id$
+**
+**	\legal
+**	Copyright (c) 2002-2005 Robert B. Quattlebaum Jr., Adrian Bentley
+**  Copyright (c) 2008 Chris Moore
+**
+**	This package is free software; you can redistribute it and/or
+**	modify it under the terms of the GNU General Public License as
+**	published by the Free Software Foundation; either version 2 of
+**	the License, or (at your option) any later version.
+**
+**	This package is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+**	General Public License for more details.
+**	\endlegal
+*/
+/* ========================================================================= */
+
+/* === H E A D E R S ======================================================= */
+
+#ifdef USING_PCH
+#	include "pch.h"
+#else
+#ifdef HAVE_CONFIG_H
+#	include <config.h>
+#endif
+
+#include <synfig/general.h>
+
+#include "vectorization.h"
+#include <synfigapp/canvasinterface.h>
+#include <synfigapp/main.h>
+
+#include <synfigapp/localization.h>
+
+#endif
+
+using namespace std;
+using namespace etl;
+using namespace synfig;
+using namespace synfigapp;
+using namespace Action;
+
+/* === M A C R O S ========================================================= */
+
+ACTION_INIT();
+ACTION_SET_NAME();
+ACTION_SET_LOCAL_NAME();
+ACTION_SET_TASK();
+ACTION_SET_CATEGORY();
+ACTION_SET_PRIORITY();
+ACTION_SET_VERSION();
+ACTION_SET_CVS_ID();
+
+/* === G L O B A L S ======================================================= */
+
+/* === P R O C E D U R E S ================================================= */
+
+/* === M E T H O D S ======================================================= */
+
---
+++ synfig-studio/src/synfigapp/actions/vectorization.h
@@ -0,0 +1,63 @@
+/* === S Y N F I G ========================================================= */
+/*!	\file vectorization.h
+**	\brief Vectorization Action File
+**
+**	$Id$
+**
+**	\legal
+**	Copyright (c) 2002-2005 Robert B. Quattlebaum Jr., Adrian Bentley
+**
+**	This package is free software; you can redistribute it and/or
+**	modify it under the terms of the GNU General Public License as
+**	published by the Free Software Foundation; either version 2 of
+**	the License, or (at your option) any later version.
+**
+**	This package is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+**	General Public License for more details.
+**	\endlegal
+*/
+/* ========================================================================= */
+
+/* === S T A R T =========================================================== */
+
+#ifndef __SYNFIG_APP_ACTION_VECTORIZATION_H
+#define __SYNFIG_APP_ACTION_VECTORIZATION_H
+
+/* === H E A D E R S ======================================================= */
+
+#include <synfigapp/action.h>
+#include <synfig/layer.h>
+
+/* === M A C R O S ========================================================= */
+
+/* === T Y P E D E F S ===================================================== */
+
+/* === C L A S S E S & S T R U C T S ======================================= */
+
+namespace synfigapp {
+
+class Instance;
+
+namespace Action {
+
+class Vectorization :
+	public Super
+{
+private:
+
+	
+public:
+
+	Vectorization();
+
+	
+};
+
+}; // END of namespace action
+}; // END of namespace studio
+
+/* === E N D =============================================================== */
+
+#endif
