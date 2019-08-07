2bdc689c840dfaf0560802b8e943b1cdf54e0d20
Ankit Kumar Dwivedi <dwivedi.ankit21@gmail.com>
2019-08-07 23:32:21 +0530

    add delete skeleton

--- synfig-studio/src/synfigapp/vectorizer/centerlinevectorizer.cpp
+++ synfig-studio/src/synfigapp/vectorizer/centerlinevectorizer.cpp
@@ -74,25 +74,25 @@ std::vector< etl::handle<synfig::Layer> > VectorizerCore::centerlineVectorize(et
   // Extracts a polygonal, minimal yet faithful representation of image contours
   Contours polygons;
   studio::polygonize(image, polygons, globals);
-  for (int i = 0; i < polygons.size(); ++i) 
-  {
-    for (int j = 0; j < polygons[i].size(); ++j) 
-    {
-      for (int k = 0; k < polygons[i][j].size(); ++k)
-      {
-      std::cout<<"i= "<<i<<" j= "<<j<<" k="<<k<<" Data: ";
-      std::cout<< "[(" << polygons[i][j][k].m_position[0]<<", "<<polygons[i][j][k].m_position[1]<<", "<<polygons[i][j][k].m_position[2] <<")("
-      << polygons[i][j][k].m_direction[0]<<", "<<polygons[i][j][k].m_direction[1]<<", "<<polygons[i][j][k].m_direction[2] <<")("
-      << polygons[i][j][k].m_AngularMomentum[0]<<", "<<polygons[i][j][k].m_AngularMomentum[1]<<", "<<polygons[i][j][k].m_AngularMomentum[2] <<")("
-      << polygons[i][j][k].m_AuxiliaryMomentum1[0]<<", "<<polygons[i][j][k].m_AuxiliaryMomentum1[1]<<", "<<polygons[i][j][k].m_AuxiliaryMomentum1[2] <<")("
-      << polygons[i][j][k].m_AuxiliaryMomentum2[0]<<", "<<polygons[i][j][k].m_AuxiliaryMomentum2[1]<<", "<<polygons[i][j][k].m_AuxiliaryMomentum2[2] <<")"
-      <<" Concave: "<< polygons[i][j][k].m_concave << " Attr: "
-      << polygons[i][j][k].m_attributes <<" Time: "<< polygons[i][j][k].m_updateTime
-      <<" Ancestor"<< polygons[i][j][k].m_ancestor 
-      <<" AncC: "<< polygons[i][j][k].m_ancestorContour << "]\n";
-      }
-    }
-}
+//   for (int i = 0; i < polygons.size(); ++i) 
+//   {
+//     for (int j = 0; j < polygons[i].size(); ++j) 
+//     {
+//       for (int k = 0; k < polygons[i][j].size(); ++k)
+//       {
+//       std::cout<<"i= "<<i<<" j= "<<j<<" k="<<k<<" Data: ";
+//       std::cout<< "[(" << polygons[i][j][k].m_position[0]<<", "<<polygons[i][j][k].m_position[1]<<", "<<polygons[i][j][k].m_position[2] <<")("
+//       << polygons[i][j][k].m_direction[0]<<", "<<polygons[i][j][k].m_direction[1]<<", "<<polygons[i][j][k].m_direction[2] <<")("
+//       << polygons[i][j][k].m_AngularMomentum[0]<<", "<<polygons[i][j][k].m_AngularMomentum[1]<<", "<<polygons[i][j][k].m_AngularMomentum[2] <<")("
+//       << polygons[i][j][k].m_AuxiliaryMomentum1[0]<<", "<<polygons[i][j][k].m_AuxiliaryMomentum1[1]<<", "<<polygons[i][j][k].m_AuxiliaryMomentum1[2] <<")("
+//       << polygons[i][j][k].m_AuxiliaryMomentum2[0]<<", "<<polygons[i][j][k].m_AuxiliaryMomentum2[1]<<", "<<polygons[i][j][k].m_AuxiliaryMomentum2[2] <<")"
+//       <<" Concave: "<< polygons[i][j][k].m_concave << " Attr: "
+//       << polygons[i][j][k].m_attributes <<" Time: "<< polygons[i][j][k].m_updateTime
+//       <<" Ancestor"<< polygons[i][j][k].m_ancestor 
+//       <<" AncC: "<< polygons[i][j][k].m_ancestorContour << "]\n";
+//       }
+//     }
+// }
   // step 3
   // The process of skeletonization reduces all objects in an image to lines, 
   //  without changing the essential structure of the image.
@@ -101,19 +101,19 @@ std::vector< etl::handle<synfig::Layer> > VectorizerCore::centerlineVectorize(et
   // partial progresses
   SkeletonList *skeletons = studio::skeletonize(polygons, this, globals);
 
-  for (SkeletonList::iterator currGraphPtr = skeletons->begin(); currGraphPtr != skeletons->end(); ++currGraphPtr) 
-  {
-    std::cout<<"Skeleton data\n";
-    SkeletonGraph &currGraph   = **currGraphPtr;
-    std::cout<<"Link Count :"<<currGraph.getLinksCount()<<"Node Count :"<<currGraph.getNodesCount()<<"\n";
-    for (int i = 0; i < currGraph.getNodesCount(); ++i)
-    {
+  // for (SkeletonList::iterator currGraphPtr = skeletons->begin(); currGraphPtr != skeletons->end(); ++currGraphPtr) 
+  // {
+  //   std::cout<<"Skeleton data\n";
+  //   SkeletonGraph &currGraph   = **currGraphPtr;
+  //   std::cout<<"Link Count :"<<currGraph.getLinksCount()<<"Node Count :"<<currGraph.getNodesCount()<<"\n";
+  //   for (int i = 0; i < currGraph.getNodesCount(); ++i)
+  //   {
 
-      std::cout<<"m_content :"<<currGraph.getNode(i)->operator[](0) <<", "<<currGraph.getNode(i)->operator[](1)<<", "
-      <<currGraph.getNode(i)->operator[](2)<<"\n";
+  //     std::cout<<"m_content :"<<currGraph.getNode(i)->operator[](0) <<", "<<currGraph.getNode(i)->operator[](1)<<", "
+  //     <<currGraph.getNode(i)->operator[](2)<<"\n";
 
-    }
-  }
+  //   }
+  // }
 
   if (isCanceled()) 
   {
@@ -141,7 +141,7 @@ std::vector< etl::handle<synfig::Layer> > VectorizerCore::centerlineVectorize(et
 //   // step 6
 //   // Converts each forward or single Sequence of the image in its corresponding Stroke.
   studio::conversionToStrokes(sortibleResult, globals, image);
-
+  deleteSkeletonList(skeletons);
   return sortibleResult;
 
 	// synfig::Canvas::Handle child_canvas;
@@ -176,7 +176,6 @@ std::vector< etl::handle<synfig::Layer> > VectorizerCore::centerlineVectorize(et
 //   if (globals.currConfig->m_makeFrame) addFrameStrokes(result, ras, palette);
 //   // randomizeExtremities(result);   //Cuccio random - non serve...
 
-//   deleteSkeletonList(skeletons);
 
 //   return result;
 }
