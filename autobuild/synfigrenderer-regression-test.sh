0e97b20f09f438b13f2b7b83d40d56c28679b5f9
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2019-08-18 23:08:15 +0700

    Set path

--- autobuild/synfigrenderer-regression-test.sh
+++ autobuild/synfigrenderer-regression-test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-set -x 
+set -x
 
-ls
+export PATH="${TRAVIS_BUILD_DIR}/_production/build/bin:$PATH"
+which synfig
 
 ccache -s # show ccache stats
 git clone --depth 1 https://github.com/synfig/synfig-tests-regressions.git
