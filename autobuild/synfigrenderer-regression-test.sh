27310952348da2ee8af4f9626392fa041d830095
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2019-08-25 19:12:05 +0700

    Remove debugging output

--- autobuild/synfigrenderer-regression-test.sh
+++ autobuild/synfigrenderer-regression-test.sh
@@ -3,8 +3,7 @@
 set -x
 
 export PATH="${TRAVIS_BUILD_DIR}/_production/build/bin:$PATH"
-which synfig
 
 ccache -s # show ccache stats
-git clone --depth 1 https://github.com/synfig/synfig-tests-regressions.git
+git clone https://github.com/synfig/synfig-tests-regressions.git
 bash ./synfig-tests-regressions/test-rendering.sh results
