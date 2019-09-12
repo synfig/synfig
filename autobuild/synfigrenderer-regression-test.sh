2b54ed842195016f44c246658f092729dea037bd
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2019-09-12 19:20:12 +0700

    Limit clone depth when fetching regresions test

--- autobuild/synfigrenderer-regression-test.sh
+++ autobuild/synfigrenderer-regression-test.sh
@@ -3,5 +3,5 @@
 set -x 
 
 ccache -s # show ccache stats
-git clone https://github.com/synfig/synfig-tests-regressions.git
+git clone --depth 1 https://github.com/synfig/synfig-tests-regressions.git
 bash ./synfig-tests-regressions/test-rendering.sh results
