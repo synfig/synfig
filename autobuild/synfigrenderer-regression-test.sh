e4cff3adf2a3c5c5ffe6ad7063fafb4c8d08f5d6
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2019-10-29 17:41:38 +0700

    Change URL to get tests from GitLab repo

--- autobuild/synfigrenderer-regression-test.sh
+++ autobuild/synfigrenderer-regression-test.sh
@@ -5,5 +5,5 @@ set -x
 export PATH="${TRAVIS_BUILD_DIR}/_production/build/bin:$PATH"
 
 ccache -s # show ccache stats
-git clone https://github.com/synfig/synfig-tests-regressions.git
-bash ./synfig-tests-regressions/test-rendering.sh results
+git clone https://gitlab.com/synfig/synfig-tests.git
+bash ./synfig-tests/test-rendering.sh results
