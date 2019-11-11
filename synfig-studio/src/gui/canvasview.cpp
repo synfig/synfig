synfig/synfig

commit 0589a7db427e3bed6e784e5f611674bdc7eb5dfe

Keyframe "find" methods not raises exceptions anymore (windows speedup)

synfig-studio/src/gui/canvasview.cpp
@@ -2236,7 +2236,9 @@ CanvasView::on_time_changed()
 	if (!is_playing())
 	{
 		try {
-			get_canvas()->keyframe_list().find(time);
+			//get_canvas()->keyframe_list().find(time);
+			KeyframeList::iterator iter;
+			get_canvas()->keyframe_list().find(time, iter); // not sure this is needed? O_o
 			// Widget::override_color() is deprecated since Gtkmm 3.16: Use a custom style provider and style classes instead.
 			// This function is very slow!
 			current_time_widget->override_color(Gdk::RGBA("#FF0000"));

synfig-core/src/synfig/keyframe.cpp
@@ -129,17 +129,19 @@ KeyframeList::sync()
 	//dump();
 }
 
-KeyframeList::iterator
-KeyframeList::find(const UniqueID &x)
+bool
+KeyframeList::find(const UniqueID &x, KeyframeList::iterator &out)
 {
-	KeyframeList::iterator iter;
+	out = std::find(begin(), end(), x);
+	return out != end();
+	/*KeyframeList::iterator iter;
 	iter=std::find(begin(),end(),x);
 	if(iter==end())
 		throw Exception::NotFound(strprintf("KeyframeList::find(): Can't find UniqueID %d",x.get_uid()));
-	return iter;
+	return iter;*/
 }
 
-KeyframeList::const_iterator
+/*KeyframeList::const_iterator
 KeyframeList::find(const UniqueID &x)const
 {
 	KeyframeList::const_iterator iter;
@@ -147,7 +149,7 @@ KeyframeList::find(const UniqueID &x)const
 	if(iter==end())
 		throw Exception::NotFound(strprintf("KeyframeList::find(): Can't find UniqueID %d",x.get_uid()));
 	return iter;
-}
+}*/
 
 KeyframeList::iterator
 KeyframeList::add(const Keyframe &x)
@@ -163,25 +165,26 @@ KeyframeList::add(const Keyframe &x)
 void
 KeyframeList::erase(const UniqueID &x)
 {
-	std::vector<Keyframe>::erase(find(x));
+	KeyframeList::iterator iter;
+	if (find(x, iter)) std::vector<Keyframe>::erase(iter);
 }
 
-KeyframeList::iterator
-KeyframeList::find(const Time &x)
+bool
+KeyframeList::find(const Time &x, KeyframeList::iterator &out)
 {
 	KeyframeList::iterator iter;
 	iter=binary_find(begin(),end(),x);
-	if(iter!=end() && iter->get_time().is_equal(x))
-		return iter;
-/*	iter++;
-	if(iter!=end() && iter->get_time().is_equal(x))
-		return iter;
-*/
-	throw Exception::NotFound(strprintf("KeyframeList::find(): Can't find Keyframe %s",x.get_string().c_str()));
+	if(iter!=end() && iter->get_time().is_equal(x)) {
+		out = iter;
+		return true;
+	}
+
+	//throw Exception::NotFound(strprintf("KeyframeList::find(): Can't find Keyframe %s",x.get_string().c_str()));
+	return false;
 }
 
-KeyframeList::iterator
-KeyframeList::find_next(const Time &x, bool ignore_disabled)
+bool
+KeyframeList::find_next(const Time &x, KeyframeList::iterator &out, bool ignore_disabled)
 {
 	KeyframeList::iterator iter(binary_find(begin(),end(),x));
 
@@ -192,17 +195,20 @@ KeyframeList::find_next(const Time &x, bool ignore_disabled)
 			iter->get_time().is_more_than(x)
 			&& 
 			( !ignore_disabled || iter->active() )
-		)
-			return iter;
+		) {
+			out = iter;
+			return true;
+		}
 		++iter;
 	}
 
-	throw Exception::NotFound(strprintf("KeyframeList::find(): Can't find next Keyframe %s",x.get_string().c_str()));
+	//throw Exception::NotFound(strprintf("KeyframeList::find(): Can't find next Keyframe %s",x.get_string().c_str()));
+	return false;
 }
 
 
-KeyframeList::iterator
-KeyframeList::find_prev(const Time &x, bool ignore_disabled)
+bool 
+KeyframeList::find_prev(const Time &x, KeyframeList::iterator &out, bool ignore_disabled)
 {
 	KeyframeList::iterator iter(binary_find(begin(),end(),x));
 
@@ -215,8 +221,10 @@ KeyframeList::find_prev(const Time &x, bool ignore_disabled)
 				iter->get_time().is_less_than(x)
 				&&
 				( !ignore_disabled || iter->active() ) 
-			)
-				return iter;
+			) {
+				out = iter;
+				return true;
+			}
 			--iter;
 		};
 		if
@@ -224,16 +232,20 @@ KeyframeList::find_prev(const Time &x, bool ignore_disabled)
 			iter->get_time().is_less_than(x)
 			&&
 			( !ignore_disabled || iter->active() ) 
-		)
-			return iter;
+		) {
+				out = iter;
+				return true;
+		}
 	}
-	throw Exception::NotFound(strprintf("KeyframeList::find(): Can't find prev Keyframe %s",x.get_string().c_str()));
+
+	return false;
+	//throw Exception::NotFound(strprintf("KeyframeList::find(): Can't find prev Keyframe %s",x.get_string().c_str()));
 
 }
 
 
 
-KeyframeList::const_iterator
+/*KeyframeList::const_iterator
 KeyframeList::find(const Time &x)const
 {
 	return const_cast<KeyframeList*>(this)->find(x);
@@ -253,15 +265,21 @@ KeyframeList::find_prev(const Time &x, bool ignore_disabled)const
 {
 	return const_cast<KeyframeList*>(this)->find_prev(x, ignore_disabled);
 
-}
+}*/
 
 void
-KeyframeList::find_prev_next(const Time& time, Time &prev, Time &next, bool ignore_disabled)const
+KeyframeList::find_prev_next(const Time& time, Time &prev, Time &next, bool ignore_disabled)
 {
-	try { prev=find_prev(time, ignore_disabled)->get_time(); }
-	catch(...) { prev=Time::begin(); }
-	try { next=find_next(time, ignore_disabled)->get_time(); }
-	catch(...) { next=Time::end(); }
+	KeyframeList::iterator iter;
+	if (find_prev(time, iter, ignore_disabled))
+		prev = iter->get_time();
+	else
+		prev=Time::begin();
+	
+	if (find_next(time, iter, ignore_disabled))
+		next = iter->get_time();
+	else
+		next=Time::end();
 }
 
 void
@@ -270,15 +288,13 @@ KeyframeList::insert_time(const Time& location, const Time& delta)
 //	synfig::info("KeyframeList::insert_time(): loc=%s, delta=%s",location.get_string().c_str(),delta.get_string().c_str());
 	if(!delta)
 		return;
-	try
-	{
-		// find next from time (location) including deactivated kf
-		iterator iter(find_next(location, false));
+
+	KeyframeList::iterator iter;
+	if (find_next(location, iter, false)) {
 		for(;iter!=end();++iter)
 		{
 			iter->set_time(iter->get_time()+delta);
 		}
 		sync();
 	}
-	catch(Exception::NotFound&) { }
 }

synfig-core/src/synfig/keyframe.h
@@ -133,24 +133,24 @@ public:
 
 	void erase(const UniqueID &x);
 
-	iterator find(const UniqueID &x);
+	bool find(const UniqueID &x, KeyframeList::iterator &out);
 
-	const_iterator find(const UniqueID &x)const;
+	//const_iterator find(const UniqueID &x)const;
 
 	//! Finds the keyframe at an exact point in time
-	iterator find(const Time &x);
+	bool find(const Time &x, KeyframeList::iterator &out);
 
 	//! Finds the keyframe after that point in time
-	iterator find_next(const Time &x, bool ignore_disabled = true);
+	bool find_next(const Time &x, KeyframeList::iterator &out, bool ignore_disabled = true);
 
 	//! Finds the keyframe before that point in time
-	iterator find_prev(const Time &x, bool ignore_disabled = true);
+	bool find_prev(const Time &x, KeyframeList::iterator &out, bool ignore_disabled = true);
 
-	const_iterator find(const Time &x)const;
+/*	const_iterator find(const Time &x)const;
 	const_iterator find_next(const Time &x, bool ignore_disabled = true)const;
-	const_iterator find_prev(const Time &x, bool ignore_disabled = true)const;
+	const_iterator find_prev(const Time &x, bool ignore_disabled = true)const;*/
 
-	void find_prev_next(const Time& time, Time &prev, Time &next, bool ignore_disabled = true)const;
+	void find_prev_next(const Time& time, Time &prev, Time &next, bool ignore_disabled = true);
 
 	void insert_time(const Time& location, const Time& delta);
 

synfig-studio/src/gui/actionmanagers/keyframeactionmanager.cpp
@@ -266,8 +266,15 @@ KeyframeActionManager::refresh()
 	//activate actions depending on context
 	{
 		//get the keyframe at current time
-		bool kf_at_current_time = true;
-		try
+		bool kf_at_current_time = false;
+		KeyframeList::iterator iter;
+		if (canvas_interface_->get_canvas()->keyframe_list().find(canvas_interface_->get_time(), iter)) {
+			kf_at_current_time = true;
+			if (action_group_->get_action("action-KeyframeDuplicate"))
+				action_group_->get_action("action-KeyframeDuplicate")->set_sensitive(false);
+		}
+
+		/*try
 		{
 			canvas_interface_->get_canvas()->keyframe_list().find(canvas_interface_->get_time());
 			if(action_group_->get_action("action-KeyframeDuplicate"))
@@ -276,7 +283,7 @@ KeyframeActionManager::refresh()
 		catch(synfig::Exception::NotFound)
 		{
 			kf_at_current_time = false;
-		}
+		}*/
 		//get the beginning and ending time of the time slider
 		Time begin_time=canvas_interface_->get_canvas()->rend_desc().get_time_start();
 		Time end_time=canvas_interface_->get_canvas()->rend_desc().get_time_end();

synfig-studio/src/gui/trees/keyframetreestore.cpp
@@ -925,20 +925,15 @@ KeyframeTreeStore::change_keyframe(synfig::Keyframe keyframe)
 		row_changed(get_path(row),row);
 
 		// If exist, previous row should be updated too (length value)
-		try
+		if (new_index != 0)
 		{
-			if (new_index != 0)
-			{
-				synfig::Keyframe keyframe_prev = *(get_canvas()->keyframe_list().find_prev(keyframe.get_time(),false));
-				Gtk::TreeRow row_prev(find_row(keyframe_prev));
+			KeyframeList::iterator keyframe_prev;
+			if (get_canvas()->keyframe_list().find_prev(keyframe.get_time(), keyframe_prev, false)) {
+				//synfig::Keyframe keyframe_prev = *(get_canvas()->keyframe_list().find_prev(keyframe.get_time(),false));
+				Gtk::TreeRow row_prev(find_row(*keyframe_prev));
 				dump_iterator(row_prev,"change_keyframe,row_prev");
 				row_changed(get_path(row_prev),row_prev);
 			}
-		}
-
-		catch(Exception::NotFound &x)
-		{
-			g_warning("%s", x.what());
 		}
 	}
 	catch(std::exception &x)

synfig-studio/src/gui/widgets/widget_keyframe_list.cpp
@@ -263,25 +263,30 @@ Widget_Keyframe_List::perform_move_kf(bool delta)
 		} catch(...) { return false; }
 	} else {
 		// find prev from selected kf time including deactivated kf
-		Keyframe prev_kf(*kf_list->find_prev(selected_kf_time, false));
-		Time prev_kf_time(prev_kf.get_time());
-		if (prev_kf_time >= dragging_kf_time) { // Not allowed
-			warning(_("Delta set not allowed"));
-			info("Widget_Keyframe_List::perform_move_kf(%i)::prev_kf_time=%s", delta, prev_kf_time.get_string().c_str());
-			info("Widget_Keyframe_List::perform_move_kf(%i)::dragging_kf_time=%s", delta, dragging_kf_time.get_string().c_str());
-			return false;
-		} else {
-			Time old_delta_time(selected_kf_time-prev_kf_time);
-			Time new_delta_time(dragging_kf_time-prev_kf_time);
-			Time change_delta(new_delta_time-old_delta_time);
-			Action::Handle action(Action::create("KeyframeSetDelta"));
-			if (!action)
+		KeyframeList::iterator iter;
+		if (kf_list->find_prev(selected_kf_time, iter, false)) {
+			//Keyframe prev_kf(*kf_list->find_prev(selected_kf_time, false));
+			Keyframe prev_kf(*iter);
+			Time prev_kf_time(prev_kf.get_time());
+			if (prev_kf_time >= dragging_kf_time) { // Not allowed
+				warning(_("Delta set not allowed"));
+				info("Widget_Keyframe_List::perform_move_kf(%i)::prev_kf_time=%s", delta, prev_kf_time.get_string().c_str());
+				info("Widget_Keyframe_List::perform_move_kf(%i)::dragging_kf_time=%s", delta, dragging_kf_time.get_string().c_str());
 				return false;
-			action->set_param("canvas", canvas_interface->get_canvas());
-			action->set_param("canvas_interface", canvas_interface);
-			action->set_param("keyframe", prev_kf);
-			action->set_param("delta", change_delta);
-			canvas_interface->get_instance()->perform_action(action);
+			} else {
+				Time old_delta_time(selected_kf_time-prev_kf_time);
+				Time new_delta_time(dragging_kf_time-prev_kf_time);
+				Time change_delta(new_delta_time-old_delta_time);
+				Action::Handle action(Action::create("KeyframeSetDelta"));
+				if (!action)
+					return false;
+				action->set_param("canvas", canvas_interface->get_canvas());
+				action->set_param("canvas_interface", canvas_interface);
+				action->set_param("keyframe", prev_kf);
+				action->set_param("delta", change_delta);
+				canvas_interface->get_instance()->perform_action(action);
+			}
+
 		}
 	}
 
@@ -348,11 +353,23 @@ Widget_Keyframe_List::on_event(GdkEvent *event)
 				ttip = _("Click and drag keyframes");
 			} else
 			if (t - p_t < n_t - t) {
-				Keyframe kf = *kf_list->find_prev(t);
-				ttip = kf.get_description().empty() ? String(_("No name")) : kf.get_description();
+				//Keyframe kf = *kf_list->find_prev(t);
+				//ttip = kf.get_description().empty() ? String(_("No name")) : kf.get_description();
+				KeyframeList::iterator iter;
+				if (kf_list->find_prev(t, iter)) {
+					Keyframe kf(*iter);
+					ttip = kf.get_description().empty() ? String(_("No name")) : kf.get_description();
+				}
+					
+				
 			} else {
-				Keyframe kf(*kf_list->find_next(t));
-				ttip = kf.get_description().empty() ? String(_("No name")) : kf.get_description();
+				//Keyframe kf(*kf_list->find_next(t));
+				//ttip = kf.get_description().empty() ? String(_("No name")) : kf.get_description();
+				KeyframeList::iterator iter;
+				if (kf_list->find_next(t, iter)) {
+					Keyframe kf(*iter);
+					ttip = kf.get_description().empty() ? String(_("No name")) : kf.get_description();
+				}
 			}
 			set_tooltip_text(ttip);
 			dragging = false;
@@ -368,11 +385,19 @@ Widget_Keyframe_List::on_event(GdkEvent *event)
 		Time prev_t, next_t;
 		kf_list->find_prev_next(t, prev_t, next_t, false);
 		if (t - prev_t < next_t - t) {
-			if (t - prev_t <= time_ratio)
-				kf = &*kf_list->find_prev(t, false);
+			if (t - prev_t <= time_ratio) {
+				//kf = &*kf_list->find_prev(t, false);
+				KeyframeList::iterator iter;
+				if (kf_list->find_prev(t, iter, false))
+					kf = &*iter;
+			}
 		} else {
-			if (next_t - t <= time_ratio)
-				kf = &*kf_list->find_next(t, false);
+			if (next_t - t <= time_ratio) {
+				//kf = &*kf_list->find_next(t, false);
+				KeyframeList::iterator iter;
+				if (kf_list->find_next(t, iter, false))
+					kf = &*iter;
+			}
 		}
 
 		switch(event->button.button) {

synfig-studio/src/gui/workarearenderer/renderer_timecode.cpp
@@ -102,7 +102,14 @@ Renderer_Timecode::render_vfunc(
 		try
 		{
 			int w, h;
-			layout->set_text(canvas->keyframe_list().find(cur_time)->get_description());
+			KeyframeList::iterator iter;
+			//layout->set_text(canvas->keyframe_list().find(cur_time)->get_description());
+			if (canvas->keyframe_list().find(cur_time, iter)) {
+				layout->set_text(iter->get_description());
+			} else {
+				get_work_area()->timecode_width = get_work_area()->timecode_height = 0;
+				return;
+			}
 			layout->get_size(w, h);
 			get_work_area()->timecode_width = int(w*1.0/Pango::SCALE);
 			get_work_area()->timecode_height = int(h*1.0/Pango::SCALE);

synfig-studio/src/synfigapp/actions/activepointsetsmart.cpp
@@ -196,7 +196,51 @@ Action::ActivepointSetSmart::enclose_activepoint(const synfig::Activepoint& acti
 {
 	times.insert(activepoint.get_time());
 
-	if(get_edit_mode()&MODE_ANIMATE_PAST) try
+	if (get_edit_mode()&MODE_ANIMATE_PAST) {
+		// Try to find prev keyframe
+		// Keyframe keyframe(*get_canvas()->keyframe_list().find_prev(activepoint.get_time()));
+		KeyframeList::iterator iter;
+		if (get_canvas()->keyframe_list().find_prev(activepoint.get_time(), iter)) {
+			Keyframe keyframe(*iter);
+
+			if (!times.count(keyframe.get_time())) {
+				times.insert(keyframe.get_time());
+
+			try { value_node->list[index].find(keyframe.get_time()); }
+			catch(synfig::Exception::NotFound)
+			{
+				Action::Handle action(ActivepointAdd::create());
+
+				action->set_param("canvas",get_canvas());
+				action->set_param("canvas_interface",get_canvas_interface());
+				action->set_param("value_desc",value_desc);
+
+				if(!value_node->list[index].timing_info.empty())
+				{
+					action->set_param("time",keyframe.get_time());
+				}
+				else
+				{
+					synfig::Activepoint tmp;
+
+					tmp.set_state(true);
+					tmp.set_time(keyframe.get_time());
+					action->set_param("activepoint",tmp);
+				}
+
+				assert(action->is_ready());
+				if(!action->is_ready())
+					throw Error(Error::TYPE_NOTREADY);
+
+				add_action_front(action);
+			}
+
+
+			}
+
+		}
+	}
+	/*try
 	{
 		// Try to find prev keyframe
 		Keyframe keyframe(*get_canvas()->keyframe_list().find_prev(activepoint.get_time()));
@@ -236,9 +280,51 @@ Action::ActivepointSetSmart::enclose_activepoint(const synfig::Activepoint& acti
 		}
 	}
 	catch(int) { }
-	catch(synfig::Exception::NotFound) { }
+	catch(synfig::Exception::NotFound) { }*/
 
-	if(get_edit_mode()&MODE_ANIMATE_FUTURE)try
+	if(get_edit_mode() & MODE_ANIMATE_FUTURE)
+	{
+		// Try to find next keyframe
+		KeyframeList::iterator iter;
+		if (get_canvas()->keyframe_list().find_next(activepoint.get_time(), iter)) {
+			Keyframe keyframe(*iter);
+
+			if (!times.count(keyframe.get_time())) {
+				times.insert(keyframe.get_time());
+
+				try { value_node->list[index].find(keyframe.get_time()); }
+				catch(synfig::Exception::NotFound)
+				{
+					Action::Handle action(ActivepointAdd::create());
+
+					action->set_param("canvas",get_canvas());
+					action->set_param("canvas_interface",get_canvas_interface());
+					action->set_param("value_desc",value_desc);
+
+					if(!value_node->list[index].timing_info.empty())
+					{
+						action->set_param("time",keyframe.get_time());
+					}
+					else
+					{
+						synfig::Activepoint tmp;
+
+						tmp.set_state(true);
+						tmp.set_time(keyframe.get_time());
+						action->set_param("activepoint",tmp);
+					}
+
+					assert(action->is_ready());
+					if(!action->is_ready())
+						throw Error(Error::TYPE_NOTREADY);
+
+					add_action_front(action);
+				}
+
+			}
+		}
+	}
+	/*try
 	{
 		// Try to find next keyframe
 		Keyframe keyframe(*get_canvas()->keyframe_list().find_next(activepoint.get_time()));
@@ -278,7 +364,7 @@ Action::ActivepointSetSmart::enclose_activepoint(const synfig::Activepoint& acti
 		}
 	}
 	catch(int) { }
-	catch(synfig::Exception::NotFound) { }
+	catch(synfig::Exception::NotFound) { }*/
 }
 
 void

commit 9d28194a5ff302798fa00b6c60bb358ab4e1fdda

Fix Current Time Widget behavior broken in 0589a7d: it should display text in red only when there is a keyframe at current time

synfig-studio/src/gui/canvasview.cpp
@@ -2283,14 +2283,12 @@ CanvasView::on_time_changed()
 	current_time_widget->set_value(time);
 	if (!is_playing())
 	{
-		try {
-			//get_canvas()->keyframe_list().find(time);
-			KeyframeList::iterator iter;
-			get_canvas()->keyframe_list().find(time, iter); // not sure this is needed? O_o
+		KeyframeList::iterator iter;
+		if (get_canvas()->keyframe_list().find(time, iter)) {
 			// Widget::override_color() is deprecated since Gtkmm 3.16: Use a custom style provider and style classes instead.
 			// This function is very slow!
 			current_time_widget->override_color(Gdk::RGBA("#FF0000"));
-		} catch(...) {
+		} else {
 			// Widget::override_color() is deprecated since Gtkmm 3.16: Use a custom style provider and style classes instead.
 			// This function is very slow!
 			current_time_widget->override_color(Gdk::RGBA(0));