synfig/synfig

commit c2f156175a2566e6608e6b8b67845dc1e5f0344e

Fix #972: Can't read directories on OSX 10.15 Catalina. Thanks to Artem Konoplin.

3-package-osx-dmg.sh
@@ -50,7 +50,8 @@ mkapp()
 	[ ! -d ${APPDIR_FINAL} ] || rm -rf ${APPDIR_FINAL}
 
 	cp -Rf "${SCRIPTPATH}/autobuild/osx/app-template" "$APPDIR"
-	mv ${APPDIR}/Contents/MacOS/synfigstudio $APPDIR/Contents/MacOS/SynfigStudio || true
+	mv "${APPDIR}/Contents/MacOS/synfigstudio.sh" "$APPDIR/Contents/MacOS/SynfigStudio.sh" || true
+	g++ "${SCRIPTPATH}/autobuild/osx/synfig_osx_launcher.cpp" -o "$APPDIR/Contents/MacOS/SynfigStudio"
 
 	#mkdir -p "$APPCONTENTS/bin"
 	#cp -R "$MACPORTS/bin/ffmpeg" "$APPCONTENTS/bin"

autobuild/osx/synfig_osx_launcher.cpp
@@ -1,0 +1,13 @@
+#include <cstdlib>
+#include <string>
+
+int main(int argc, char* argv[]) {
+  std::string buf(argv[0]);
+
+  std::size_t found = buf.rfind('/'); // looking for a last slash in path
+  if (found != std::string::npos) {
+    buf = buf.substr(0, found + 1); // get the directory where exe is located
+  }
+
+  return system(buf.append("SynfigStudio.sh").c_str());
+}

commit 373af3f9987fbc2dfb12cff6466768e1d1f0b6a8

Fix error introduced in c2f156175a2566

3-package-osx-dmg.sh
@@ -263,7 +263,7 @@ mkapp()
 
 	# app bundle files
 	echo "*** Please do _NOT_ delete this file. The file script depends on it. ***" > "$APPCONTENTS/v$VERSION"
-	sed -i "" -e "s/_VERSION_/$VERSION/g" "$APPDIR/Contents/MacOS/SynfigStudio"
+	sed -i "" -e "s/_VERSION_/$VERSION/g" "$APPDIR/Contents/MacOS/SynfigStudio.sh"
 	sed -i "" -e "s/_VERSION_/$VERSION/g" "$APPDIR/Contents/Info.plist"
 
 	# save information about the ports which make up this build
@@ -311,6 +311,7 @@ mkdmg()
 	cp -R ${SCRIPTPATH}/synfig-studio/COPYING /Volumes/"$VOLNAME"/LICENSE.txt
 	cp -R ${SCRIPTPATH}/autobuild/osx/README_FIRST.pdf /Volumes/"$VOLNAME"/README_FIRST.pdf
 	mv /Volumes/"$VOLNAME"/SynfigStudio.app/Contents/MacOS/synfigstudio /Volumes/"$VOLNAME"/SynfigStudio.app/Contents/MacOS/SynfigStudio || true
+	mv /Volumes/"$VOLNAME"/SynfigStudio.app/Contents/MacOS/synfigstudio.sh /Volumes/"$VOLNAME"/SynfigStudio.app/Contents/MacOS/SynfigStudio.sh || true
 
 	# open the window so that the icon database is generated
 	open /Volumes/"$VOLNAME" || true