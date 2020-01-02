a00c4ff16c0a6b3171027a7bcc6f468475d1a87b
Rodolfo Ribeiro Gomes <rodolforg@gmail.com>
2020-01-02 14:13:27 -0300

    basic implementation of sound wave widget

--- synfig-studio/src/gui/widgets/widget_soundwave.cpp
+++ synfig-studio/src/gui/widgets/widget_soundwave.cpp
@@ -55,6 +55,27 @@ Widget_SoundWave::Widget_SoundWave()
 {
 	add_events(Gdk::BUTTON_PRESS_MASK | Gdk::BUTTON_RELEASE_MASK | Gdk::SCROLL_MASK | Gdk::POINTER_MOTION_MASK | Gdk::KEY_PRESS_MASK | Gdk::KEY_RELEASE_MASK);
 	setup_mouse_handler();
+
+	set_default_page_size(255);
+	set_zoom(1.0);
+
+	int range_max = 255;
+	int range_min = 0;
+	int h = get_allocated_height();
+
+	ConfigureAdjustment(range_adjustment)
+		.set_lower(-range_max /*- 0.05*range_adjustment->get_page_size()*/)
+		.set_upper(-range_min /*+ 0.05*range_adjustment->get_page_size()*/)
+		.set_step_increment(range_adjustment->get_page_size()*20.0/h) // 20 pixels
+		.set_value((range_max-range_min)/2)
+		.finish();
+}
+
+Widget_SoundWave::~Widget_SoundWave()
+{
+	clear();
+	if (time_model_changed_connection.connected())
+		time_model_changed_connection.disconnect();
 }
 
 bool Widget_SoundWave::load(const std::string& filename)
@@ -133,8 +154,36 @@ bool Widget_SoundWave::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
 
 	std::lock_guard<std::mutex> lock(mutex);
 
+	const int bytes_per_sample = 1;
+
+	Gdk::RGBA color = get_style_context()->get_color();
+	cr->set_source_rgb(color.get_red(), color.get_green(), color.get_blue());
+
+	cr->move_to(0, 0);
+
+	const int stride = frequency * bytes_per_sample * n_channels;
+
+	for (double x = 0; x < get_width(); x+=0.1) {
+		synfig::Time t = time_plot_data->get_t_from_pixel_coord(x);
+		t = t - sound_delay;
+		synfig::Time dt = t - time_plot_data->time_model->get_lower();
+		int index = int(dt * stride) + channel_idx;
+		int value = 0;
+		if (index >= 0 && index < buffer_length)
+			std::memcpy(&value, &buffer[index], bytes_per_sample);
+		int y = time_plot_data->get_pixel_y_coord(value);
+		cr->line_to(x, y);
+	}
+	cr->set_line_width(0.3);
+	cr->stroke();
 
 	draw_current_time(cr);
+
+	int h = get_height();
+
+	ConfigureAdjustment(range_adjustment)
+		.set_step_increment(range_adjustment->get_page_size()*20.0/h) // 20 pixels
+		.finish();
 
 	cr->restore();
 	return true;
@@ -187,5 +236,54 @@ void Widget_SoundWave::setup_mouse_handler()
 
 bool Widget_SoundWave::do_load(const std::string& filename)
 {
-	return false;
+	std::string real_filename = Glib::filename_from_uri(filename);
+	Mlt::Profile profile;
+	Mlt::Producer *track = new Mlt::Producer(profile, (std::string("avformat:") + real_filename).c_str());
+	if (!track->get_producer() || track->get_length() <= 0) {
+		delete track;
+		track = new Mlt::Producer(profile, (std::string("vorbis:") + real_filename).c_str());
+		if (!track->get_producer() || track->get_length() <= 0) {
+			delete track;
+			return false;
+		}
+	}
+
+	const int length = track->get_length();
+	double fps = track->get_fps();
+	int start_frame = (time_plot_data->time_model->get_lower() - sound_delay) * fps;
+	int end_frame = (time_plot_data->time_model->get_upper() - sound_delay) * fps;
+	start_frame = synfig::clamp(start_frame, 0, length);
+	end_frame = synfig::clamp(end_frame, 0, length);
+
+	unsigned char *outbuffer = nullptr;
+
+	for (int i = start_frame; i < end_frame; ++i) {
+		Mlt::Frame *frame = track->get_frame(i);
+		if (!frame)
+			break;
+		mlt_audio_format format = mlt_audio_u8;
+		int bytes_per_sample = 1;
+		int _frequency = frequency? frequency : 48000;
+		int _channels = 2;
+		int _n_samples = 0;
+		void * _buffer = frame->get_audio(format, _frequency, _channels, _n_samples);
+		if (!buffer) {
+			buffer_length = (end_frame - start_frame + 1) * _channels * bytes_per_sample * std::round(_frequency/fps);
+			buffer = new unsigned char[buffer_length];
+			outbuffer = buffer;
+		}
+		int _n_bytes = _n_samples * _channels * bytes_per_sample;
+		std::memcpy(outbuffer, _buffer, _n_bytes);
+		outbuffer += _n_bytes;
+		frequency = _frequency;
+		n_channels = _channels;
+		n_samples += _n_samples;
+		delete frame;
+	}
+	buffer_length = outbuffer - buffer;
+	if (channel_idx > n_channels)
+		channel_idx = 0;
+	queue_draw();
+	delete track;
+	return true;
 }
