c81db9f98289af3fa61bcb8acb499d2c5b992308
Firas Hanife <FirasHanife@gmail.com>
2020-02-11 11:26:45 +0100

    Merge PR #1157: Fix some ui typos
    * Fix typo
    * Fix double space

--- synfig-studio/src/synfigapp/actions/canvasremove.cpp
+++ synfig-studio/src/synfigapp/actions/canvasremove.cpp
@@ -109,7 +109,7 @@ Action::CanvasRemove::perform()
 		throw Error(_("You cannot remove the root canvas!"));
 
 	if(get_canvas()->is_inline())
-		throw Error(_("You cannot remove an canvas from a Group!"));
+		throw Error(_("You cannot remove a canvas from a Group!"));
 
 	parent_canvas=get_canvas()->parent();
 	canvas_id=get_canvas()->get_id();
