5421739b842eee189c18acc69781cc0afab84786
Firas Hanife <FirasHanife@gmail.com>
2020-02-13 02:08:25 +0100

    Fix spacing

--- synfig-core/src/synfig/target_cairo_tile.cpp
+++ synfig-core/src/synfig/target_cairo_tile.cpp
@@ -165,7 +165,7 @@ synfig::Target_Cairo_Tile::render_frame_(Context context,ProgressCallback *cb)
 			// Add the tile to the target
 			if(!add_tile(cairo_surface_reference(surface), x,y))
 			{
-				if(cb)cb->error(_("add_tile():Unable to put surface on target"));
+				if(cb)cb->error(_("add_tile(): Unable to put surface on target"));
 				return false;
 			}
 			cairo_destroy(cr);
