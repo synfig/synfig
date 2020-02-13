f8d22228781f7fe7b5c697e8382ca7da7d02b443
Firas Hanife <FirasHanife@gmail.com>
2020-02-13 01:47:17 +0100

    Fix spacing

--- synfig-core/src/synfig/target_cairo_tile.cpp
+++ synfig-core/src/synfig/target_cairo_tile.cpp
@@ -261,7 +261,7 @@ synfig::Target_Cairo_Tile::render(ProgressCallback *cb)
 	}
 	catch (const String& str)
 	{
-		if (cb) cb->error(_("Caught string :")+str);
+		if (cb) cb->error(_("Caught string: ")+str);
 		return false;
 	}
 	catch (std::bad_alloc&)
