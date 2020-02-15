e7f11239f2dfad3e5731d882c8676822e38dac02
Firas Hanife <FirasHanife@gmail.com>
2020-02-15 12:41:32 +0100

    Make entry translatable

--- synfig-core/src/synfig/valuenodes/valuenode_compare.cpp
+++ synfig-core/src/synfig/valuenodes/valuenode_compare.cpp
@@ -50,7 +50,7 @@ using namespace synfig;
 
 /* === G L O B A L S ======================================================= */
 
-REGISTER_VALUENODE(ValueNode_Compare, RELEASE_VERSION_0_62_00, "compare", "Compare")
+REGISTER_VALUENODE(ValueNode_Compare, RELEASE_VERSION_0_62_00, "compare", _("Compare"))
 
 /* === P R O C E D U R E S ================================================= */
 
