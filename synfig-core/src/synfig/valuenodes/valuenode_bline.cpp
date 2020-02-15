fc924cdd317124a298ad81f716dfe486af23aaac
Firas Hanife <FirasHanife@gmail.com>
2020-02-15 12:37:01 +0100

    Make entry translatable

--- synfig-core/src/synfig/valuenodes/valuenode_bline.cpp
+++ synfig-core/src/synfig/valuenodes/valuenode_bline.cpp
@@ -62,7 +62,7 @@ using namespace synfig;
 
 /* === G L O B A L S ======================================================= */
 
-REGISTER_VALUENODE(ValueNode_BLine, RELEASE_VERSION_0_61_06, "bline", "Spline")
+REGISTER_VALUENODE(ValueNode_BLine, RELEASE_VERSION_0_61_06, "bline", _("Spline"))
 
 /* === P R O C E D U R E S ================================================= */
 
