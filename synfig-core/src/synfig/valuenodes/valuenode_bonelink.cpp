e30abc448814e224484dba8712fa9398052d9d43
Firas Hanife <FirasHanife@gmail.com>
2020-02-15 12:40:24 +0100

    Make entry translatable

--- synfig-core/src/synfig/valuenodes/valuenode_bonelink.cpp
+++ synfig-core/src/synfig/valuenodes/valuenode_bonelink.cpp
@@ -49,7 +49,7 @@ using namespace synfig;
 
 /* === G L O B A L S ======================================================= */
 
-REGISTER_VALUENODE(ValueNode_BoneLink, RELEASE_VERSION_1_0, "bone_link", "Bone Link")
+REGISTER_VALUENODE(ValueNode_BoneLink, RELEASE_VERSION_1_0, "bone_link", _("Bone Link"))
 
 /* === P R O C E D U R E S ================================================= */
 
