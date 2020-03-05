788ae3e8a7052cfe0742819fe377ce157f773658
AnishGG <anish.gulati@research.iiit.ac.in>
2020-03-06 04:07:59 +0530

    Improvements in travis testing script for Lottie

--- lottie_test.sh
+++ lottie_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Abort on Error
-set -e
+# Don't abort on Error
+# set -e
 
 DIRECTORY=`pwd`
 program="$DIRECTORY/synfig-studio/plugins/lottie-exporter/lottie-exporter.py"
@@ -9,8 +9,28 @@ program="$DIRECTORY/synfig-studio/plugins/lottie-exporter/lottie-exporter.py"
 git clone https://gitlab.com/synfig/synfig-tests.git
 
 test_files="$DIRECTORY/synfig-tests/export/lottie/*.sif"
+
 for filename in $test_files;
 do 
-    echo $filename;
-    python3 $program $filename;
+    pass=1
+    python3 $program $filename || { # This line catches errors in python script
+        ((Error_count=Error_count+1))
+        ((pass=0))
+    }
+
+    # To display whether a file ran successfully or not
+    if [ $pass -eq 1 ]
+    then
+        echo "[OK] $filename";
+    else
+        echo "[FAIL] $filename";
+    fi
 done
+echo -e "\nFiles failed to export: $Error_count\n"
+
+if [ $Error_count > 0 ]
+    then
+        exit 1
+    else
+        exit 0
+fi
