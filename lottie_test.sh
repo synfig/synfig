dbf1237806a92cbb40cf28857b4f50c55588a89e
AnishGG <anish.gulati@research.iiit.ac.in>
2020-03-06 20:03:04 +0530

    Colorize the output in Travis; Directory finding approach changed

--- lottie_test.sh
+++ lottie_test.sh
@@ -2,18 +2,27 @@
 # Don't abort on Error
 # set -e
 
-DIRECTORY=`pwd`
-program="$DIRECTORY/synfig-studio/plugins/lottie-exporter/lottie-exporter.py"
+WORKDIR=`dirname "$0"`
+pushd "${WORKDIR}" > /dev/null
+WORKDIR=`pwd`
+popd > /dev/null
+
+# Python program to run lottie
+LOTTIE="${WORKDIR}/synfig-studio/plugins/lottie-exporter/lottie-exporter.py"
 
 # Clone the tests directory
 git clone https://gitlab.com/synfig/synfig-tests.git
+test_files="${WORKDIR}/synfig-tests/export/lottie/*.sif"
 
-test_files="$DIRECTORY/synfig-tests/export/lottie/*.sif"
+# Color escape codes in bash
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'    # no color
 
 for filename in $test_files;
 do 
     pass=1
-    python3 $program $filename || { # This line catches errors in python script
+    python3 $LOTTIE $filename || { # This line catches errors in python script
         ((Error_count=Error_count+1))
         ((pass=0))
     }
@@ -21,9 +30,9 @@ do
     # To display whether a file ran successfully or not
     if [ $pass -eq 1 ]
     then
-        echo "[OK] $filename";
+        echo -e "${GREEN}[OK]${NC} $filename";
     else
-        echo "[FAIL] $filename";
+        echo -e "${RED}[FAIL]${NC} $filename";
     fi
 done
 echo -e "\nFiles failed to export: $Error_count\n"
