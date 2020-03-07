caf643ee68f4cdab71fe3b37483c8a7e7fce752f
AnishGG <anish.gulati@research.iiit.ac.in>
2020-03-08 00:21:03 +0530

    Job is not allowed to fail; Some tests which are consistently failing do not fail the job

--- lottie_test.sh
+++ lottie_test.sh
@@ -17,29 +17,56 @@ test_files="${WORKDIR}/synfig-tests/export/lottie/*.sif"
 # Color escape codes in bash
 GREEN='\033[0;32m'
 RED='\033[0;31m'
-NC='\033[0m'    # no color
+YELLOW='\033[0;33m'
+no_color='\033[0m'
+
+Error_count=0
+Support_soon=0
+
+# Files which are allowed to fail
+failing_files=(070-skeleton.sif 075-skeleton-group.sif 076-skeleton-group-animated.sif 079-skeleton-animated-spline.sif 082-converters-scale-link.sif 083-converters-scale-link-animated.sif)
 
 for filename in $test_files;
 do 
-    pass=1
+    pass=1      # 0 -> File is failing to export unexpectedly, 1 -> File is exporting, 2 -> File is allowed to fail
     python3 $LOTTIE $filename || { # This line catches errors in python script
-        ((Error_count=Error_count+1))
-        ((pass=0))
+
+        # Check if the program is allowed to fail
+        for i in ${failing_files[@]}
+        do
+            if [ $i = `basename $filename` ]
+            then
+                ((Support_soon=Support_soon+1))
+                pass=2
+                break
+            fi
+        done
+
+        # If it is not allowed to fail, then increase the number of errored files
+        if [ $pass -eq 1 ]
+        then
+            ((Error_count=Error_count+1))
+            ((pass=0))
+        fi
     }
 
     # To display whether a file ran successfully or not
     if [ $pass -eq 1 ]
     then
-        echo -e "${GREEN}[OK]${NC} $filename";
+        echo -e "${GREEN}[OK]${no_color} $filename";
+    elif [ $pass -eq 2 ]
+    then
+        echo -e "${YELLOW}[SUPPORT SOON]${no_color} $filename";
     else
-        echo -e "${RED}[FAIL]${NC} $filename";
+        echo -e "${RED}[FAIL]${no_color} $filename";
     fi
 done
-echo -e "\nFiles failed to export: $Error_count\n"
+echo -e "\nFiles failed to export: $Error_count"
+echo -e "Files failed, but the failure is expected: $Support_soon\n"
 
-if [ $Error_count > 0 ]
+if [ $Error_count -gt 0 ]
     then
-        exit 1
+        exit 1  # Job should fail
     else
-        exit 0
+        exit 0  # Job should pass
 fi
