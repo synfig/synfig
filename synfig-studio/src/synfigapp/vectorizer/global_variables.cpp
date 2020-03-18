4d6ee05d1f4c9a847ac81b8fbb0db47a1728ffed
Arya <ARYA-BHARDWAJ>
2020-03-18 12:34:39 +0530

    added required changes

--- synfig-studio/src/synfigapp/vectorizer/global_variables.cpp
+++ synfig-studio/src/synfigapp/vectorizer/global_variables.cpp
@@ -1,4 +1,31 @@
-#include "global_variables.h"
+/* === S Y N F I G ========================================================= */
+/*!	\file action_param.cpp
+**	\brief Template File
+**
+**	$Id$
+**
+**	\legal
+**	Copyright (c) 2002-2005 Robert B. Quattlebaum Jr., Adrian Bentley
+**
+**	This package is free software; you can redistribute it and/or
+**	modify it under the terms of the GNU General Public License as
+**	published by the Free Software Foundation; either version 2 of
+**	the License, or (at your option) any later version.
+**
+**	This package is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+**	General Public License for more details.
+**	\endlegal
+*/
+/* ========================================================================= */
+
+/* === H E A D E R S ======================================================= */
+#include"global_variables.h"
+
+
+
+/* === G L O B A L  V A R I A B L E S ======================================================= */
 
 const double global_variables::Polyg_eps_max = 1;     // Sequence simplification max error
 const double global_variables::Polyg_eps_mul = 0.75;  // Sequence simple thickness-multiplier error
--- synfig-studio/src/synfigapp/vectorizer/global_variables.h
+++ synfig-studio/src/synfigapp/vectorizer/global_variables.h
@@ -1,3 +1,33 @@
+/* === S Y N F I G ========================================================= */
+/*!	\file global _variable.h
+**	\brief Template File
+**
+**	$Id$
+**
+**	\legal
+**	Copyright (c) 2002-2005 Robert B. Quattlebaum Jr., Adrian Bentley
+**	Copyright (c) 2007 Chris Moore
+**
+**	This package is free software; you can redistribute it and/or
+**	modify it under the terms of the GNU General Public License as
+**	published by the Free Software Foundation; either version 2 of
+**	the License, or (at your option) any later version.
+**
+**	This package is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+**	General Public License for more details.
+**	\endlegal
+*/
+/* ========================================================================= */
+
+/* === S T A R T =========================================================== */
+
+#ifndef __SYNFIG_APP_GLOBAL_VARIABLE_H
+#define __SYNFIG_APP_GLOBAL_VARIABL_H
+
+/* === C L A S S E S & S T R U C T S ======================================= */
+
 class global_variables {
   public:
     static const double Polyg_eps_max;     // Sequence simplification max error
@@ -9,3 +39,4 @@ class global_variables {
     static bool max_thickness_zero;
 };
 
+#endif
