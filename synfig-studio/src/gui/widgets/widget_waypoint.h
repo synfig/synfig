de30a23af9c0fc0b76108e832a43aac34d5cba63
Rodolfo Ribeiro Gomes <rodolforg@gmail.com>
2020-03-27 23:02:56 -0300

    remove unneeded included

--- synfig-studio/src/gui/widgets/widget_waypoint.h
+++ synfig-studio/src/gui/widgets/widget_waypoint.h
@@ -27,21 +27,12 @@
 
 /* === H E A D E R S ======================================================= */
 
-#include <gtkmm/adjustment.h>
-#include <gtkmm/table.h>
-#include <gtkmm/button.h>
-#include <gtkmm/box.h>
-#include <gtkmm/comboboxtext.h>
 #include <gtkmm/spinbutton.h>
 #include <gtkmm/alignment.h>
 #include <gtkmm/frame.h>
 
-#include <synfigapp/value_desc.h>
+#include <synfig/canvas.h>
 #include <synfig/waypoint.h>
-#include <synfig/string.h>
-#include <synfig/time.h>
-#include "widgets/widget_enum.h"
-
 
 /* === M A C R O S ========================================================= */
 
@@ -55,6 +46,7 @@ namespace studio {
 class Widget_ValueBase;
 class Widget_Time;
 class Widget_Waypoint;
+class Widget_Enum;
 
 class Widget_Waypoint : public Gtk::Alignment
 {
