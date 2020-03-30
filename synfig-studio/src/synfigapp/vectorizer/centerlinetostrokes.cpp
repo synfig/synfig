fb59acf1638c0b4eb328bb0b8e285f9508493aad
ankit-kumar-dwivedi <dwivedi.ankit21@gmail.com>
2020-03-31 00:06:23 +0530

    added copyright to vectorizer

--- synfig-studio/src/synfigapp/vectorizer/centerlinetostrokes.cpp
+++ synfig-studio/src/synfigapp/vectorizer/centerlinetostrokes.cpp
@@ -5,17 +5,30 @@
 **	$Id$
 **
 **	\legal
-**	Copyright (c) 2002-2005 Robert B. Quattlebaum Jr., Adrian Bentley
+**	This file uses code from OpenToonz open-source animation software  (https://github.com/opentoonz/opentoonz/), which is developed from Toonz, a software originally created by Digital Video, S.p.A., Rome Italy Digital Video, S.p.A., Rome Italy.
 **
-**	This package is free software; you can redistribute it and/or
-**	modify it under the terms of the GNU General Public License as
-**	published by the Free Software Foundation; either version 2 of
-**	the License, or (at your option) any later version.
+**	Copyright (c) 2016 - 2019, DWANGO Co., Ltd.
+**  Copyright (c) 2016 Toshihiro Shimizu - https://github.com/meso
+**  Copyright (c) 2016 Shinya Kitaoka - https://github.com/skitaoka
+**  Copyright (c) 2016 shun-iwasawa - https://github.com/shun-iwasawa
+**  Copyright (c) 2016 Campbell Barton - https://github.com/ideasman42
+**  Copyright (c) 2019 luzpaz - https://github.com/luzpaz
+
+**	Copyright (c) 2019 - 2020, Ankit Kumar Dwivedi - https://github.com/ankit-kumar-dwivedi
 **
-**	This package is distributed in the hope that it will be useful,
-**	but WITHOUT ANY WARRANTY; without even the implied warranty of
-**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-**	General Public License for more details.
+**	LICENSE
+** 
+**	BSD 3-Clause "New" or "Revised" License
+** 
+**	Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+**
+**	1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+**
+**	2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+**
+**	3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+**
+**	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 **	\endlegal
 */
 /* ========================================================================= */
