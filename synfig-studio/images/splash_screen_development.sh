f10cb1341a3c9018120edeaefdccd9e9ff4b7126
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2020-03-31 14:11:52 +0400

    Do not use "realpath" command in build process (Autotools)

--- synfig-studio/images/splash_screen_development.sh
+++ synfig-studio/images/splash_screen_development.sh
@@ -3,11 +3,13 @@
 set -x
 
 
-SRCDIR_RELATIVE=`realpath --relative-to="$PWD" "$2"`
+if [ ! -f ./logo_tmp.sif ]; then
+	cp -f $2/logo.sif ./logo_tmp.sif
+fi
 
 export commit_id=`git log --no-color -1 | head -n 1 | cut -f 2 -d ' ' | cut -c -6`
 export commit_date=`git show --pretty=format:%ci HEAD |  head -c 10`
 export branch=`git branch -a --no-color --contains HEAD | sed -e s/\*\ // | sed -e s/\(no\ branch\)// | head -n 1 | sed s/^' '//`
 export branch=`echo $branch | egrep origin/master > /dev/null && echo master || echo $branch | cut -d ' ' -f 1 | sed -e 's/.*\///'`
-sed "s|%branch%|$branch|" "$2/splash_screen_development.sif.in" | sed "s|%commit_date%|$commit_date|" | sed "s|%commit_id%|$commit_id|" | sed "s|synfig_icon.sif#|$SRCDIR_RELATIVE/synfig_icon.sif#|" > "$1"
+sed "s|%branch%|$branch|" "$2/splash_screen_development.sif.in" | sed "s|%commit_date%|$commit_date|" | sed "s|%commit_id%|$commit_id|" | sed "s|logo.sif#|logo_tmp.sif#|" > "$1"
 
