28ff68127e2018735014e0764a068b8f5ce57364
Rodolfo Ribeiro Gomes <rodolforg@gmail.com>
2020-04-07 15:32:37 -0300

    remove sparing semicolons

--- synfig-core/src/synfig/valuenode.cpp
+++ synfig-core/src/synfig/valuenode.cpp
@@ -752,7 +752,7 @@ LinkableValueNode::link_name(int i)const
 	Vocab vocab(get_children_vocab());
 	Vocab::iterator iter(vocab.begin());
 	int j=0;
-	for(;iter!=vocab.end() && j<i; iter++, j++) {};
+	for(;iter!=vocab.end() && j<i; iter++, j++) {}
 	return iter!=vocab.end()?iter->get_name():String();
 }
 
@@ -762,7 +762,7 @@ LinkableValueNode::link_local_name(int i)const
 	Vocab vocab(get_children_vocab());
 	Vocab::iterator iter(vocab.begin());
 	int j=0;
-	for(;iter!=vocab.end() && j<i; iter++, j++){};
+	for(;iter!=vocab.end() && j<i; iter++, j++) {}
 	return iter!=vocab.end()?iter->get_local_name():String();
 }
 
