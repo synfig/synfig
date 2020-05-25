443ebea27a2042edc626ec5a96d61caae5abc9b7
AnishGG <anish.gulati@research.iiit.ac.in>
2020-05-25 11:01:44 +0530

    Update lottie test script

--- autobuild/lottie_test.sh
+++ autobuild/lottie_test.sh
@@ -23,24 +23,16 @@ no_color='\033[0m'
 Error_count=0
 Support_soon=0
 
-# Files which are allowed to fail
-failing_files=(070-skeleton.sif 075-skeleton-group.sif 076-skeleton-group-animated.sif 079-skeleton-animated-spline.sif 082-converters-scale-link.sif 083-converters-scale-link-animated.sif)
-
 for filename in $test_files;
 do 
     pass=1      # 0 -> File is failing to export unexpectedly, 1 -> File is exporting, 2 -> File is allowed to fail
     python3 $LOTTIE $filename "${filename%.sif}.json" || { # This line catches errors in python script
 
-        # Check if the program is allowed to fail
-        for i in ${failing_files[@]}
-        do
-            if [ $i = `basename $filename` ]
-            then
-                ((Support_soon=Support_soon+1))
-                pass=2
-                break
-            fi
-        done
+        # Check if the current file is allowed to fail[ATF]
+        if [[ `basename $filename` == *"ATF"* ]]; then
+            ((Support_soon=Support_soon+1))
+            pass=2
+        fi
 
         # If it is not allowed to fail, then increase the number of errored files
         if [ $pass -eq 1 ]
