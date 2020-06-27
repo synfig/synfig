d007f97f889425abc9f66461f6d8ab0114b9b732
Ayush Chamoli <ayushchamoli8@gmail.com>
2020-06-27 12:30:00 +0530

    Added a custom function for intltool_merge
    - Moved `/cmake/Modules/SynfigGettext.cmake` to `/cmake/SynfigGettext.cmake`.
    
    - Moved `/cmake/Modules/SynfigFindgGit.cmake` to `/cmake/SynfigFindGit.cmake`.
    
    - Fixed an issue where `build_pofiles_synfigstudio` wasn't building.

--- cmake/SynfigGettext.cmake
+++ cmake/SynfigGettext.cmake
@@ -30,12 +30,12 @@ function(SYNFIG_PROCESS_PO_FILES)
     set(_moFiles)
 
     if(MSGFMT_EXECUTABLE)
-        CMAKE_PARSE_ARGUMENTS(
-                _parsedArguments
-                "${_options}"
-                "${_targetName};${_moName};${_installDestination}"
-                "${_language}"
-                ${ARGN}
+        cmake_parse_arguments(
+            _parsedArguments
+            "${_options}"
+            "${_targetName};${_moName};${_installDestination}"
+            "${_language}"
+            ${ARGN}
         )
 
         foreach(_current_LANGUAGE ${_parsedArguments_LANGUAGES})
@@ -43,16 +43,16 @@ function(SYNFIG_PROCESS_PO_FILES)
             set(_moDirectory ${SYNFIG_BUILD_ROOT}/share/locale/${_current_LANGUAGE}/LC_MESSAGES)
             set(_moFile ${_moDirectory}/${_parsedArguments_MO_NAME}.mo)
             add_custom_command(OUTPUT ${_moFile}
-                    COMMAND ${CMAKE_COMMAND} -E make_directory ${_moDirectory}
-                    COMMAND ${MSGFMT_EXECUTABLE} -o ${_moFile} ${_poFile}
-                    WORKING_DIRECTORY ".."
-                    DEPENDS ${_poFile}
-                    )
+                COMMAND ${CMAKE_COMMAND} -E make_directory ${_moDirectory}
+                COMMAND ${MSGFMT_EXECUTABLE} -o ${_moFile} ${_poFile}
+                WORKING_DIRECTORY "."
+                DEPENDS ${_poFile}
+            )
 
             if(_parsedArguments_INSTALL_DESTINATION)
                 install(
-                        FILES ${_moFile} DESTINATION
-                        ${_parsedArguments_INSTALL_DESTINATION}/${_current_LANGUAGE}/LC_MESSAGES/
+                    FILES ${_moFile} DESTINATION
+                    ${_parsedArguments_INSTALL_DESTINATION}/${_current_LANGUAGE}/LC_MESSAGES/
                 )
             endif()
 
@@ -74,6 +74,6 @@ function(SYNFIG_PROCESS_PO_FILES)
             add_dependencies(build_pofiles ${_parsedArguments_TARGET_NAME})
         endif()
     else()
-        message(FATAL_ERROR "-- Could not find msgfmt: Cannot process .po files.")
+        message(WARNING "-- Could not find msgfmt: Cannot process .po files for ${_parsedArguments_TARGET_NAME}.")
     endif()
 endfunction()
