fec5e6525fd09992648408a4dc2cd12b38b97032
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2020-07-07 17:59:46 +0700

    Set defaults for Homogenous parameter of Advanced Outline: enabled and static (#1533)

--- synfig-core/src/modules/mod_geometry/advanced_outline.cpp
+++ synfig-core/src/modules/mod_geometry/advanced_outline.cpp
@@ -725,6 +725,7 @@ Advanced_Outline::get_param_vocab()const
 	ret.push_back(ParamDesc("homogeneous")
 		.set_local_name(_("Homogeneous"))
 		.set_description(_("When true, widthpoints positions are spline length based"))
+		.set_static(true)
 	);
 	ret.push_back(ParamDesc("wplist")
 		.set_local_name(_("Width Point List"))
