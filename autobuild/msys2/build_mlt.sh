bf5fdd736046da891d3c4415db738ea598a9b462
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2020-08-13 13:44:51 +0700

    Export video files with sound (#1623)
    (cherry picked from commit 99a1cde2b1e54540857925e6899deb708e17f305)
    
    # Conflicts:
    #	autobuild/msys2/build_mlt.sh

--- autobuild/msys2/build_mlt.sh
+++ autobuild/msys2/build_mlt.sh
@@ -12,23 +12,28 @@ source ${SCRIPT_DIR}/set_env.sh
 VERSION_MLT="6.16.0"
 PATH="${PREFIX}/lib/ccache/bin:${PATH}"
 
-if [ ! -f /opt/mlt-${VERSION_MLT}/done ]; then
+MLT_PATH="/opt/mlt-${VERSION_MLT}"
+
+if [ ! -f ${MLT_PATH}/done ] || [ ! -f ${MLT_PATH}/lib/mlt/libmltavformat.dll ]; then
 
 echo " ======================= Compiling MLT++ ======================= "
 
-mkdir -p /opt/mlt-${VERSION_MLT}/
+mkdir -p ${MLT_PATH}
 
-cd /tmp
+pushd /tmp
 wget "https://github.com/mltframework/mlt/releases/download/v${VERSION_MLT}/mlt-${VERSION_MLT}.tar.gz"
 tar xzf ./mlt-${VERSION_MLT}.tar.gz
 
 pushd mlt-${VERSION_MLT}/
-echo "Install path: ${PREFIX}"
-./configure --prefix=/opt/mlt-${VERSION_MLT} --target-arch=$MSYS2_ARCH --disable-gtk2
+FIXED_MLT_PATH=`cygpath -m ${MLT_PATH}`
+echo "Install path: ${MLT_PATH}"
+echo "Fixed MLT Path: ${FIXED_MLT_PATH}"
+./configure --prefix=${FIXED_MLT_PATH} --target-arch=$MSYS2_ARCH --disable-gtk2 --enable-avformat
 make -j2 --silent
 make install
 popd
 
-touch /opt/mlt-${VERSION_MLT}/done
+touch ${MLT_PATH}/done
 
+popd
 fi
