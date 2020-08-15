cc7f4032be58c1f0491cbc9f5a42dc056759339d
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2020-08-15 22:56:31 +0700

    One more fix

--- 1-setup-osx-brew.sh
+++ 1-setup-osx-brew.sh
@@ -49,18 +49,15 @@ do
     brew info "$pkg" | grep --quiet 'Not installed' && brew install "$pkg"
 done
 
-if ! ( which pip3 >/dev/null ); then
+PIPBINARY=pip3
+if ! ( which $PIPBINARY >/dev/null ); then
     echo "No pip found. Installing..."
     echo "Running python in sudo (you need root privelegies to do that)..."
     # Dependency for lxml
-    curl https://bootstrap.pypa.io/get-pip.py | sudo python
+    curl https://bootstrap.pypa.io/get-pip.py | sudo python3
 fi
 
 # Installing lxml using pip
-export PIPBINARY=pip
-if `which pip3 >/dev/null`; then
-    PIPBINARY=pip3
-fi
 STATIC_DEPS=true sudo $PIPBINARY install lxml
 
 
