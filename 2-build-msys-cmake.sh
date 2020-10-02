fa78a4df4f991b039cfe67c6530c377a8713db6b
ice0 <konoplin@gmail.com>
2020-10-02 11:48:52 +0700

    [Appveyor] Switched to Release build (increase link speed, decrease size) (#1727)

--- 2-build-msys-cmake.sh
+++ 2-build-msys-cmake.sh
@@ -2,5 +2,5 @@
 
 export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:/opt/mlt-6.16.0/lib/pkgconfig"
 mkdir -p cmake-build-msys && cd cmake-build-msys
-cmake -GNinja .. -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache
+cmake -GNinja -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_INSTALL_PREFIX=./install -DCMAKE_BUILD_TYPE=Release ..
 ninja
