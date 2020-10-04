e08502999707e7be6da0c4883d74eceb38816364
ice0 <konoplin@gmail.com>
2020-10-04 13:03:24 +0700

    [Appveyor] Added artifacts build

--- 2-build-msys-cmake.sh
+++ 2-build-msys-cmake.sh
@@ -4,3 +4,4 @@ export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:/opt/mlt-6.16.0/lib/pkgconfig"
 mkdir -p cmake-build-msys && cd cmake-build-msys
 cmake -GNinja -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_INSTALL_PREFIX=./install -DCMAKE_BUILD_TYPE=Release ..
 ninja
+ninja install
