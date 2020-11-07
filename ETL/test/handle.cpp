2ce2609ad0db3de161e8a36fbdc5740a192677ec
Rodolfo Ribeiro Gomes <rodolforg@gmail.com>
2020-11-07 20:04:53 -0300

    remove empty === N O T E S === file header sections
    or replace it with \note doxygen command when not empty

--- ETL/test/handle.cpp
+++ ETL/test/handle.cpp
@@ -19,8 +19,6 @@
 ** You should have received a copy of the GNU General Public License
 ** along with Synfig.  If not, see <https://www.gnu.org/licenses/>.
 **
-** === N O T E S ===========================================================
-**
 ** ========================================================================= */
 
 /* === H E A D E R S ======================================================= */
