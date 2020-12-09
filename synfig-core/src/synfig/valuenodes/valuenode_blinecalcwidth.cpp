bd5ebd3cea38dfd0082152e98fb03b07765f323a
Razumovsky Dmitry <svarov@yahoo.com>
2020-12-09 10:21:04 +0700

    Fix crash in Spline Width and return of improper type in Spline converters (PR #1890)
    Fix #1731
    
    The pull request contains two commits:
    
    First fixes crash in 'Spline Width' converter when you try to wrap it in 'Switch'. Similar to previously addressed in 'Spline Vertex' and 'Spline Tangent' converters.
    Second addresses the regression introduced in this commit: a90d014
    It was no longer possible to convert real values to 'Spline Tangent/Width' and angle to 'Spline Tangent'. The reason is that upon creation of converter the size of spline would be zero and code will always return ValueBase of vector type so 'Bad Connection' error would be thrown (expected real, got vector). See linked issue for screenshot of the error.

--- synfig-core/src/synfig/valuenodes/valuenode_blinecalcwidth.cpp
+++ synfig-core/src/synfig/valuenodes/valuenode_blinecalcwidth.cpp
@@ -100,13 +100,12 @@ ValueNode_BLineCalcWidth::operator()(Time t, Real amount)const
 		printf("%s:%d operator()\n", __FILE__, __LINE__);
 
 	const ValueBase::List bline = (*bline_)(t).get_list();
-	handle<ValueNode_BLine> bline_value_node( handle<ValueNode_BLine>::cast_dynamic(bline_) );
-	assert(bline_value_node);
+	const ValueBase bline_value_node = (*bline_)(t);
 
-	const bool looped = bline_value_node->get_loop();
+	const bool looped = bline_value_node.get_loop();
 	int size = (int)bline.size();
 	int count = looped ? size : size - 1;
-	if (count < 1) return Vector();
+	if (count < 1) return Real();
 
 	bool loop         = (*loop_)(t).get(bool());
 	bool homogeneous  = (*homogeneous_)(t).get(bool());
