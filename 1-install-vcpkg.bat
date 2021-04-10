f8fe4773e5e36f7379e343e466017e45c0eea930
ice0 <konoplin@gmail.com>
2021-04-10 19:38:55 +0700

    [CMake/vcpkg] Added libpng, libjpeg, freetype dependencies detection

--- 1-install-vcpkg.bat
+++ 1-install-vcpkg.bat
@@ -1,2 +1,2 @@
 set VCPKG_DEFAULT_TRIPLET=x64-windows
-.\vcpkg.exe install boost-odeint fftw3 gettext glibmm gtkmm libxmlpp libsigcpp pango sdl2-mixer zlib
+.\vcpkg.exe install boost-odeint boost-ublas fftw3 freetype gettext glibmm gtkmm libjpeg-turbo libpng libxmlpp libsigcpp pango sdl2-mixer zlib
