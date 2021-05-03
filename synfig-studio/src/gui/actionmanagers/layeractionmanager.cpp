91f620f76c247b6f73e43d94b3e55dbbbc68e3ba
Rodolfo Ribeiro Gomes <rodolforg@gmail.com>
2021-05-03 17:26:41 -0300

    SQUASH: canceling the dialog will prevent any layer paste

--- synfig-studio/src/gui/actionmanagers/layeractionmanager.cpp
+++ synfig-studio/src/gui/actionmanagers/layeractionmanager.cpp
@@ -672,7 +672,9 @@ LayerActionManager::paste()
 
 	ValueNodeReplacementMap valuenode_replacements;
 
-	query_user_about_foreign_exported_value_nodes(canvas, valuenode_replacements);
+	bool user_accepted = query_user_about_foreign_exported_value_nodes(canvas, valuenode_replacements);
+	if (!user_accepted)
+		return;
 	if (!valuenode_replacements.empty())
 		export_value_nodes(canvas, valuenode_replacements);
 
@@ -827,7 +829,7 @@ LayerActionManager::amount_dec()
 	}
 }
 
-void LayerActionManager::query_user_about_foreign_exported_value_nodes(Canvas::Handle canvas, LayerActionManager::ValueNodeReplacementMap& valuenode_replacements) const
+bool LayerActionManager::query_user_about_foreign_exported_value_nodes(Canvas::Handle canvas, LayerActionManager::ValueNodeReplacementMap& valuenode_replacements) const
 {
 	std::vector<ValueNode::LooseHandle> foreign_exported_valuenode_list;
 
@@ -839,7 +841,7 @@ void LayerActionManager::query_user_about_foreign_exported_value_nodes(Canvas::H
 		dlg->set_destination_canvas(canvas);
 		int ret = dlg->run();
 		if (ret != Gtk::RESPONSE_OK)
-			return;
+			return false;
 
 		std::map<std::string, std::string> user_choices;
 		dlg->get_user_choices(user_choices);
@@ -872,6 +874,7 @@ void LayerActionManager::query_user_about_foreign_exported_value_nodes(Canvas::H
 			}
 		}
 	}
+	return true;
 }
 
 void
