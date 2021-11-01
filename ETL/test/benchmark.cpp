adcf46d5e51cc2c45f88ff2f17664ca83feb2c44
Rodolfo Ribeiro Gomes <rodolforg@gmail.com>
2021-11-01 16:12:09 -0300

    ETL: cleanup: remove unused ETL/fixed and ETL/_fixed.h

--- ETL/test/benchmark.cpp
+++ ETL/test/benchmark.cpp
@@ -26,7 +26,6 @@
 #include <ETL/clock>
 #include <ETL/hermite>
 #include <ETL/angle>
-#include <ETL/fixed>
 #include <ETL/surface>
 #include <ETL/gaussian>
 #include <ETL/calculus>
@@ -273,48 +272,6 @@ int hermite_double_test(void)
 	t=timer();
 
 	printf("hermite<double>:time=%f milliseconds\n",t*1000);
-	return ret;
-}
-
-int hermite_fixed_test(void)
-{
-	int ret=0;
-    int i;
-	hermite<fixed> Hermie;
-	hermite<fixed>::time_type f;
-	hermite<fixed>::time_type inc(0.0005f), inc2(1.10);
-	fixed sum(0);
-
-	etl::clock timer;
-	double t;
-
-	Hermie.p1()=0;
-	Hermie.t1()=1;
-	Hermie.p2()=0;
-	Hermie.t2()=1;
-
-	Hermie.sync();
-
-	{fixed t;
-	for(i=0,f=0,timer.reset();i<HERMITE_TEST_ITERATIONS;i++,f+=inc)
-	{
-		sum+=Hermie(f)+Hermie(f+inc2);
-		sum+=Hermie(f)+Hermie(f+inc2);
-		sum+=Hermie(f)+Hermie(f+inc2);
-		sum+=Hermie(f)+Hermie(f+inc2);
-		sum+=Hermie(f)+Hermie(f+inc2);
-		sum+=Hermie(f)+Hermie(f+inc2);
-		sum+=Hermie(f)+Hermie(f+inc2);
-		sum+=Hermie(f)+Hermie(f+inc2);
-		sum+=Hermie(f)+Hermie(f+inc2);
-		sum+=Hermie(f)+Hermie(f+inc2);
-		sum+=Hermie(f)+Hermie(f+inc2);
-		sum+=Hermie(f)+Hermie(f+inc2);
-	}
-	}
-	t=timer();
-
-	printf("hermite<fixed>:time=%f milliseconds\n",t*1000);
 	return ret;
 }
 
