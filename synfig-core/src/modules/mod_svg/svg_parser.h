26e22dfc7139516496b9e3a3f2ff4dc964b25c7d
rodolforg <rodolforg@users.noreply.github.com>
2021-11-09 05:08:48 -0300

    Svg_parser: support more outline/stroke styles (#2407)
    - linejoin "bevel"
    - linecap "square"
    - dashes ("dash_array" and "dash_offset")
       * Something is off here, but better than nothing

--- synfig-core/src/modules/mod_svg/svg_parser.h
+++ synfig-core/src/modules/mod_svg/svg_parser.h
@@ -238,6 +238,7 @@ private:
 		void build_real (xmlpp::Element* root, const String& name, float value);
 		void build_vector (xmlpp::Element* root, const String& name, float x, float y);
 		void build_vector (xmlpp::Element* root, const String& name, float x, float y, const String& guid);
+		void build_dilist(xmlpp::Element* root, const std::vector<float>& p, int linecap);
 		void build_color(xmlpp::Element* root,float r,float g,float b,float a);
 		xmlpp::Element* initializeGroupLayerNode(xmlpp::Element* root, const String& name);
 
