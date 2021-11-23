a8af26007ec2f6e880b085635be8f154ab318567
nickleus27 <nickleus27@gmail.com>
2021-11-22 20:26:51 -0800

    added bool allow_multiple_selection for backwards compatability

--- synfig-studio/src/gui/app.h
+++ synfig-studio/src/gui/app.h
@@ -417,6 +417,7 @@ public:
 	static bool dialog_select_importer(const std::string& filename, std::string& plugin);
 	static bool dialog_open_file(const std::string &title, std::string &filename, std::string preference);
 	static bool dialog_open_file(const std::string &title, std::vector<std::string> &filenames, std::string preference);
+	static bool dialog_open_file_ext(const std::string &title, std::vector<std::string> &filenames, std::string preference, bool allow_multiple_selection);
 	static bool dialog_open_file_spal(const std::string &title, std::string &filename, std::string preference);
 	static bool dialog_open_file_sketch(const std::string &title, std::string &filename, std::string preference);
 	static bool dialog_open_file_image(const std::string &title, std::string &filename, std::string preference);
