ba2473605d12bd0da326355c56202eeb95888fda
rodolforg <rodolforg@users.noreply.github.com>
2022-03-03 06:41:40 -0300

    bump LumaKey layer version: it now have less parameters (#2554)

--- synfig-core/src/modules/mod_filter/lumakey.cpp
+++ synfig-core/src/modules/mod_filter/lumakey.cpp
@@ -59,7 +59,7 @@ SYNFIG_LAYER_INIT(LumaKey);
 SYNFIG_LAYER_SET_NAME(LumaKey,"lumakey");
 SYNFIG_LAYER_SET_LOCAL_NAME(LumaKey,N_("Luma Key"));
 SYNFIG_LAYER_SET_CATEGORY(LumaKey,N_("Filters"));
-SYNFIG_LAYER_SET_VERSION(LumaKey,"0.1");
+SYNFIG_LAYER_SET_VERSION(LumaKey,"0.2");
 
 /* === P R O C E D U R E S ================================================= */
 
