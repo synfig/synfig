f57d166babf0a857f71d501c20050fa023238916
mohamedAdhamc <100296264+mohamedAdhamc@users.noreply.github.com>
2022-03-11 14:44:41 +0200

    Delete 2-build-msys-Debug-cmake .sh

--- 2-build-msys-Debug-cmake .sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:/opt/mlt-7.2.0/lib/pkgconfig"
-mkdir -p cmake-build-msys && cd cmake-build-msys
-cmake -GNinja -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_INSTALL_PREFIX=./install -DCMAKE_BUILD_TYPE=Release ..
-cmake --build .
-cmake --install . >/dev/null
