ac59992f4890668087c35290c10823253ed8caec
mohamedAdhamc <100296264+mohamedAdhamc@users.noreply.github.com>
2022-03-11 13:35:59 +0200

    Update 2-build-msys-cmake.sh

--- 2-build-msys-cmake.sh
+++ 2-build-msys-cmake.sh
@@ -2,8 +2,6 @@
 
 export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:/opt/mlt-7.2.0/lib/pkgconfig"
 mkdir -p cmake-build-msys && cd cmake-build-msys
-echo type Debug for a debug build, or Release for a release build
-read buildType
-cmake -GNinja -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_INSTALL_PREFIX=./install -DCMAKE_BUILD_TYPE=$buildType ..
+cmake -GNinja -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_INSTALL_PREFIX=./install -DCMAKE_BUILD_TYPE=Release ..
 cmake --build .
 cmake --install . >/dev/null
