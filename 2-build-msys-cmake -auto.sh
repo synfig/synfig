9a192dbf7da4cf446db26f872841e1a2f5494420
mohamedAdhamc <100296264+mohamedAdhamc@users.noreply.github.com>
2022-03-11 13:31:08 +0200

    Automating build type selection using c-make

--- 2-build-msys-cmake -auto.sh
+++ 2-build-msys-cmake -auto.sh
@@ -2,6 +2,8 @@
 
 export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:/opt/mlt-7.2.0/lib/pkgconfig"
 mkdir -p cmake-build-msys && cd cmake-build-msys
-cmake -GNinja -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_INSTALL_PREFIX=./install -DCMAKE_BUILD_TYPE=Release ..
+echo type Debug for debug build, or Release for a release build
+read buildType
+cmake -GNinja -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_INSTALL_PREFIX=./install -DCMAKE_BUILD_TYPE=$buildType ..
 cmake --build .
 cmake --install . >/dev/null
