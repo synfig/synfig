f87da4e9706ba63450351dbd173c2e41e1953c4b
mohamedAdhamc <100296264+mohamedAdhamc@users.noreply.github.com>
2022-03-12 19:18:29 +0200

    Automating msys-cmake build type selection (using arguments)
    this way 
    ./2-build-msys-cmake.sh    
    runs release build which is the default because as @ice0  mentioned "Currently we use Release build on the Windows as default just because of long linking time for Debug build"
    and 
    ./2-build-msys-cmake.sh debug
    runs debug build

--- 2-build-msys-cmake.sh
+++ 2-build-msys-cmake.sh
@@ -2,6 +2,13 @@
 
 export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:/opt/mlt-7.2.0/lib/pkgconfig"
 mkdir -p cmake-build-msys && cd cmake-build-msys
+if [[ $1 = debug ]]
+then 
+cmake -GNinja -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_INSTALL_PREFIX=./install -DCMAKE_BUILD_TYPE=Debug ..
+cmake --build .
+cmake --install . >/dev/null
+else 
 cmake -GNinja -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_INSTALL_PREFIX=./install -DCMAKE_BUILD_TYPE=Release ..
 cmake --build .
 cmake --install . >/dev/null
+fi 
