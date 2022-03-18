bd7595ac47572ef7ee63b767ce30a9f94398ff27
mohamedAdhamc <100296264+mohamedAdhamc@users.noreply.github.com>
2022-03-18 15:23:01 +0200

    Revert "mnemonics imp."
    This reverts commit f1741dc11816cc6b2221d2c53db447374692d169.

--- 2-build-msys-cmake.sh
+++ 2-build-msys-cmake.sh
@@ -2,6 +2,11 @@
 
 export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:/opt/mlt-7.2.0/lib/pkgconfig"
 mkdir -p cmake-build-msys && cd cmake-build-msys
-cmake -GNinja -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_INSTALL_PREFIX=./install -DCMAKE_BUILD_TYPE=Release ..
+mode="Release"
+if [[ -n $1 ]]
+then
+mode=$1
+fi
+cmake -GNinja -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_INSTALL_PREFIX=./install -DCMAKE_BUILD_TYPE=${mode} ..
 cmake --build .
 cmake --install . >/dev/null
