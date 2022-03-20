a9abcb35a9da645f105ebd0b283f8fa662d44300
Rodolfo Ribeiro Gomes <rodolforg@gmail.com>
2022-03-20 00:20:45 -0300

    improve output readability of unit tests with test_base.h
    Although the programming interface didn't change,
    test functions are not required to return a bool value anymore.
    
    If a test throws an unexpected exception, it doesn't end the
    whole test suit anymore. Now, this fact is signalized.

--- synfig-core/test/test_base.h
+++ synfig-core/test/test_base.h
@@ -22,11 +22,25 @@
 #ifndef SYNFIG_TESTBASE_H
 #define SYNFIG_TESTBASE_H
 
-#include <iostream> // std::cerr
 #include <cstdlib> // std::abs
+#include <iostream> // std::cerr
+#include <sstream>
 
 #include <synfig/general.h> // synfig::error , synfig::info
 #include <synfig/vector.h>
+
+struct SynfigTestException : public std::exception
+{
+	std::string function;
+	int line;
+	std::string message;
+
+	SynfigTestException(std::string function, int line, std::string message)
+		: function(function), line(line), message(message)
+	{}
+
+	const char* what() const noexcept override { return message.c_str(); }
+};
 
 std::ostream& operator<<(std::ostream& os, const synfig::Vector& v)
 {
@@ -35,64 +49,66 @@ std::ostream& operator<<(std::ostream& os, const synfig::Vector& v)
 }
 
 #define ERROR_MESSAGE_TWO_VALUES(a, b) \
-	std::cerr.precision(8); \
-	std::cerr << __FUNCTION__ << ":" << __LINE__ << std::endl << "\t - expected " << a << ", but got " << b << std::endl;
+	std::ostringstream oss; \
+	oss.precision(8); \
+	oss << "\t - expected " << a << ", but got " << b << std::endl; \
+	throw SynfigTestException{__FUNCTION__, __LINE__, oss.str()}; \
 
 #define ASSERT(value) {\
 	if (!(value)) { \
-		std::cerr << __FUNCTION__ << ":" << __LINE__ << std::endl << "\t - not TRUE: " << #value << std::endl; \
-		return true; \
+		std::ostringstream oss; \
+		oss << "\t - not TRUE: " << #value << std::endl; \
+		throw SynfigTestException{__FUNCTION__, __LINE__, oss.str()}; \
 	} \
 }
 
 #define ASSERT_FALSE(value) {\
 	if (value) { \
-		std::cerr << __FUNCTION__ << ":" << __LINE__ << std::endl << "\t - not FALSE: " << #value << std::endl; \
-		return true; \
+		std::ostringstream oss; \
+		oss << "\t - not FALSE: " << #value << std::endl; \
+		throw SynfigTestException{__FUNCTION__, __LINE__, oss.str()}; \
 	} \
 }
 
 #define ASSERT_EQUAL(expected, value) {\
 	if (expected != value) { \
 		ERROR_MESSAGE_TWO_VALUES(expected, value) \
-		return true; \
 	} \
 }
 
 #define ASSERT_NOT_EQUAL(not_acceptable, value) {\
 	if (not_acceptable == value) { \
-		std::cerr.precision(8); \
-		std::cerr << __FUNCTION__ << ":" << __LINE__ << std::endl << "\t - must not be equal" << std::endl; \
-		return true; \
+		std::ostringstream oss; \
+		oss.precision(8); \
+		oss << "\t - must not be equal: " #value << " vs. " << not_acceptable << std::endl; \
+		throw SynfigTestException{__FUNCTION__, __LINE__, oss.str()}; \
 	} \
 }
 
 #define ASSERT_APPROX_EQUAL(expected, value) {\
 	if (!synfig::approximate_equal(expected, value)) { \
 		ERROR_MESSAGE_TWO_VALUES(expected, value) \
-		return true; \
 	} \
 }
 
 #define ASSERT_APPROX_EQUAL_MICRO(expected, value) {\
 	if (std::abs(expected - value) > 1e-6) { \
 		ERROR_MESSAGE_TWO_VALUES(expected, value) \
-		return true; \
 	} \
 }
 
 #define ASSERT_VECTOR_APPROX_EQUAL_MICRO(expected, value) {\
 	if (std::abs(expected[0] - value[0]) > 2e-6 || std::abs(expected[1] - value[1]) > 2e-6) { \
 		ERROR_MESSAGE_TWO_VALUES(expected, value) \
-		return true; \
 	} \
 }
 
 #define ASSERT_EXCEPTION_THROWN(expected, action) {\
 	try {\
 		action; \
-		std::cerr << __FUNCTION__ << ":" << __LINE__ << std::endl << "\t - expected exception " << #expected << " was not thrown" << std::endl; \
-		return true; \
+		std::ostringstream oss; \
+		oss << "\t - expected exception " << #expected << " was not thrown" << std::endl; \
+		throw SynfigTestException{__FUNCTION__, __LINE__, oss.str()}; \
 	} catch (expected &ex) { \
 	} \
 }
@@ -101,8 +117,9 @@ std::ostream& operator<<(std::ostream& os, const synfig::Vector& v)
 	try {\
 		action; \
 	} catch (not_expected &ex) { \
-		std::cerr << __FUNCTION__ << ":" << __LINE__ << std::endl << "\t - it should not throw exception " << #not_expected << std::endl; \
-		return true; \
+		std::ostringstream oss; \
+		oss << "\t - it should not throw exception " << #not_expected << std::endl; \
+		throw SynfigTestException{__FUNCTION__, __LINE__, oss.str()}; \
 	} \
 }
 
@@ -110,8 +127,9 @@ std::ostream& operator<<(std::ostream& os, const synfig::Vector& v)
 	try {\
 		action; \
 	} catch (...) { \
-		std::cerr << __FUNCTION__ << ":" << __LINE__ << std::endl << "\t - it should not throw any exception" << std::endl; \
-		return true; \
+		std::ostringstream oss; \
+		oss << "\t - it should not throw any exception" << std::endl; \
+		throw SynfigTestException{__FUNCTION__, __LINE__, oss.str()}; \
 	} \
 }
 
@@ -125,8 +143,9 @@ std::ostream& operator<<(std::ostream& os, const synfig::Vector& v)
 	ACTION; \
 	tst_conn.disconnect(); \
 	if (!tst_signal_emitted) { \
-		std::cerr << __FUNCTION__ << ":" << __LINE__ << std::endl << "\t - expected signal " << #SIGNAL_NAME << " was not called" << std::endl; \
-		return true; \
+		std::ostringstream oss; \
+		oss << "\t - expected signal " << #SIGNAL_NAME << " was not called" << std::endl; \
+		throw SynfigTestException{__FUNCTION__, __LINE__, oss.str()}; \
 	} \
 }
 
@@ -140,41 +159,74 @@ std::ostream& operator<<(std::ostream& os, const synfig::Vector& v)
 	ACTION; \
 	tst_conn.disconnect(); \
 	if (tst_signal_emitted) { \
-		std::cerr << __FUNCTION__ << ":" << __LINE__ << std::endl << "\t - expected signal " << #SIGNAL_NAME << " was called" << std::endl; \
-		return true; \
+		std::ostringstream oss; \
+		oss << "\t - not-expected signal was called: " << #SIGNAL_NAME << std::endl; \
+		throw SynfigTestException{__FUNCTION__, __LINE__, oss.str()}; \
 	} \
 }
 
 #define TEST_FUNCTION(function_name) {\
-	bool fail = function_name(); \
-	if (fail) { \
-		synfig::error("%s FAILED", #function_name); \
-		tst_failures++; \
-	} else { \
-		tst_successes++; \
+	std::string error_msg; \
+	try { \
+		function_name(); \
+		tst_statistics__.successes++; \
+		std::cout << "."; \
+	} catch (SynfigTestException& exc) { \
+		tst_statistics__.failures++; \
+		std::cout << "F"; \
+		tst_statistics__.errors.push_back(exc); \
+	} catch (std::logic_error& exc) { \
+		error_msg = "<Unexpected std::logic_error thrown>: "; \
+		error_msg += exc.what(); \
+	} catch (std::runtime_error& exc) { \
+		error_msg = "<Unexpected std::runtime_error thrown>: "; \
+		error_msg += exc.what(); \
+	} catch (std::exception& exc) { \
+		error_msg = "<Unexpected std::exception thrown>: "; \
+		error_msg += exc.what(); \
+	} catch (...) { \
+		error_msg = "<Unexpected and unknown exception thrown>"; \
+	} \
+\
+	if (!error_msg.empty()) { \
+		tst_statistics__.exception_thrown++; \
+		std::cout << "X"; \
+		tst_statistics__.errors.push_back({#function_name, -1, error_msg}); \
 	} \
 }
 
 #define TEST_SUITE_BEGIN() \
 	int tst_exit_status = 0; \
 	{ \
-		int tst_successes = 0; \
-		int tst_failures = 0; \
-		bool tst_exception_thrown = false; \
-		try {
+		struct tst_statistics__ { \
+			int successes = 0; \
+			int failures = 0; \
+			int exception_thrown = 0; \
+			std::vector<SynfigTestException> errors; \
+		} tst_statistics__; \
+		{
 
 #define TEST_SUITE_END() \
-		} catch (...) { \
-			synfig::error("Some exception has been thrown."); \
-			tst_exception_thrown = true; \
 		} \
-		if (tst_exception_thrown) \
-			synfig::error("Test interrupted due to an exception thrown (%i errors and %i successful tests until then)", tst_failures, tst_successes); \
-		else if (tst_failures) \
-			synfig::error("Test finished with %i errors and %i successful tests", tst_failures, tst_successes); \
+		std::cout << std::endl; \
+		for (const auto& err : tst_statistics__.errors) { \
+			if (err.line < 0) \
+				synfig::error("ERROR:\n %s", err.function.c_str()); \
+			else \
+				synfig::warning("FAILURE:\n %s:%i", err.function.c_str(), err.line); \
+			std::cerr << err.message << std::endl; \
+		} \
+		std::cerr << std::endl << "========================================================================" << std::endl; \
+		if (tst_statistics__.exception_thrown) \
+			synfig::error("Statistics:\n %i tests were interrupted by unexpected exceptions thrown.\n %i tests failed.\n %i successful tests)", \
+				tst_statistics__.exception_thrown, tst_statistics__.failures, tst_statistics__.successes); \
+		else if (tst_statistics__.failures) \
+			synfig::warning("Statistics:\n %i tests failed.\n %i successful tests", \
+				tst_statistics__.failures, tst_statistics__.successes); \
 		else \
-			synfig::info("Success (%i tests)", tst_successes); \
-		tst_exit_status = tst_exception_thrown? 2 : (tst_failures ? 1 : 0); \
+			synfig::info("Success (%i tests)", tst_statistics__.successes); \
+		std::cerr << "========================================================================" << std::endl; \
+		tst_exit_status = tst_statistics__.exception_thrown? 2 : (tst_statistics__.failures ? 1 : 0); \
 	}
 
 #endif // SYNFIG_TESTBASE_H
