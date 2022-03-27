aa3664c505a0789fb8aeeaa78d50943c1e08b548
Karim Elmosalamy <karimosalamy@gmail.com>
2022-03-27 15:02:00 +0200

    Added Invert Color Icon
    Added invert color icon to synfig-studio images folder.
    Added the icon to the list of icons to be compiled in CMakeLists.txt and Makefile.am.
    Initialized the icon in iconcontroller.cpp.

--- synfig-studio/src/gui/iconcontroller.cpp
+++ synfig-studio/src/gui/iconcontroller.cpp
@@ -271,6 +271,7 @@ IconController::init_icons(const synfig::String& path_to_icons)
 
 	INIT_STOCK_ICON(set_outline_color, "set_outline_color." IMAGE_EXT, _("Set as Outline"));
 	INIT_STOCK_ICON(set_fill_color, "set_fill_color." IMAGE_EXT, _("Set as Fill"));
+	INIT_STOCK_ICON(invert, "invert_icon." IMAGE_EXT, _("Invert Color"));
 
 	INIT_STOCK_ICON(animate_seek_begin, "animate_seek_begin_icon." IMAGE_EXT, _("Seek to Begin"));
 	INIT_STOCK_ICON(animate_seek_prev_keyframe, "animate_seek_prev_keyframe_icon." IMAGE_EXT, _("Seek to Previous Keyframe"));
