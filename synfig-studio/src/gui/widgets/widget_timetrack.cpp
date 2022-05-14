e61dae8fafe00e7ffcf4e156cf470c82d2bda368
mohamedAdhamc <100296264+mohamedAdhamc@users.noreply.github.com>
2022-05-14 04:22:07 +0200

    indent fix

--- synfig-studio/src/gui/widgets/widget_timetrack.cpp
+++ synfig-studio/src/gui/widgets/widget_timetrack.cpp
@@ -919,11 +919,11 @@ bool Widget_Timetrack::fetch_waypoints(const WaypointItem &wi, std::set<synfig::
 
 void Widget_Timetrack::on_waypoint_clicked(const Widget_Timetrack::WaypointItem& wi, unsigned int button, Gdk::Point)
 {
-		std::set<synfig::Waypoint, std::less<synfig::UniqueID> > waypoint_set;
-		fetch_waypoints(wi, waypoint_set);
-		if (waypoint_set.size() > 0) {
-		const synfigapp::ValueDesc &value_desc = param_info_map.at(wi.path.to_string()).get_value_desc();
-		signal_waypoint_clicked().emit(value_desc, waypoint_set, button);
+	std::set<synfig::Waypoint, std::less<synfig::UniqueID> > waypoint_set;
+	fetch_waypoints(wi, waypoint_set);
+	if (waypoint_set.size() > 0) {
+	const synfigapp::ValueDesc &value_desc = param_info_map.at(wi.path.to_string()).get_value_desc();
+	signal_waypoint_clicked().emit(value_desc, waypoint_set, button);
 	}
 }
 
