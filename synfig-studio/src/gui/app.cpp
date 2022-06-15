817ba7bb79be13295e1066d22e666aff6c1f8a78
mohamedAdhamc <100296264+mohamedAdhamc@users.noreply.github.com>
2022-06-15 21:50:31 +0200

    forgot header

--- synfig-studio/src/gui/app.cpp
+++ synfig-studio/src/gui/app.cpp
@@ -145,6 +145,7 @@
 #include <synfig/soundprocessor.h>
 #include <synfig/string_helper.h>
 #include <synfig/version.h>
+#include <gui/exception_guard.h>
 
 #include <synfigapp/action.h>
 #include <synfigapp/canvasinterface.h>
