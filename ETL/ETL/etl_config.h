e85c4de72485d797dcd385b10bbbd1dfdcf4be38
Rodolfo Ribeiro Gomes <rodolforg@gmail.com>
2022-06-18 10:03:39 -0300

    refactor(ETL): remove unused macros in ETL/etl_config.h

--- ETL/ETL/etl_config.h
+++ ETL/ETL/etl_config.h
@@ -9,24 +9,4 @@
 
 #define ETL_DIRECTORY_SEPARATOR		ETL_DIRECTORY_SEPARATOR0
 
-/* If __FUNC__ is not defined,
-** try to define it. If we cannot,
-** then just leave it undefined.
-*/
-#ifndef __FUNC__
-/*
- * # if defined __cplusplus ? __GNUC_PREREQ (2, 6) : __GNUC_PREREQ (2, 4)
-#   define __FUNC__	__PRETTY_FUNCTION__
-# else
-#  if defined __STDC_VERSION__ && __STDC_VERSION__ >= 199901L
-#   define __FUNC__	__func__
-#  endif
-# endif
-*/
-#endif
-
-#ifndef NULL
-#define NULL	0
-#endif
-
 #endif
