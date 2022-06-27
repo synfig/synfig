7e20c64dd2dea42876a4ec6ba21b70903f1f3678
Rodolfo Ribeiro Gomes <rodolforg@gmail.com>
2022-06-27 07:53:58 -0300

    refactor(test): synfig::Angle ported to our test_base.h

--- synfig-core/test/angle.cpp
+++ synfig-core/test/angle.cpp
@@ -1,6 +1,6 @@
-/*! ========================================================================
-** Extended Template and Library Test Suite
-** Angle Class Test
+/* === S Y N F I G ========================================================= */
+/*! \file angle.cpp
+**  \brief Test Angle class
 **
 ** Copyright (c) 2002 Robert B. Quattlebaum Jr.
 ** Copyright (c) 2008 Chris Moore
@@ -24,121 +24,102 @@
 
 /* === H E A D E R S ======================================================= */
 
-#include <stdio.h>
 #include <synfig/angle.h>
+
 #include <ETL/hermite>
+
+#include "test_base.h"
 
 /* === M A C R O S ========================================================= */
 
-using namespace etl;
+using namespace synfig;
 
 /* === C L A S S E S ======================================================= */
 
-int angle_test()
+void test_angle_dist_between_bigger_and_smaller_angle()
 {
-	int ret=0;
-	float dist;
+	float dist = Angle::deg(Angle::deg(330).dist(Angle::deg(30))).get();
+	ASSERT_EQUAL(300, floor(dist+0.5))
+}
 
-	dist=synfig::Angle::deg(synfig::Angle::deg(330).dist(synfig::Angle::deg(30))).get();
-	printf("angle: angular difference between 330deg and 30deg is %0.1fdeg\n",dist);
-	if(floor(dist+0.5)!=300)
+void test_angle_dist_between_smaller_and_bigger_angle()
+{
+	float dist = Angle::deg(Angle::deg(30).dist(Angle::deg(330))).get();
+	ASSERT_EQUAL(-300, floor(dist+0.5))
+}
+
+void test_angle_dist_between_positive_and_negative_angle()
+{
+	float dist = Angle::deg(Angle::deg(30).dist(Angle::deg(-30))).get();
+	ASSERT_EQUAL(60, floor(dist+0.5))
+}
+
+void test_angle_dist_between_negative_and_positive_angle()
+{
+	float dist = Angle::deg(Angle::deg(-30).dist(Angle::deg(30))).get();
+	ASSERT_EQUAL(-60, floor(dist+0.5))
+}
+
+void test_angle_dist_almost_minus_180d()
+{
+	float dist = Angle::deg(Angle::deg(20).dist(Angle::deg(195))).get();
+	ASSERT_EQUAL(-175, floor(dist+0.5))
+}
+
+void test_angle_dist_little_less_than_minus_180d_do_not_change_signal()
+{
+	float dist = Angle::deg(Angle::deg(20).dist(Angle::deg(205))).get();
+	ASSERT_EQUAL(-185, floor(dist+0.5))
+}
+
+void test_angle_dist_for_multiple_rotations_1()
+{
+	for(int i=-1000;i<1000;i++)
 	{
-		printf("angle: error: should be 300deg!\n");
-		ret++;
+		float dist=Angle::deg(Angle::deg(20+i+360).dist(Angle::deg(205+i-360))).get();
+		ASSERT_EQUAL(535, floor(dist+0.5))
 	}
+}
 
-	dist=synfig::Angle::deg(synfig::Angle::deg(30).dist(synfig::Angle::deg(330))).get();
-	printf("angle: angular difference between 30deg and 330deg is %0.1fdeg\n",dist);
-	if(floor(dist+0.5)!=-300)
+void test_angle_dist_for_multiple_rotations_2()
+{
+	for(int i=-1000;i<1000;i++)
 	{
-		printf("angle: error: should be -300deg!\n");
-		ret++;
+		float dist=Angle::deg(Angle::deg(20+i-360).dist(Angle::deg(195+i+360))).get();
+		ASSERT_EQUAL(-895, floor(dist+0.5))
 	}
+}
 
-	dist=synfig::Angle::deg(synfig::Angle::deg(30).dist(synfig::Angle::deg(-30))).get();
-	printf("angle: angular difference between 30deg and -30deg is %0.1fdeg\n",dist);
-	if(floor(dist+0.5)!=60)
+void test_angle_affine_combo_and_hermite()
+{
+	Angle a(Angle::deg(-2005));
+	Angle b(Angle::deg(200));
+
+	affine_combo<Angle> combo;
+
+	etl::hermite<Angle> hermie(a,b,b.dist(a),b.dist(a));
+
+	for(float f=0;f<1.001;f+=0.1)
 	{
-		printf("angle: error: should be 60deg!\n");
-		ret++;
+		printf("@%f--affine_combo: %f hermie: %f\n",Angle::deg(f).get(),Angle::deg(combo(a,b,f)).get(),Angle::deg(hermie(f)).get());
 	}
-
-	dist=synfig::Angle::deg(synfig::Angle::deg(-30).dist(synfig::Angle::deg(30))).get();
-	printf("angle: angular difference between -30deg and 30deg is %0.1fdeg\n",dist);
-	if(floor(dist+0.5)!=-60)
-	{
-		printf("angle: error: should be -60deg!\n");
-		ret++;
-	}
-
-	dist=synfig::Angle::deg(synfig::Angle::deg(20).dist(synfig::Angle::deg(195))).get();
-	printf("angle: angular difference between 20deg and 195deg is %0.1fdeg\n",dist);
-	if(floor(dist+0.5)!=-175)
-	{
-		printf("angle: error: should be -175deg!\n");
-		ret++;
-	}
-
-	dist=synfig::Angle::deg(synfig::Angle::deg(20).dist(synfig::Angle::deg(205))).get();
-	printf("angle: angular difference between 20deg and 205deg is %0.1fdeg\n",dist);
-	if(floor(dist+0.5)!=-185)
-	{
-		printf("angle: error: should be -185deg!\n");
-		ret++;
-	}
-
-	int i;
-
-	for(i=-1000;i<1000;i++)
-	{
-		dist=synfig::Angle::deg(synfig::Angle::deg(20+i+360).dist(synfig::Angle::deg(205+i-360))).get();
-		if(floor(dist+0.5)!=535)
-		{
-			printf("angle: error: Badness at %d!\n",i);
-			ret++;
-		}
-
-	}
-
-	for(i=-1000;i<1000;i++)
-	{
-		dist=synfig::Angle::deg(synfig::Angle::deg(20+i-360).dist(synfig::Angle::deg(195+i+360))).get();
-		if(floor(dist+0.5)!=-895)
-		{
-			printf("angle: error: Badness at %d!\n",i);
-			ret++;
-		}
-
-	}
-
-
-
-	{
-		float f;
-		synfig::Angle a(synfig::Angle::deg(-2005));
-		synfig::Angle b(synfig::Angle::deg(200));
-
-		affine_combo<synfig::Angle> combo;
-
-		etl::hermite<synfig::Angle> hermie(a,b,b.dist(a),b.dist(a));
-
-		for(f=0;f<1.001;f+=0.1)
-		{
-			printf("@%f--affine_combo: %f hermie: %f\n",synfig::Angle::deg(f).get(),synfig::Angle::deg(combo(a,b,f)).get(),synfig::Angle::deg(hermie(f)).get());
-		}
-
-	}
-
-	return ret;
 }
 
 /* === E N T R Y P O I N T ================================================= */
 
-int main()
-{
-	int error=0;
+int main() {
 
-	error+=angle_test();
+	TEST_SUITE_BEGIN()
+	TEST_FUNCTION(test_angle_dist_between_bigger_and_smaller_angle)
+	TEST_FUNCTION(test_angle_dist_between_smaller_and_bigger_angle)
+	TEST_FUNCTION(test_angle_dist_between_positive_and_negative_angle)
+	TEST_FUNCTION(test_angle_dist_between_negative_and_positive_angle)
+	TEST_FUNCTION(test_angle_dist_almost_minus_180d)
+	TEST_FUNCTION(test_angle_dist_little_less_than_minus_180d_do_not_change_signal)
+	TEST_FUNCTION(test_angle_dist_for_multiple_rotations_1)
+	TEST_FUNCTION(test_angle_dist_for_multiple_rotations_2)
+	TEST_FUNCTION(test_angle_affine_combo_and_hermite)
+	TEST_SUITE_END()
 
-	return error;
+	return tst_exit_status;
 }
