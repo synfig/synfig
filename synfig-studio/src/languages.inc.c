717fdc3590ff730de37701dfacb42ef5a770a368
mohamedAdhamc <100296264+mohamedAdhamc@users.noreply.github.com>
2022-07-30 15:24:32 +0200

    alignment fix

--- synfig-studio/src/languages.inc.c
+++ synfig-studio/src/languages.inc.c
@@ -10,7 +10,7 @@
 //
 //
 
-{ "ar",          "Arabic",                          },
+{ "ar",          "Arabic",                           },
 { "bs",          "Bosnian",                          },
 { "ca",          "Catalan",                          },
 { "ca@valencia", "Catalan (Valencian)",              },
