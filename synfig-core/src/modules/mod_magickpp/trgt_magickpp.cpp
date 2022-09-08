9784b2d67a102495f2d1b3bdb1ff918f39e7dafc
Rodolfo Ribeiro Gomes <rodolforg@gmail.com>
2022-09-08 10:38:27 -0300

    fix(trgt_magickpp): the previous row pointer was always the current one

--- synfig-core/src/modules/mod_magickpp/trgt_magickpp.cpp
+++ synfig-core/src/modules/mod_magickpp/trgt_magickpp.cpp
@@ -239,12 +239,12 @@ magickpp_trgt::end_frame()
 bool
 magickpp_trgt::start_frame(synfig::ProgressCallback */*callback*/)
 {
+	previous_row_buffer_pointer = buffer_pointer;
+
 	if (buffer_pointer == buffer1.data())
 		buffer_pointer = current_row_buffer_pointer = buffer2.data();
 	else
 		buffer_pointer = current_row_buffer_pointer = buffer1.data();
-
-	previous_row_buffer_pointer = buffer_pointer;
 
 	transparent = false;
 
@@ -260,8 +260,7 @@ magickpp_trgt::start_scanline(int /*scanline*/)
 bool
 magickpp_trgt::end_scanline()
 {
-	if (previous_row_buffer_pointer)
-		color_to_pixelformat(previous_row_buffer_pointer, color_buffer.data(), PF_RGB|PF_A, 0, width);
+	color_to_pixelformat(current_row_buffer_pointer, color_buffer.data(), PF_RGB|PF_A, 0, width);
 
 	if (!transparent && previous_row_buffer_pointer) {
 		for (int i = 0; i < width; i++) {
