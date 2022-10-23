7a7e5b90e5e431f06d805a4a8cbe7001a193dc59
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2022-10-24 06:56:27 +0700

    fix: Interface background became white after clicking OK in Preferences dialog (#2732)
    The canvas area is actually a Gtk::DrawingArea, and its default background
    color is white (on light themes).
    However, at start, it's not properly draw, and only the central area is
    drawn by Synfig software (via workarea renderers). When Work Area itself
    gains focus, it shows its white background defined by Gtk theme.
    
    Fix #2636

--- synfig-studio/src/synfigapp/value_desc.cpp
+++ synfig-studio/src/synfigapp/value_desc.cpp
@@ -55,7 +55,11 @@ const ValueDesc ValueDesc::blank;
 void ValueDesc::on_id_changed()
 {
 	try {
-		name = get_value_node()->get_id();
+		synfig::ValueNode::Handle value_node = get_value_node();
+		if (value_node)
+				name = value_node->get_id();
+		else
+				name.clear();
 	} catch (Exception::IDNotFound &) {
 		name.clear();
 	}
