synfig/synfig

commit c91ce50797be72fa56b59f4aa36c986ef504e7b4

fix(ffmpeg): do not try to export audio for gif or mng (#2915)

synfig-core/src/modules/mod_ffmpeg/trgt_ffmpeg.cpp
@@ -148,9 +148,14 @@ ffmpeg_trgt::set_rend_desc(RendDesc *given_desc)
 bool
 ffmpeg_trgt::init(ProgressCallback* cb = nullptr)
 {
+	bool with_sound = false;
+	const std::string extension = etl::filename_extension(filename);
+	const std::vector<const char*> image_only_extensions{".gif", ".mng"};
+	const bool does_file_format_support_audio = std::find(image_only_extensions.begin(), image_only_extensions.end(), extension) != image_only_extensions.end();
 
-	bool with_sound = true;
-	if(!SoundProcessor::subsys_init()) {
+	if (!does_file_format_support_audio) {
+		with_sound = false;
+	} else if(!SoundProcessor::subsys_init()) {
 		if (cb) cb->error(_("Unable to initialize Sound subsystem"));
 		with_sound = false;
 	} else {

commit c5904339e5515d883a5197c055e11765468f92e0

fix(ffmpeg): logic for detection of image-only videos was inverted
error introduced by me in #2915 (c91ce50797be72fa56b59f4aa36c986ef504e7b4)

synfig-core/src/modules/mod_ffmpeg/trgt_ffmpeg.cpp
@@ -151,7 +151,7 @@ ffmpeg_trgt::init(ProgressCallback* cb = nullptr)
 	bool with_sound = false;
 	const std::string extension = etl::filename_extension(filename);
 	const std::vector<const char*> image_only_extensions{".gif", ".mng"};
-	const bool does_file_format_support_audio = std::find(image_only_extensions.begin(), image_only_extensions.end(), extension) != image_only_extensions.end();
+	const bool does_file_format_support_audio = std::find(image_only_extensions.begin(), image_only_extensions.end(), extension) == image_only_extensions.end();
 
 	if (!does_file_format_support_audio) {
 		with_sound = false;