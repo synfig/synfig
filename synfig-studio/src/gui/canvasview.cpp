synfig/synfig

commit 87cc5d28090b172eabdf7f92baaded3bd20cd01e

Apply action to all selected layers from pop-over menu (#2352)

synfig-studio/src/gui/canvasview.cpp
@@ -1789,7 +1789,12 @@ CanvasView::popup_layer_menu(Layer::Handle layer)
 	param_list.add("time",canvas_interface()->get_time());
 	param_list.add("canvas",Canvas::Handle(layer->get_canvas()));
 	param_list.add("canvas_interface",canvas_interface());
-	param_list.add("layer",layer);
+
+	SelectionManager::LayerList layer_list(get_selection_manager()->get_selected_layers());
+	SelectionManager::LayerList::iterator iter;
+
+	for(iter=layer_list.begin();iter!=layer_list.end();++iter)
+		param_list.add("layer",Layer::Handle(*iter));
 
 	//Gtk::Menu *newlayers(manage(new Gtk::Menu()));
 	//build_new_layer_menu(*newlayers);

commit 63f7675fd6b4507ec5b5f2b135cd5a43f99a5dd5

fix: Right-clicking a layer on a just opened file shows empty menu (#2781)
The issue was introduced by PR #2352

When the list of selected layers was empty, then
clicking on the layer opened an empty menu.

Now the logic has changed:
If the layer is in the list of selected layers, then
actions are applied to them, if not, then the selection
is removed from them and the layer that was clicked
becomes selected (and actions applied to it).

synfig-studio/src/gui/canvasview.cpp
@@ -1753,11 +1753,19 @@ CanvasView::popup_layer_menu(Layer::Handle layer)
 	param_list.add("canvas",Canvas::Handle(layer->get_canvas()));
 	param_list.add("canvas_interface",canvas_interface());
 
-	SelectionManager::LayerList layer_list(get_selection_manager()->get_selected_layers());
-	SelectionManager::LayerList::iterator iter;
+	SelectionManager::LayerList layer_list = get_selection_manager()->get_selected_layers();
 
-	for(iter=layer_list.begin();iter!=layer_list.end();++iter)
-		param_list.add("layer",Layer::Handle(*iter));
+	auto found = std::find(layer_list.cbegin(), layer_list.cend(), layer);
+	if (found == layer_list.cend())	{
+		// if the layer is not in the list of already selected layers, then clear selection and select it
+		get_selection_manager()->clear_selected_layers();
+		get_selection_manager()->set_selected_layer(layer);
+		param_list.add("layer", layer);
+	} else {
+		for (auto& layer_handle : layer_list) {
+			param_list.add("layer", layer_handle);
+		}
+	}
 
 	//Gtk::Menu *newlayers(manage(new Gtk::Menu()));
 	//build_new_layer_menu(*newlayers);