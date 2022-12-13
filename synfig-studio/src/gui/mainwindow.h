9be0e01056834a1841162b924d2dc26575c29818
rodolforg <rodolforg@users.noreply.github.com>
2022-12-12 23:13:43 -0300

    refactor: switched `MainWindow` to `Gtk::ApplicationWindow` (#2935)

--- synfig-studio/src/gui/mainwindow.h
+++ synfig-studio/src/gui/mainwindow.h
@@ -31,7 +31,7 @@
 /* === H E A D E R S ======================================================= */
 
 #include <gtkmm/actiongroup.h>
-#include <gtkmm/window.h>
+#include <gtkmm/applicationwindow.h>
 #include <synfig/string.h>
 
 /* === M A C R O S ========================================================= */
@@ -44,7 +44,7 @@ namespace studio {
 	class Dockable;
 	class DockBook;
 
-	class MainWindow: public Gtk::Window
+	class MainWindow: public Gtk::ApplicationWindow
 	{
 	private:
 		Gtk::Bin *bin_;
@@ -75,7 +75,7 @@ namespace studio {
 		virtual bool on_key_press_event(GdkEventKey *key_event);
 
 	public:
-		MainWindow();
+		MainWindow(const Glib::RefPtr<Gtk::Application>& application);
 		virtual ~MainWindow();
 
 		Gtk::Bin& root() { return *bin_; }
