5f81684235929ea4dab5644cd7e2896fa9d1901f
Rodolfo Ribeiro Gomes <rodolforg@gmail.com>
2023-01-19 08:47:48 -0300

    refactor: insert main menu actions for selected layers
    Layer menu item is now complete

--- synfig-studio/src/gui/instance.h
+++ synfig-studio/src/gui/instance.h
@@ -32,6 +32,9 @@
 
 /* === H E A D E R S ======================================================= */
 
+#include <giomm/menu.h>
+#include <giomm/simpleactiongroup.h>
+
 #include <ETL/handle>
 #include <gtkmm/treestore.h>
 #include <gui/trees/historytreestore.h>
@@ -221,11 +224,13 @@ public:
 	void add_actions_to_menu(Gtk::Menu *menu, const synfigapp::Action::ParamList &param_list1,const synfigapp::Action::ParamList &param_list2, synfigapp::Action::Category category=synfigapp::Action::CATEGORY_ALL)const;
 
 	void add_actions_to_group(const Glib::RefPtr<Gtk::ActionGroup>& action_group, synfig::String& ui_info,   const synfigapp::Action::ParamList &param_list, synfigapp::Action::Category category=synfigapp::Action::CATEGORY_ALL)const;
+	void add_actions_to_group_and_menu(const Glib::RefPtr<Gio::SimpleActionGroup>& action_group, const std::string& action_group_name, const Glib::RefPtr<Gio::Menu>& menu, const synfigapp::Action::ParamList& param_list, synfigapp::Action::Category category = synfigapp::Action::CATEGORY_ALL) const;
 
 	void add_special_layer_actions_to_menu(Gtk::Menu *menu, const synfig::Layer::Handle &layer) const;
 	void add_special_layer_actions_to_group(const Glib::RefPtr<Gtk::ActionGroup>& action_group, synfig::String& ui_info, const synfig::Layer::Handle &layer) const;
 	void add_special_layer_actions_to_menu(Gtk::Menu *menu, const synfigapp::SelectionManager::LayerList &layers) const;
 	void add_special_layer_actions_to_group(const Glib::RefPtr<Gtk::ActionGroup>& action_group, synfig::String& ui_info, const synfigapp::SelectionManager::LayerList &layers) const;
+	void add_special_layer_actions_to_group_and_menu(const Glib::RefPtr<Gio::SimpleActionGroup>& action_group, const std::string& action_group_name, const Glib::RefPtr<Gio::Menu>& menu, const synfigapp::SelectionManager::LayerList &layers) const;
 
 	void process_action(synfig::String name, synfigapp::Action::ParamList param_list);
 
