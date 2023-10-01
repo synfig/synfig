0a13177cd7f62888c2be402184b005814443cc5a
Rodolfo Ribeiro Gomes <rodolforg@gmail.com>
2023-09-30 23:41:29 -0300

    style: use synfig-core header template for handle.h

--- synfig-core/src/synfig/handle.h
+++ synfig-core/src/synfig/handle.h
@@ -1,38 +1,32 @@
-/* === E T L =============================================================== */
-/*!	\file _handle.h
-**	\brief Template Object Handle Implementation
-**	\internal
+/* === S Y N F I G ========================================================= */
+/*! \file handle.h
+** \brief Template Object Handle Implementation
 **
-**	\legal
-**	Copyright (c) 2002 Robert B. Quattlebaum Jr.
-**	Copyright (c) 2007, 2008 Chris Moore
+** \legal
+** Copyright (c) 2002 Robert B. Quattlebaum Jr.
+** Copyright (c) 2007, 2008 Chris Moore
 **
-**	This file is part of Synfig.
+** This file is part of Synfig.
 **
-**	Synfig is free software: you can redistribute it and/or modify
-**	it under the terms of the GNU General Public License as published by
-**	the Free Software Foundation, either version 2 of the License, or
-**	(at your option) any later version.
+** Synfig is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 2 of the License, or
+** (at your option) any later version.
 **
-**	Synfig is distributed in the hope that it will be useful,
-**	but WITHOUT ANY WARRANTY; without even the implied warranty of
-**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-**	GNU General Public License for more details.
+** Synfig is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
 **
-**	You should have received a copy of the GNU General Public License
-**	along with Synfig.  If not, see <https://www.gnu.org/licenses/>.
-**	\endlegal
-**
-**	\note
-**		This is an internal header file, included by other ETL headers.
-**		You should not attempt to use it directly.
-*/
-/* ========================================================================= */
+** You should have received a copy of the GNU General Public License
+** along with Synfig.  If not, see <https://www.gnu.org/licenses/>.
+** \endlegal
+** ========================================================================= */
 
 /* === S T A R T =========================================================== */
 
-#ifndef __ETL__HANDLE_H
-#define __ETL__HANDLE_H
+#ifndef SYNFIG_HANDLE_H
+#define SYNFIG_HANDLE_H
 
 /* === H E A D E R S ======================================================= */
 
@@ -909,4 +903,4 @@ template <class T>		   bool operator<(const T*				  lhs,const loose_handle<T>& r
 
 /* === E N D =============================================================== */
 
-#endif
+#endif // SYNFIG_HANDLE_H
