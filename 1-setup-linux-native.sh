fd4dc4cc1588e564f30467e9ade41c922ff08ced
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2024-05-04 15:41:37 +0700

    build: Update script for building with cmake
    Closes #3352

--- 1-setup-linux-native.sh
+++ 1-setup-linux-native.sh
@@ -66,6 +66,8 @@ if ([ "$ID_LIKE" == "fedora" ] && [ "$VERSION_ID" > 22 ]); then
     PKG_LIST="git \
             intltool \
             cmake \
+            ninja-build \
+            ccache \
             libpng-devel \
             libjpeg-devel \
             fftw-devel \
@@ -112,6 +114,8 @@ elif ( [ "$ID_LIKE" == "fedora" ] && [ "$VERSION_ID" <= 22 ] ) || ( [ "$ID_LIKE"
     PKG_LIST="git \
             intltool \
             cmake \
+            ninja-build \
+            ccache \
             libpng-devel \
             libjpeg-devel \
             fftw-devel \
@@ -150,7 +154,7 @@ elif ( [ "$ID_LIKE" == "fedora" ] && [ "$VERSION_ID" <= 22 ] ) || ( [ "$ID_LIKE"
     fi
 
 elif [ "$ID_LIKE" == "suse opensuse" ]; then
-    PKG_LIST="git libpng-devel libjpeg-devel freetype-devel fontconfig-devel atk-devel pango-devel cairo-devel gtk3-devel gettext-devel libxml2-devel libxml++-devel gcc-c++ autoconf automake libtool libtool-ltdl-devel shared-mime-info"
+    PKG_LIST="git cmake ninja ccache libpng-devel libjpeg-devel freetype-devel fontconfig-devel atk-devel pango-devel cairo-devel gtk3-devel gettext-devel libxml2-devel libxml++-devel gcc-c++ autoconf automake libtool libtool-ltdl-devel shared-mime-info"
     PKG_LIST="${PKG_LIST} OpenEXR-devel libmng-devel ImageMagick-c++-devel gtkmm3-devel glibmm2-devel"
 
     if ! ( rpm -qv $PKG_LIST ); then
@@ -189,7 +193,8 @@ elif [ "$ID_LIKE" == "arch" ]; then
             mlt \
             openexr \
             shared-mime-info \
-            cmake make \
+            cmake make ninja \
+            ccache \
             python-lxml"
     echo "Running pacman (root privileges are needed)..."
     echo
@@ -201,6 +206,8 @@ elif [ -f /etc/altlinux-release ]; then
                 git-core \
                 shared-mime-info \
                 cmake \
+                ninja-build \
+                ccache \
                 intltool \
                 gettext \
                 libjpeg-devel \
@@ -245,6 +252,8 @@ elif [ "$ID_LIKE" == "debian" ] || [ "$ID_LIKE" == "ubuntu" ] || [ "$ID_LIKE" ==
             PKG_LIST=" \
                 build-essential \
                 cmake \
+                ninja \
+                ccache \
                 autoconf automake autopoint \
                 shared-mime-info \
                 libltdl3-dev \
--- 2-build-cmake.sh
+++ 2-build-cmake.sh
@@ -1,59 +1,29 @@
 #!/usr/bin/env bash
-#
-# = Usage: =
-#    ./2-build-cmake.sh [OPTIONS]
-#
-# OPTIONS
-# -d = Debug build (standard)
-# -r = Release build
-# -n = Don't write the run-portable.sh to the out dir
-# -j NUMBER = Set parallel make jobs (1 <= NUMBER <= 999) (standard is 1)
-# -i = Enable incremental build (disables the clean_build_dir, gen_dir_structure and the build_images steps). Before using this option, you will have to build Synfig Studio on time without this option on the selected build mode (or use the --rerender option)
-# --rerender = Rerender the images (even if incremental build is activated)
-# -p = Only print out the current active build settings and exit
-# --data-prefix = The installed Synfig Studio looks for it's data (icons, sounds, etc) in [DATA_PREFIX]/share/. This option sets a custom DATA_PREFIX. (Standard is the out directory)
-#
-# = Examples =
-# Make a debug build
-#    ./build-cmake -d
-#
-# Make a release build
-#    ./build-cmake -r
-#
-# Make a release build with two parallel make jobs
-#    ./build-cmake -r -j 2
 
-#Define dir paths
-pwd_dir="$PWD"
-absolute_script_path="$(readlink -f "$0")"
-absolute_base_dir="$(dirname "$absolute_script_path")"
+set -e
 
-cd ${absolute_base_dir}
+WORKDIR=`dirname "$0"`
+cd "${WORKDIR}"
+WORKDIR=`pwd`
 
-# Include build folder names and build functions
-source ./autobuild/build-cmake-common.sh
+mkdir -p cmake-build && cd cmake-build
 
-if [ $? -ne 0 ]
-    then
-        echo "Failed to include: ./autobuild/build-cmake-common.sh"
-        cd "$pwd_dir"
-        exit
+mode="Release"
+if [[ -n $1 ]]
+then
+mode=$1
 fi
 
-# Parse build options
-parse_build_arguments "$@"
+cmake -GNinja -DENABLE_TESTS=ON -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_INSTALL_PREFIX=./install -DCMAKE_BUILD_TYPE=${mode} ..
+cmake --build .
+cmake --install . >/dev/null
+ctest --output-on-failure
 
-# Print build settings
-print_build_settings
-
-# Exec build steps
-clean_build_dir
-gen_dir_structure
-build_etl
-build_synfig_core
-build_synfig_studio
-write_portable_run_code
-
-# Print success, restore the current directory and exit
-echo "Build successful to: ${absolute_base_dir}/${cmake_build_dir}/${out_dir}"
-cd "$pwd_dir"
+echo
+echo
+echo "Done. Please check your Synfig installation in"
+echo " ${WORKDIR}/cmake-build/output/$mode/"
+echo
+echo "You can start Synfig by executing"
+echo " ${WORKDIR}/cmake-build/output/$mode/bin/synfigstudio"
+echo
