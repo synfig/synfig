aea3ba94bbf9ba7bf44a1ecfdd3e2c9cdcdeba0f
Rodolfo Ribeiro Gomes <rodolforg@gmail.com>
2024-06-02 12:35:38 -0300

    style: fix Codacy complains for cmake build script

--- 2-build-cmake.sh
+++ 2-build-cmake.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-WORKDIR=`dirname "$0"`
+WORKDIR=$(dirname "$0")
 cd "${WORKDIR}"
-WORKDIR=`pwd`
+WORKDIR=$(pwd)
 
 mkdir -p cmake-build && cd cmake-build
 
@@ -14,7 +14,7 @@ then
 mode=$1
 fi
 
-cmake -GNinja -DENABLE_TESTS=ON -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_INSTALL_PREFIX=./install -DCMAKE_BUILD_TYPE=${mode} ..
+cmake -GNinja -DENABLE_TESTS=ON -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_INSTALL_PREFIX=./install -DCMAKE_BUILD_TYPE="${mode}" ..
 cmake --build .
 cmake --install . >/dev/null
 ctest --output-on-failure
