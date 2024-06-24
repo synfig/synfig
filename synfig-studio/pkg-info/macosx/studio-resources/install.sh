09ebbedc802f5867fcc40e8d19c25f22193895da
Rodolfo Ribeiro Gomes <rodolforg@gmail.com>
2024-06-24 08:23:46 -0300

    build: remove creation of deprecated synfig-config
    Use pkgconf instead

--- synfig-studio/pkg-info/macosx/studio-resources/install.sh
+++ synfig-studio/pkg-info/macosx/studio-resources/install.sh
@@ -18,7 +18,7 @@ PREFIX=/usr/local
 
 cd $RESOURCE_DIR
 
-echo "Creating synfig-config script"
+echo "Creating installation directory tree"
 [ -d $PREFIX ] || mkdir $PREFIX
 [ -d $PREFIX/bin ] || mkdir $PREFIX/bin
 [ -d $PREFIX/include ] || mkdir $PREFIX/include
@@ -27,24 +27,8 @@ echo "Creating synfig-config script"
 
 echo "Cleaning up any previous installation"
 [ -d $PREFIX/include/synfig ] && rm -fr $PREFIX/include/synfig
-ln -s /Library/Frameworks/synfig.framework/Headers $PREFIX/include/synfig
 
-sed '
-s:@exec_prefix@:/usr/local:g;
-s:@prefix@:/usr/local:g;
-s:@bindir@:$exec_prefix/bin:g;
-s:@libdir@:$exec_prefix/lib:g;
-s:@includedir@:$prefix/include:g;
-s:@VERSION@:@_VERSION_@:g;
-s:@PACKAGE@:@_PACKAGE_@:g;
-s:@LIBS@::g;
-s:@VERSION@:@_VERSION_@:;
-s:@PACKAGE@:@_PACKAGE_@:;
-s:@CONFIG_LIBS@:-F/Library/Frameworks/synfig.framework:;
-s:@synfig_LIBS@:-F/Library/Frameworks/synfig.framework:;
-s:@CONFIG_CFLAGS@:-framework synfig:;
-' < $RESOURCE_DIR/synfig-config.in > $PREFIX/bin/synfig-config
-chmod 775 $PREFIX/bin/synfig-config
+ln -s /Library/Frameworks/synfig.framework/Headers $PREFIX/include/synfig
 
 echo "Precompiling Headers"
 #/usr/bin/c++ -precomp /Library/Frameworks/synfig.framework/Headers/synfig.h -o /Library/Frameworks/synfig.framework/Headers/synfig.p
@@ -56,6 +40,3 @@ echo "Done with shell script"
 
 
 exit 0
-
-
-
