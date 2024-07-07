bfc853ea5375eeff21dd45c4c1f4e9112339f0ff
veermetri05 <veermetri05@gmail.com>
2024-07-07 14:58:47 +0530

    review fixes

--- synfig-studio/src/gui/dialogs/dialog_pluginmanager.cpp
+++ synfig-studio/src/gui/dialogs/dialog_pluginmanager.cpp
@@ -1,24 +1,36 @@
 /* === S Y N F I G ========================================================= */
-/*!	\file dialogs/dialog_pluginmanager.h
-**	\brief Implementation of dialog to install, uninstall and manage plugins
-**
-**	$Id$
+/*!	\file template.cpp
+**	\brief Template File
 **
 **	\legal
 **	Copyright (c) 2002-2005 Robert B. Quattlebaum Jr., Adrian Bentley
 **
-**	This package is free software; you can redistribute it and/or
-**	modify it under the terms of the GNU General Public License as
-**	published by the Free Software Foundation; either version 2 of
-**	the License, or (at your option) any later version.
+**	This file is part of Synfig.
 **
-**	This package is distributed in the hope that it will be useful,
+**	Synfig is free software: you can redistribute it and/or modify
+**	it under the terms of the GNU General Public License as published by
+**	the Free Software Foundation, either version 2 of the License, or
+**	(at your option) any later version.
+**
+**	Synfig is distributed in the hope that it will be useful,
 **	but WITHOUT ANY WARRANTY; without even the implied warranty of
-**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-**	General Public License for more details.
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**	GNU General Public License for more details.
+**
+**	You should have received a copy of the GNU General Public License
+**	along with Synfig.  If not, see <https://www.gnu.org/licenses/>.
 **	\endlegal
 */
 /* ========================================================================= */
+
+/* === H E A D E R S ======================================================= */
+
+#ifdef USING_PCH
+#	include "pch.h"
+#else
+#ifdef HAVE_CONFIG_H
+#	include <config.h>
+#endif
 
 #include "dialog_pluginmanager.h"
 
@@ -26,15 +38,27 @@
 #include <gtkmm/notebook.h>
 #include <gtkmm/listbox.h>
 #include <gtkmm/listboxrow.h>
-#include <gui/localization.h>
 
+#include <gui/localization.h>
+#include <gui/app.h>
 
 #include <synfig/general.h>
 #include <synfig/os.h>
 
+#endif
+
+/* === U S I N G =========================================================== */
+
 using namespace synfig;
 using namespace studio;
 
+/* === M A C R O S ========================================================= */
+
+/* === G L O B A L S ======================================================= */
+
+/* === P R O C E D U R E S ================================================= */
+
+/* === M E T H O D S ======================================================= */
 
 Dialog_PluginManager::Dialog_PluginManager(Gtk::Window& parent):
     Gtk::Dialog(_("Plugin Manager"), parent),
