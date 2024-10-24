3b123c866c26586d05778a62a5ea5e270f488c77
veermetri05 <veermetri05@gmail.com>
2024-10-24 19:34:46 +0530

    plugin configuration: saving, restoring

--- synfig-studio/src/gui/pluginmanager.cpp
+++ synfig-studio/src/gui/pluginmanager.cpp
@@ -63,8 +63,125 @@
 
 #include <synfig/general.h>
 #include <synfig/os.h>
+#include "pluginmanager.h"
 
 #endif
+
+namespace JSON {
+   
+    void Parser::skip_whitespace() {
+        while (input_[pos_] == ' ' || input_[pos_] == '\n' || 
+               input_[pos_] == '\r' || input_[pos_] == '\t')
+            pos_++;
+    }
+    
+    std::string Parser::parse_string() {
+        pos_++;
+        std::string value;
+        while (input_[pos_] != '"' || (pos_ > 0 && input_[pos_-1] == '\\')) {
+            if (input_[pos_] == '\\') {
+                pos_++;
+                switch (input_[pos_]) {
+                    case '"': value += '"'; break;
+                    case '\\': value += '\\'; break;
+                    case '/': value += '/'; break;
+                    case 'b': value += '\b'; break;
+                    case 'f': value += '\f'; break;
+                    case 'n': value += '\n'; break;
+                    case 'r': value += '\r'; break;
+                    case 't': value += '\t'; break;
+                    case 'u': {
+                        // Skip unicode handling for now
+                        pos_ += 4;
+                        value += '?';
+                        break;
+                    }
+                    default: value += input_[pos_];
+                }
+            } else {
+                value += input_[pos_];
+            }
+            pos_++;
+        }
+        pos_++; // Skip closing quote
+        return value;
+    }
+    
+    std::map<std::string, std::string> Parser::parse_object() {
+        pos_++; // Skip opening brace
+        std::map<std::string, std::string> object;
+        
+        while (true) {
+            skip_whitespace();
+            if (input_[pos_] == '}') {
+                pos_++;
+                break;
+            }
+            
+            if (!object.empty()) {
+                if (input_[pos_] != ',')
+                    throw std::runtime_error("Expected comma in object");
+                pos_++;
+                skip_whitespace();
+            }
+            
+            if (input_[pos_] != '"')
+                throw std::runtime_error("Expected string key in object");
+            
+            std::string key = parse_string();
+            
+            skip_whitespace();
+            if (input_[pos_] != ':')
+                throw std::runtime_error("Expected colon after key in object");
+            pos_++;
+            
+            skip_whitespace();
+            if (input_[pos_] == '"')
+                object[key] = parse_string();
+            else if (input_[pos_] == '{') {
+                // Skip nested objects for now, treat as empty string
+                int depth = 1;
+                while (depth > 0) {
+                    pos_++;
+                    if (input_[pos_] == '{') depth++;
+                    if (input_[pos_] == '}') depth--;
+                }
+                pos_++;
+                object[key] = "";
+            }
+            else if (input_[pos_] == '[') {
+                // Skip arrays for now, treat as empty string
+                int depth = 1;
+                while (depth > 0) {
+                    pos_++;
+                    if (input_[pos_] == '[') depth++;
+                    if (input_[pos_] == ']') depth--;
+                }
+                pos_++;
+                object[key] = "";
+            }
+            else {
+                // Parse number or other value until next delimiter
+                std::string value;
+                while (input_[pos_] != ',' && input_[pos_] != '}') {
+                    value += input_[pos_];
+                    pos_++;
+                }
+                object[key] = value;
+            }
+        }
+        
+        return object;
+    }
+    
+	std::map<std::string, std::string> Parser::parse(const std::string& json) {
+		Parser parser(json.c_str());
+		parser.skip_whitespace();
+		if (parser.input_[parser.pos_] != '{')
+			throw std::runtime_error("Expected object");
+		return parser.parse_object();
+	}
+};
 
 std::string
 JSON::escape_string(const std::string& str)
@@ -177,7 +294,9 @@ fetch_data_in_widget(const Gtk::Widget* w, std::map<std::string, std::string>& d
 			data[w->get_name()] = static_cast<const Gtk::ColorButton*>(w)->get_rgba().to_string();
 		} else if (GTK_IS_FONT_BUTTON(w->gobj())) {
 			// https://docs.gtk.org/Pango/type_func.FontDescription.from_string.html
-			PangoFontDescription* font_desc = gtk_font_chooser_get_font_desc(GTK_FONT_CHOOSER(w));
+			const auto* font_button = static_cast<const Gtk::FontButton*>(w);
+		    data[w->get_name()] = font_button->get_font_name();
+			PangoFontDescription* font_desc = gtk_font_chooser_get_font_desc(GTK_FONT_CHOOSER(w->gobj()));
 			char* str = pango_font_description_to_string(font_desc);
 			pango_font_description_free(font_desc);
 			data[w->get_name()] = str;
@@ -196,14 +315,15 @@ fetch_data_in_widget(const Gtk::Widget* w, std::map<std::string, std::string>& d
 			data[w->get_name()] = static_cast<const Gtk::Entry*>(w)->get_text();
 		}
 	}
-	if (GTK_IS_CONTAINER(w->gobj())) {
+	if (GTK_IS_CONTAINER(w->gobj()) && (w->get_name().find("gtkmm__") == 0)) {
+		// synfig::info("Fetching children of " + w->get_name() + ((w->get_name().find("gtkmm__") == 0) ? "0" : "1"));
 		for (const Gtk::Widget* c : static_cast<const Gtk::Container*>(w)->get_children())
 			fetch_data_in_widget(c, data);
 	}
 };
 
-static std::map<std::string, std::string>
-parse_dialog(const Gtk::Widget& dialog_contents)
+std::map<std::string, std::string>
+studio::PluginManager::parse_dialog(const Gtk::Widget& dialog_contents)
 {
 	std::map<std::string, std::string> data;
 	fetch_data_in_widget(&dialog_contents, data);
@@ -553,7 +673,7 @@ bool studio::PluginManager::check_and_run_dialog(const PluginScript& script, std
 					int result = dialog.run();
 					if (result != Gtk::RESPONSE_ACCEPT)
 						return false;
-					auto dialog_data = parse_dialog(*contents);
+					auto dialog_data = PluginManager::parse_dialog(*contents);
 //					delete dialog;
 					for (const auto& d : dialog_data) {
 						if (!dialog_args.empty())
