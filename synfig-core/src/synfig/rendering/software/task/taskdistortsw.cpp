75c37d2bcf2095f8b8eadaab2e3d4c25129ba13e
Rodolfo Ribeiro Gomes <rodolforg@gmail.com>
2025-01-03 12:14:59 -0300

    fix: shift caused by DistortSW

--- synfig-core/src/synfig/rendering/software/task/taskdistortsw.cpp
+++ synfig-core/src/synfig/rendering/software/task/taskdistortsw.cpp
@@ -51,44 +51,57 @@ using namespace synfig::rendering;
 
 bool TaskDistortSW::run_task(const rendering::TaskDistort& task) const
 {
-	if (!task.sub_task())
-		return true;
+	const rendering::Task::Handle& sub_task = task.sub_task();
+
 	if (!task.is_valid())
 		return true;
-
-	const Vector ppu = task.get_pixels_per_unit();
+	if (!sub_task)
+		return true;
+	if (!sub_task->is_valid())
+		return true;
 
 	rendering::TaskSW::LockWrite la(&task);
 	if (!la)
 		return false;
 
-	rendering::TaskSW::LockRead lb(task.sub_task());
+	rendering::TaskSW::LockRead lb(sub_task);
 	if (!lb)
 		return false;
-	const Vector ppub = task.sub_task()->get_pixels_per_unit();
-	const int tw = task.target_rect.get_width();
+
 	const synfig::Surface& b = lb->get_surface();
 
+	Matrix raster_to_world_transformation;
+	{
+		const Vector upp = task.get_units_per_pixel();
+		raster_to_world_transformation.m00 = upp[0];
+		raster_to_world_transformation.m11 = upp[1];
+		raster_to_world_transformation.m20 = task.source_rect.minx - upp[0]*task.target_rect.minx;
+		raster_to_world_transformation.m21 = task.source_rect.miny - upp[1]*task.target_rect.miny;
+	}
+
+	Matrix subtask_world_to_raster_transformation;
+	{
+		Task::Handle sub_task = task.sub_task();
+		const Vector sub_ppu = sub_task->get_pixels_per_unit();
+		subtask_world_to_raster_transformation.m00 = sub_ppu[0];
+		subtask_world_to_raster_transformation.m11 = sub_ppu[1];
+		subtask_world_to_raster_transformation.m20 = sub_task->target_rect.minx - sub_ppu[0]*sub_task->source_rect.minx;
+		subtask_world_to_raster_transformation.m21 = sub_task->target_rect.miny - sub_ppu[1]*sub_task->source_rect.miny;
+	}
+
+	const int tw = task.target_rect.get_width();
+	const Vector dx = raster_to_world_transformation.axis_x(); // upp[0]
+	const Vector dy = raster_to_world_transformation.axis_y() - dx*(Real)tw; // (-dx*tw, upp[1])
+	Vector p = raster_to_world_transformation.get_transformed( Vector((Real)task.target_rect.minx, (Real)task.target_rect.miny) );
 	synfig::Surface::pen pen(la->get_surface().get_pen(task.target_rect.minx, task.target_rect.miny));
-
-	Matrix bounds_transformation;
-	bounds_transformation.m00 = ppu[0];
-	bounds_transformation.m11 = ppu[1];
-	bounds_transformation.m20 = task.target_rect.minx - ppu[0]*task.source_rect.minx;
-	bounds_transformation.m21 = task.target_rect.miny - ppu[1]*task.source_rect.miny;
-
-	Matrix inv_matrix = bounds_transformation.get_inverted();
-
-	Vector dx = inv_matrix.axis_x();
-	Vector dy = inv_matrix.axis_y() - dx*(Real)tw;
-	Vector p = inv_matrix.get_transformed( Vector((Real)task.target_rect.minx, (Real)task.target_rect.miny) );
 
 	for (int iy = task.target_rect.miny; iy < task.target_rect.maxy; ++iy, p += dy, pen.inc_y(), pen.dec_x(tw)) {
 		for (int ix = task.target_rect.minx; ix < task.target_rect.maxx; ++ix, p += dx, pen.inc_x()) {
 			Point tmp = point_vfunc(p);
 
-			float u = (tmp[0]-task.required_source_rect.minx)*ppub[0];
-			float v = (tmp[1]-task.required_source_rect.miny)*ppub[1];
+			Point raster_pos = subtask_world_to_raster_transformation.get_transformed(tmp);
+			Real u = raster_pos[0];
+			Real v = raster_pos[1];
 			if (u<0 || v<0 || u>=b.get_w() || v>=b.get_h() || std::isnan(u) || std::isnan(v)) {
 				// problem! It shouldn't happen!!
 				pen.put_value(Color::magenta());
