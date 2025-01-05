3669f9d4142c4bf904d35743089866ba0d101a50
Rodolfo Ribeiro Gomes <rodolforg@gmail.com>
2025-01-05 07:08:55 -0300

    refactor: better organization of TaskDistortSW

--- synfig-core/src/synfig/rendering/software/task/taskdistortsw.cpp
+++ synfig-core/src/synfig/rendering/software/task/taskdistortsw.cpp
@@ -49,68 +49,101 @@ using namespace synfig::rendering;
 
 /* === M E T H O D S ======================================================= */
 
-bool TaskDistortSW::run_task(const rendering::TaskDistort& task) const
+bool
+TaskDistortSW::run_task(const rendering::TaskDistort& task) const
 {
-	const rendering::Task::Handle& sub_task = task.sub_task();
+	LoopInfo info = get_loop_info(task);
 
-	if (!task.is_valid())
+	if (info.may_end)
 		return true;
-	if (!sub_task)
-		return true;
-	if (!sub_task->is_valid())
-		return true;
+	if (info.should_abort)
+		return false;
 
 	rendering::TaskSW::LockWrite la(&task);
 	if (!la)
 		return false;
 
-	rendering::TaskSW::LockRead lb(sub_task);
-	if (!lb)
+	const rendering::Task::Handle& sub_task = task.sub_task();
+
+	rendering::TaskSW::LockRead sub_lock(sub_task);
+	if (!sub_lock)
 		return false;
 
-	const synfig::Surface& b = lb->get_surface();
+	const synfig::Surface& sub_surface = sub_lock->get_surface();
 
-	Matrix raster_to_world_transformation;
-	{
-		const Vector upp = task.get_units_per_pixel();
-		raster_to_world_transformation.m00 = upp[0];
-		raster_to_world_transformation.m11 = upp[1];
-		raster_to_world_transformation.m20 = task.source_rect.minx - upp[0]*task.target_rect.minx;
-		raster_to_world_transformation.m21 = task.source_rect.miny - upp[1]*task.target_rect.miny;
-	}
-
-	Matrix subtask_world_to_raster_transformation;
-	{
-		Task::Handle sub_task = task.sub_task();
-		const Vector sub_ppu = sub_task->get_pixels_per_unit();
-		subtask_world_to_raster_transformation.m00 = sub_ppu[0];
-		subtask_world_to_raster_transformation.m11 = sub_ppu[1];
-		subtask_world_to_raster_transformation.m20 = sub_task->target_rect.minx - sub_ppu[0]*sub_task->source_rect.minx;
-		subtask_world_to_raster_transformation.m21 = sub_task->target_rect.miny - sub_ppu[1]*sub_task->source_rect.miny;
-	}
-
-	const int tw = task.target_rect.get_width();
-	const Vector dx = raster_to_world_transformation.axis_x(); // upp[0]
-	const Vector dy = raster_to_world_transformation.axis_y() - dx*(Real)tw; // (-dx*tw, upp[1])
-	Vector p = raster_to_world_transformation.get_transformed( Vector((Real)task.target_rect.minx, (Real)task.target_rect.miny) );
 	synfig::Surface::pen pen(la->get_surface().get_pen(task.target_rect.minx, task.target_rect.miny));
 
-	for (int iy = task.target_rect.miny; iy < task.target_rect.maxy; ++iy, p += dy, pen.inc_y(), pen.dec_x(tw)) {
-		for (int ix = task.target_rect.minx; ix < task.target_rect.maxx; ++ix, p += dx, pen.inc_x()) {
-			Point tmp = point_vfunc(p);
+	for (int iy = task.target_rect.miny; iy < task.target_rect.maxy; ++iy, info.initial_p += info.p_dy, pen.move(info.pen_dy[0], info.pen_dy[1])) {
+		for (int ix = task.target_rect.minx; ix < task.target_rect.maxx; ++ix, info.initial_p += info.p_dx, pen.inc_x()) {
+			Point res = point_vfunc(info.initial_p);
 
-			Point raster_pos = subtask_world_to_raster_transformation.get_transformed(tmp);
-			Real u = raster_pos[0];
-			Real v = raster_pos[1];
-			if (u<0 || v<0 || u>=b.get_w() || v>=b.get_h() || std::isnan(u) || std::isnan(v)) {
-				// problem! It shouldn't happen!!
-				pen.put_value(Color::magenta());
+			Point raster_p = info.sub_world_to_raster_transformation.get_transformed(res);
+
+			if (!raster_p.is_valid()) {
+				// problem! It shouldn't happen!! At least one of the coordinates is NaN
+				pen.put_value(Color::cyan());
 				continue;
 			} else {
-				pen.put_value(b.cubic_sample(u,v));
+				Real u = raster_p[0];
+				Real v = raster_p[1];
+				if (u<0 || v<0 || u>=sub_surface.get_w() || v>=sub_surface.get_h()) {
+					// problem! It shouldn't happen!! Out of sub_task surface bounds
+					pen.put_value(Color::magenta());
+					continue;
+				}
+
+				pen.put_value(sub_surface.cubic_sample(u,v));
 			}
 		}
 	}
 
 	return true;
+}
+
+TaskDistortSW::LoopInfo
+TaskDistortSW::get_loop_info(const Task& task)
+{
+	LoopInfo info;
+
+	if (!task.is_valid()) {
+		info.may_end = true;
+		return info;
+	}
+
+	if (task.sub_tasks.empty() || !task.sub_tasks[0]) {
+		info.may_end = true;
+		return info;
+	}
+
+	{
+		auto sub_task = task.sub_tasks.at(0);
+		if (!sub_task->is_valid()) {
+			info.may_end = true;
+			return info;
+		}
+
+		const Vector sub_ppu = sub_task->get_pixels_per_unit();
+		info.sub_world_to_raster_transformation.m00 = sub_ppu[0];
+		info.sub_world_to_raster_transformation.m11 = sub_ppu[1];
+		info.sub_world_to_raster_transformation.m20 = sub_task->target_rect.minx - sub_ppu[0]*sub_task->source_rect.minx;
+		info.sub_world_to_raster_transformation.m21 = sub_task->target_rect.miny - sub_ppu[1]*sub_task->source_rect.miny;
+	}
+
+	const Vector upp = task.get_units_per_pixel();
+
+	Matrix raster_to_world_transformation;
+	raster_to_world_transformation.m00 = upp[0];
+	raster_to_world_transformation.m11 = upp[1];
+	raster_to_world_transformation.m20 = task.source_rect.minx - upp[0]*task.target_rect.minx;
+	raster_to_world_transformation.m21 = task.source_rect.miny - upp[1]*task.target_rect.miny;
+
+	const int tw = task.target_rect.get_width();
+	info.p_dx = {upp[0] , 0};
+	info.p_dy = {- info.p_dx[0] * tw, upp[1]};
+
+	info.pen_dy = {-tw, 1};
+
+	info.initial_p = raster_to_world_transformation.get_transformed( Vector(Real(task.target_rect.minx), Real(task.target_rect.miny)) );
+
+	return info;
 }
