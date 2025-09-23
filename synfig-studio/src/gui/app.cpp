2c90ddc2c7e76185fa84f8f6d530d2db930f7118
Abdelhadi Wael <waelhadi380@gmail.com>
2025-09-23 20:45:03 +0300

    remove the old brush tool and change the new brush tool icon

--- synfig-studio/src/gui/app.cpp
+++ synfig-studio/src/gui/app.cpp
@@ -1738,7 +1738,7 @@ void App::init(const synfig::String& rootpath)
 		/* other */
 		state_manager->add_state(&state_text);
 		if(!getenv("SYNFIG_DISABLE_SKETCH" )) state_manager->add_state(&state_sketch);
-		if(!getenv("SYNFIG_DISABLE_BRUSH"  ) && App::enable_experimental_features) state_manager->add_state(&state_brush);
+		// if(!getenv("SYNFIG_DISABLE_BRUSH"  ) && App::enable_experimental_features) state_manager->add_state(&state_brush);
 		state_manager->add_state(&state_brush2);
 		state_manager->add_state(&state_zoom);
 
