dd573524afcc75fd41e512e4e5d113147188f713
Ahmed Fathy <ahmedfathi20044002@gmail.com>
2026-07-07 14:31:01 +0300

    fix(UI): make Bezier hover detection instantly responsive to mouse movement
    Previously, Bezier hovering was only evaluated when the canvas happened to redraw (e.g. from moving over a Duck). This adds proper hover tracking to WorkArea to queue redraws reliably on motion.

--- synfig-studio/src/gui/workarea.h
+++ synfig-studio/src/gui/workarea.h
@@ -173,6 +173,7 @@ private:
 
 	etl::handle<Duckmatic::Duck> clicked_duck;
 	etl::handle<Duckmatic::Duck> hover_duck;
+	etl::handle<Duckmatic::Bezier> hover_bezier;
 
 	//! When dragging the viewport, this is set to the origin of the drag
 	synfig::Point drag_point;
