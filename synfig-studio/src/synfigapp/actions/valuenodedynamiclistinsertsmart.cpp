42b8482ec6db4fd2c01a7c452bdc95a8e50643e4
XYuktaaa <xyukta@gmail.com>
2026-09-18 18:27:33 +0530

    fix: restore ActivepointSetOff for dynamic list insertion

--- synfig-studio/src/synfigapp/actions/valuenodedynamiclistinsertsmart.cpp
+++ synfig-studio/src/synfigapp/actions/valuenodedynamiclistinsertsmart.cpp
@@ -258,6 +258,24 @@ Action::ValueNodeDynamicListInsertSmart::prepare()
 				throw Error(Error::TYPE_NOTREADY);
 
 			add_action(action);
+			// This commented code creates a 'off' Active Point at time.begin()
+			// that produces bugs like
+			action=Action::create("ActivepointSetOff");
+
+			if(!action)
+				throw Error(_("Unable to find action \"ActivepointSetOff\""));
+
+			action->set_param("edit_mode",MODE_ANIMATE);
+			action->set_param("canvas",get_canvas());
+			action->set_param("canvas_interface",get_canvas_interface());
+			action->set_param("time",Time::begin());
+			action->set_param("origin",origin);
+			action->set_param("value_desc",ValueDesc(value_node,index));
+
+			if(!action->is_ready())
+				throw Error(Error::TYPE_NOTREADY);
+
+			add_action(action);
 
 			// If we are inserting the first element, or don't want to
 			// keep the shape, there is nothing more to do
