44c575e386e72929d492c5e0509b78077e05a8cc
rodolforg <rodolforg@users.noreply.github.com>
2026-09-20 14:24:41 -0300

    fix: wrong computation of total length of strokes (it could crash) (#3799)

--- synfig-studio/src/synfigapp/wplistconverter.cpp
+++ synfig-studio/src/synfigapp/wplistconverter.cpp
@@ -104,7 +104,7 @@ WPListConverter::operator()(std::list<synfig::WidthPoint> &wp_out, const std::li
 	if (synfig::approximate_zero(distances[n-1])) {
 		norm_distances.push_back(0.);
 	} else {
-		int total_length = distances[n-1];
+		Real total_length = distances[n-1];
 		for(i=0;i<n;i++)
 			norm_distances.push_back(distances[i]/total_length);
 	}
